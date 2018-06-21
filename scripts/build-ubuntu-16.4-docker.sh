e856b03ae18029622ce899b3b5f53edaf959a1ed
Jacek Danecki <jacek.danecki@intel.com>
2018-06-21 13:39:56 +0200

    ci: fetch all tags before build
    Signed-off-by: Jacek Danecki <jacek.danecki@intel.com>

--- scripts/build-ubuntu-16.4-docker.sh
+++ scripts/build-ubuntu-16.4-docker.sh
@@ -19,6 +19,7 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
+git fetch -t
 git clone ../compute-runtime neo
 docker build -f scripts/docker/Dockerfile-ubuntu-16.04-gcc-5 -t neo-ubuntu-16.04-gcc-5:ci .
 
--- scripts/build-ubuntu-18.4-docker.sh
+++ scripts/build-ubuntu-18.4-docker.sh
@@ -19,6 +19,7 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
+git fetch -t
 git clone ../compute-runtime neo
 docker build -f scripts/docker/Dockerfile-ubuntu-18.04-gcc-7 -t neo-ubuntu-18.4-gcc-7:ci .
 
