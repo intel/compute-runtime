cf971158c6672b9937cb958b3c374e3f79e38310
Dunajski, Bartosz <bartosz.dunajski@intel.com>
2018-08-14 10:28:13 +0200

    Improve compilation time: split api_tests_wrapper into 3 files
    Change-Id: I5ae90154fb9baa5fb60adceef1995097b4125b81
    Signed-off-by: Dunajski, Bartosz <bartosz.dunajski@intel.com>

---
+++ unit_tests/api/api_tests_wrapper3.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2018, Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "unit_tests/api/cl_get_kernel_work_group_info_tests.inl"
+#include "unit_tests/api/cl_get_mem_object_info_tests.inl"
+#include "unit_tests/api/cl_get_pipe_info_tests.inl"
+#include "unit_tests/api/cl_get_platform_ids_tests.inl"
+#include "unit_tests/api/cl_get_platform_info_tests.inl"
+#include "unit_tests/api/cl_get_program_build_info_tests.inl"
+#include "unit_tests/api/cl_get_program_info_tests.inl"
+#include "unit_tests/api/cl_get_supported_image_formats_tests.inl"
+#include "unit_tests/api/cl_icd_get_platform_ids_khr_tests.inl"
+#include "unit_tests/api/cl_intel_accelerator_tests.inl"
+#include "unit_tests/api/cl_intel_motion_estimation.inl"
+#include "unit_tests/api/cl_link_program_tests.inl"
+#include "unit_tests/api/cl_release_command_queue_tests.inl"
+#include "unit_tests/api/cl_release_context_tests.inl"
+#include "unit_tests/api/cl_release_event_tests.inl"
+#include "unit_tests/api/cl_release_kernel_tests.inl"
+#include "unit_tests/api/cl_release_mem_obj_tests.inl"
+#include "unit_tests/api/cl_release_program_tests.inl"
+#include "unit_tests/api/cl_retain_mem_obj_tests.inl"
+#include "unit_tests/api/cl_retain_release_command_queue_tests.inl"
+#include "unit_tests/api/cl_retain_release_context_tests.inl"
+#include "unit_tests/api/cl_retain_release_device_tests.inl"
+#include "unit_tests/api/cl_retain_release_sampler_tests.inl"
+#include "unit_tests/api/cl_set_default_device_command_queue_tests.inl"
+#include "unit_tests/api/cl_set_event_callback_tests.inl"
+#include "unit_tests/api/cl_set_kernel_arg_svm_pointer_tests.inl"
+#include "unit_tests/api/cl_set_kernel_exec_info_tests.inl"
+#include "unit_tests/api/cl_set_mem_object_destructor_callback_tests.inl"
+#include "unit_tests/api/cl_set_performance_configuration_tests.inl"
+#include "unit_tests/api/cl_svm_alloc_tests.inl"
+#include "unit_tests/api/cl_svm_free_tests.inl"
+#include "unit_tests/api/cl_unload_compiler_tests.inl"
+#include "unit_tests/api/cl_unload_platform_compiler_tests.inl"
