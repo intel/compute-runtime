7292a6e23b4bc7e70f27ce3f968b72be0709db05
Artur Harasimiuk <artur.harasimiuk@intel.com>
2018-08-27 17:13:16 +0200

    cmake: respect value of NEO_DRIVER_VERSION when provided externally
    Change-Id: I32913a45739df7e0991119b98118a36afc7a65bb
    Signed-off-by: Artur Harasimiuk <artur.harasimiuk@intel.com>

--- version.cmake
+++ version.cmake
@@ -19,48 +19,51 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 
 if(UNIX)
-  find_program(GIT NAMES git)
-  if(NOT "${GIT}" STREQUAL "GIT-NOTFOUND")
-    if(IS_DIRECTORY ${IGDRCL_SOURCE_DIR}/.git)
-      set(GIT_arg --git-dir=${IGDRCL_SOURCE_DIR}/.git show -s --format=%ct)
-      execute_process(
-        COMMAND ${GIT} ${GIT_arg}
-        OUTPUT_VARIABLE GIT_output
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-      )
+  if(NOT DEFINED NEO_DRIVER_VERSION)
+    find_program(GIT NAMES git)
+    if(NOT "${GIT}" STREQUAL "GIT-NOTFOUND")
+      if(IS_DIRECTORY ${IGDRCL_SOURCE_DIR}/.git)
+        set(GIT_arg --git-dir=${IGDRCL_SOURCE_DIR}/.git show -s --format=%ct)
+        execute_process(
+          COMMAND ${GIT} ${GIT_arg}
+          OUTPUT_VARIABLE GIT_output
+          OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+      endif()
     endif()
-  endif()
 
-  if(NOT DEFINED NEO_VERSION_MAJOR)
-    if(NOT DEFINED GIT_output)
-      set(NEO_VERSION_MAJOR 1)
-    else()
-      SET(DATE_arg --date=@${GIT_output} +%y)
-      execute_process(
-        COMMAND date ${DATE_arg}
-        OUTPUT_VARIABLE NEO_VERSION_MAJOR
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-      )
-      message(STATUS "Computed version major is: ${NEO_VERSION_MAJOR}")
+    if(NOT DEFINED NEO_VERSION_MAJOR)
+      if(NOT DEFINED GIT_output)
+        set(NEO_VERSION_MAJOR 1)
+      else()
+        SET(DATE_arg --date=@${GIT_output} +%y)
+        execute_process(
+          COMMAND date ${DATE_arg}
+          OUTPUT_VARIABLE NEO_VERSION_MAJOR
+          OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+        message(STATUS "Computed version major is: ${NEO_VERSION_MAJOR}")
+      endif()
     endif()
-  endif()
 
-  if(NOT DEFINED NEO_VERSION_MINOR)
-    if(NOT DEFINED GIT_output)
-      set(NEO_VERSION_MINOR 0)
-    else()
-      SET(DATE_arg --date=@${GIT_output} +%V)
-      execute_process(
-        COMMAND date ${DATE_arg}
-        OUTPUT_VARIABLE NEO_VERSION_MINOR
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-      )
-      message(STATUS "Computed version minor is: ${NEO_VERSION_MINOR}")
+    if(NOT DEFINED NEO_VERSION_MINOR)
+      if(NOT DEFINED GIT_output)
+        set(NEO_VERSION_MINOR 0)
+      else()
+        SET(DATE_arg --date=@${GIT_output} +%V)
+        execute_process(
+          COMMAND date ${DATE_arg}
+          OUTPUT_VARIABLE NEO_VERSION_MINOR
+          OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+        message(STATUS "Computed version minor is: ${NEO_VERSION_MINOR}")
+      endif()
     endif()
-  endif()
 
-  if(NOT DEFINED NEO_VERSION_BUILD)
-    set(NEO_VERSION_BUILD 0)
+    if(NOT DEFINED NEO_VERSION_BUILD)
+      set(NEO_VERSION_BUILD 0)
+    endif()
+    set(NEO_DRIVER_VERSION "${NEO_VERSION_MAJOR}.${NEO_VERSION_MINOR}.${NEO_VERSION_BUILD}")
   endif()
 else()
   if(NOT DEFINED NEO_VERSION_MAJOR)
@@ -74,6 +77,5 @@ else()
   if(NOT DEFINED NEO_VERSION_BUILD)
     set(NEO_VERSION_BUILD 0)
   endif()
+  set(NEO_DRIVER_VERSION "${NEO_VERSION_MAJOR}.${NEO_VERSION_MINOR}.${NEO_VERSION_BUILD}")
 endif(UNIX)
-
-set(NEO_DRIVER_VERSION "${NEO_VERSION_MAJOR}.${NEO_VERSION_MINOR}.${NEO_VERSION_BUILD}")
