623c0cecf4df4f4319b547590ebc033aecabaa14
Venevtsev, Igor <igor.venevtsev@intel.com>
2018-08-30 16:23:37 +0200

    Fix for aub tests run
    Always create correct run_{gen}_aub_tests
    targets but do not build them while
    DO_NOT_RUN_AUB_TESTS is specified
    
    Change-Id: Iea6d48169e17b4c3391f5fe4a6fab3a7e426e133

--- unit_tests/aub_tests/common.cmake
+++ unit_tests/aub_tests/common.cmake
@@ -18,24 +18,29 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 function(neo_run_aub_target gen gen_name product slices subslices eu_per_ss)
-    if(NOT DO_NOT_RUN_AUB_TESTS)
-        add_custom_command(
-            TARGET run_${gen}_aub_tests
-            POST_BUILD
-            COMMAND WORKING_DIRECTORY ${TargetDir}
-            COMMAND echo re-creating working directory for ${gen_name} AUBs generation...
-            COMMAND ${CMAKE_COMMAND} -E remove_directory ${TargetDir}/${gen}_aub
-            COMMAND ${CMAKE_COMMAND} -E make_directory ${TargetDir}/${gen}_aub
-            COMMAND ${CMAKE_COMMAND} -E make_directory ${TargetDir}/${gen}_aub/aub_out
-            COMMAND ${CMAKE_COMMAND} -E make_directory ${TargetDir}/${gen}_aub/cl_cache
-        )
+    add_custom_command(
+        TARGET run_${gen}_aub_tests
+        POST_BUILD
+        COMMAND WORKING_DIRECTORY ${TargetDir}
+        COMMAND echo re-creating working directory for ${gen_name} AUBs generation...
+        COMMAND ${CMAKE_COMMAND} -E remove_directory ${TargetDir}/${gen}_aub
+        COMMAND ${CMAKE_COMMAND} -E make_directory ${TargetDir}/${gen}_aub
+        COMMAND ${CMAKE_COMMAND} -E make_directory ${TargetDir}/${gen}_aub/aub_out
+        COMMAND ${CMAKE_COMMAND} -E make_directory ${TargetDir}/${gen}_aub/cl_cache
+    )
 
-        add_custom_command(
-            TARGET run_${gen}_aub_tests
-            POST_BUILD
-            COMMAND WORKING_DIRECTORY ${TargetDir}
-            COMMAND echo Running AUB generation for ${gen_name} in ${TargetDir}/${gen}_aub
-            COMMAND igdrcl_aub_tests --product ${product} --slices ${slices} --subslices ${subslices} --eu_per_ss ${eu_per_ss} --gtest_repeat=1 ${IGDRCL_TESTS_LISTENER_OPTION}
+    add_custom_command(
+        TARGET run_${gen}_aub_tests
+        POST_BUILD
+        COMMAND WORKING_DIRECTORY ${TargetDir}
+        COMMAND echo Running AUB generation for ${gen_name} in ${TargetDir}/${gen}_aub
+        COMMAND igdrcl_aub_tests --product ${product} --slices ${slices} --subslices ${subslices} --eu_per_ss ${eu_per_ss} --gtest_repeat=1 ${IGDRCL_TESTS_LISTENER_OPTION}
+    )
+
+    if(DO_NOT_RUN_AUB_TESTS)
+        set_target_properties(run_${gen}_aub_tests PROPERTIES
+            EXCLUDE_FROM_DEFAULT_BUILD TRUE
+            EXCLUDE_FROM_ALL TRUE
         )
     endif()
 endfunction()
