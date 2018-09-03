b1d020485a7d1f7105a8c9db84c0fc12ff0a0e68
Jacek Danecki <jacek.danecki@intel.com>
2018-09-03 12:47:41 +0200

    remove directories from rpm package
    - to fix conflicts with filesystem package on Centos 7
    
    https://github.com/intel/compute-runtime/pull/80
    
    Change-Id: I715e76058815f0b12b2c7f8fab6d56b35f776a9f
    Signed-off-by: Jacek Danecki <jacek.danecki@intel.com>

--- package.cmake
+++ package.cmake
@@ -81,7 +81,7 @@ if(UNIX)
   endif()
 
   set(CPACK_SET_DESTDIR TRUE)
-  set(CPACK_PACKAGE_RELOCATABLE TRUE)
+  set(CPACK_PACKAGE_RELOCATABLE FALSE)
   set(CPACK_PACKAGE_NAME "intel-opencl")
   set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Intel OpenCL GPU driver")
   set(CPACK_PACKAGE_VENDOR "Intel")
@@ -107,7 +107,11 @@ if(UNIX)
   set(CPACK_RPM_COMPONENT_INSTALL ON)
   set(CPACK_ARCHIVE_COMPONENT_INSTALL ON)
   set(CPACK_COMPONENTS_ALL igdrcl)
-
+  set(CPACK_RPM_EXCLUDE_FROM_AUTO_FILELIST_ADDITION
+    /etc/ld.so.conf.d
+    /usr/local
+    /usr/local/lib64
+  )
   include(CPack)
 
 endif(UNIX)
