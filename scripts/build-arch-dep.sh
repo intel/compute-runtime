40146291adae97d30b9c251e5c61d26ca949e47a
Artur Harasimiuk <artur.harasimiuk@intel.com>
2018-09-18 09:11:08 +0200

    Update copyright headers
    Updating files modified in 2018 only. Older files remain with old style
    copyright header
    
    Change-Id: Ic99f2e190ad74b4b7f2bd79dd7b9fa5fbe36ec92
    Signed-off-by: Artur Harasimiuk <artur.harasimiuk@intel.com>

--- scripts/build-arch-dep.sh
+++ scripts/build-arch-dep.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
-# Copyright (c) 2018, Intel Corporation
 #
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
+# Copyright (C) 2018 Intel Corporation
 #
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
+# SPDX-License-Identifier: MIT
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-# OTHER DEALINGS IN THE SOFTWARE.
 
 wget https://aur.archlinux.org/cgit/aur.git/snapshot/ncurses5-compat-libs.tar.gz
 tar -xzf ncurses5-compat-libs.tar.gz
