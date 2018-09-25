52090c4c4ae06f2739a2c65972544b215a6ceb84
Jacek Danecki <jacek.danecki@intel.com>
2018-09-25 17:25:40 +0200

    ci: use LP ocl-dev account for CI builds on Ubuntu and Arch
    Signed-off-by: Jacek Danecki <jacek.danecki@intel.com>

--- scripts/prepare-arch-igc-ppa.sh
+++ scripts/prepare-arch-igc-ppa.sh
@@ -5,18 +5,18 @@
 # SPDX-License-Identifier: MIT
 #
 
-IGC_VER=18.33.809-1
+IGC_VER=18.38.907-1
 
 cd /root
 
 install_libs() {
-    wget https://launchpad.net/~intel-opencl/+archive/ubuntu/intel-opencl/+files/$1
+    wget https://launchpad.net/~ocl-dev/+archive/ubuntu/intel-opencl/+files/$1
     ar -x $1
     tar -xJf data.tar.xz
     rm -f data.tar.xz $1
 }
 
-install_libs intel-opencl-clang_4.0.15-1~ppa1~bionic1_amd64.deb
+install_libs intel-opencl-clang_4.0.16-1~ppa1~bionic1_amd64.deb
 install_libs intel-igc-core_${IGC_VER}~ppa1~bionic1_amd64.deb
 install_libs intel-igc-opencl-dev_${IGC_VER}~ppa1~bionic1_amd64.deb
 install_libs intel-igc-opencl_${IGC_VER}~ppa1~bionic1_amd64.deb
