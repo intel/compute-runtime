840dd305b722439c9cc085a1470b020d29a4d779
Napiatek, Henryk J <henryk.j.napiatek@intel.com>
2018-10-16 11:58:36 +0200

    Fix bug in setup of Windows unit level tests
    The fix allows repetitive execution of tests under Windows.
    
    Change-Id: Ie65ed5af76f550c0a48fcef88c5824e503614e5d

--- unit_tests/global_environment.cpp
+++ unit_tests/global_environment.cpp
@@ -1,23 +1,8 @@
 /*
- * Copyright (c) 2017, Intel Corporation
+ * Copyright (C) 2017-2018 Intel Corporation
  *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
+ * SPDX-License-Identifier: MIT
  *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #include "global_environment.h"
@@ -26,7 +11,7 @@
 
 TestEnvironment::TestEnvironment(void)
     : libraryFrontEnd(nullptr), libraryIGC(nullptr),
-      libraryOS(nullptr) {
+      libraryOS(nullptr), hwInfoDefaultDebugVars() {
     igcDebugVarStack.reserve(3);
     fclDebugVarStack.reserve(3);
 }
@@ -36,6 +21,9 @@ void TestEnvironment::SetUp() {
 
     fclPushDebugVars(fclDefaultDebugVars);
     igcPushDebugVars(igcDefaultDebugVars);
+    if (libraryOS == nullptr) {
+        libraryOS = setAdapterInfo(hwInfoDefaultDebugVars.pPlatform, hwInfoDefaultDebugVars.pSysInfo);
+    }
 }
 
 void TestEnvironment::TearDown() {
@@ -76,8 +64,7 @@ void TestEnvironment::setDefaultDebugVars(
     HardwareInfo &hwInfo) {
     fclDefaultDebugVars = fclDefaults;
     igcDefaultDebugVars = igcDefaults;
-
-    libraryOS = setAdapterInfo(hwInfo.pPlatform, hwInfo.pSysInfo);
+    hwInfoDefaultDebugVars = hwInfo;
 }
 
 void TestEnvironment::setMockFileNames(
