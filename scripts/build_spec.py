337d374bc56f1c17889271303d83c33cba4c5d11
Artur Harasimiuk <artur.harasimiuk@intel.com>
2018-11-06 11:36:50 +0100

    fedora/spec update: don't bundle GmmLib into intel-opencl package
    Change-Id: I03243d0c7ca321aff23d514ff577d30827e6a4d1
    Signed-off-by: Artur Harasimiuk <artur.harasimiuk@intel.com>

--- scripts/build_spec.py
+++ scripts/build_spec.py
@@ -6,7 +6,7 @@
 #
 
 # Usage:
-# ./scripts/build_spec.py manifests/manifest.yml scripts/fedora.spec.in <version> <revision>
+# ./scripts/build_spec.py scripts/fedora.spec.in <version> <revision>
 #
 
 import datetime
@@ -15,37 +15,26 @@ import re
 import sys
 import yaml
 
-if len(sys.argv) < 5:
+if len(sys.argv) < 4:
     print "ERROR! invalid number of parameters"
     print
     print "Usage:"
-    print "  ./scripts/build_spec.py <manifest> <spec.in> <version> <revision>"
+    print "  ./scripts/build_spec.py <spec.in> <version> <revision>"
     print
     sys.exit(1)
-
-manifest_file = sys.argv[1]
 
 repo = git.Repo(".")
 neo_revision = repo.head.commit
 
-with open(manifest_file, 'r') as f:
-    manifest = yaml.load(f)
-
-gmmlib_revision = manifest['components']['gmmlib']['revision']
-
 c = repo.commit(neo_revision)
 cd = datetime.datetime.fromtimestamp(c.committed_date)
 
-pkg_version = "%s.%s.%s" %(str(cd.isocalendar()[0])[-2:], cd.isocalendar()[1], sys.argv[3])
+pkg_version = "%s.%s.%s" %(str(cd.isocalendar()[0])[-2:], cd.isocalendar()[1], sys.argv[2])
 
-with open(sys.argv[2], 'r') as f:
+with open(sys.argv[1], 'r') as f:
     for line in f.readlines():
         if not re.match(".*__NEO_COMMIT_ID__$", line.strip()) is None:
             print "%s" % (line.rstrip().replace("__NEO_COMMIT_ID__", "%s" % neo_revision))
-            continue
-
-        if not re.match(".*__GMMLIB_COMMIT_ID__$", line.strip()) is None:
-            print "%s" % (line.rstrip().replace("__GMMLIB_COMMIT_ID__", "%s" % gmmlib_revision))
             continue
 
         if not re.match(".*__NEO_PACKAGE_VERSION__$", line.strip()) is None:
@@ -53,7 +42,7 @@ with open(sys.argv[2], 'r') as f:
             continue
 
         if not re.match(".*__NEO_PACKAGE_RELEASE__.*", line.strip()) is None:
-            print "%s" % (line.rstrip().replace("__NEO_PACKAGE_RELEASE__", "%s" % sys.argv[4]))
+            print "%s" % (line.rstrip().replace("__NEO_PACKAGE_RELEASE__", "%s" % sys.argv[3]))
             continue
 
         print line.rstrip()
