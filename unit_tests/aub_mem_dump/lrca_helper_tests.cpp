e5a8616d266e1e53ab5d06e2a9dba8f2051d2cbf
Hoppe, Mateusz <mateusz.hoppe@intel.com>
2018-11-13 10:11:10 +0100

    Test Lrca initialization
    - debug mode LRI must be present
    
    Change-Id: Idd9f9a85b9610177cb7ca378e37178ce333aa8b2

---
+++ unit_tests/aub_mem_dump/lrca_helper_tests.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include "runtime/aub_mem_dump/aub_mem_dump.h"
+#include "test.h"
+
+#include <memory>
+
+using namespace AubMemDump;
+
+TEST(LrcaHelper, WhenLrcaHelperIsInitializedThenLrcaIncludesDebugModeLri) {
+    LrcaHelper helper(0x2000);
+
+    auto lrcaBufferSize = helper.sizeLRCA / sizeof(uint32_t);
+    auto lrca = std::unique_ptr<uint32_t[]>(new uint32_t[lrcaBufferSize]);
+    helper.initialize(reinterpret_cast<void *>(lrca.get()));
+
+    bool debugModeLriFound = false;
+    for (uint32_t i = 0; i < lrcaBufferSize; i += 2) {
+        if (lrca[i] == 0x20d8 && lrca[i + 1] == 0x00200020) {
+            debugModeLriFound = true;
+            break;
+        }
+    }
+
+    EXPECT_TRUE(debugModeLriFound);
+}
