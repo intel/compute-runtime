1ff92d51dcec5ea06bc21a78ae4854306f5cbcee
Dunajski, Bartosz <bartosz.dunajski@intel.com>
2018-11-16 14:09:42 +0100

    Initial implementation of PCH for Windows
    Change-Id: Ib083e3dac003a75ae67d2e2747dde6dd0e6ff92d
    Signed-off-by: Dunajski, Bartosz <bartosz.dunajski@intel.com>

---
+++ unit_tests/igdrcl_tests_pch.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include "igdrcl_tests_pch.h"
---
+++ unit_tests/igdrcl_tests_pch.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include "runtime/gen_common/hw_cmds.h"
+#include "runtime/gmm_helper/gmm_lib.h"
+#include "runtime/helpers/abort.h"
+#include "runtime/helpers/completion_stamp.h"
+#include "runtime/helpers/debug_helpers.h"
+#include "runtime/helpers/kmd_notify_properties.h"
+#include "runtime/helpers/hw_info.h"
+#include "runtime/helpers/ptr_math.h"
+#include "runtime/memory_manager/memory_constants.h"
+#include "runtime/sku_info/sku_info_base.h"
+
+#include "third_party/opencl_headers/CL/cl.h"
+
+#include "unit_tests/gen_common/gen_cmd_parse.h"
+#include "test.h"
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <set>
+#include <stdio.h>
+#include <string>
+#include <vector>
