7170c1e2b444d77e87b2e1717f13f98cc3b4c1ac
Jacek Danecki <jacek.danecki@intel.com>
2018-11-16 10:41:57 +0100

    update Arch builds
    Use igc and gmmlib packages versions from fedora.spec.in to download
    deb packages from launchpad.net.
    
    Signed-off-by: Jacek Danecki <jacek.danecki@intel.com>

--- scripts/prepare-arch-dep-ppa.sh
+++ scripts/prepare-arch-dep-ppa.sh
@@ -5,9 +5,12 @@
 # SPDX-License-Identifier: MIT
 #
 
-IGC_VER=18.38.907-1
-
 cd /root
+IGC_VER="`grep igc_version_required neo/scripts/fedora.spec.in | grep global | awk '{print $3}'`-1"
+GMM_VER="`grep gmmlib_version_required neo/scripts/fedora.spec.in | grep global | awk '{print $3}'`-1"
+
+echo "IGC_VER=${IGC_VER}"
+echo "GMM_VER=${GMM_VER}"
 
 install_libs() {
     wget https://launchpad.net/~ocl-dev/+archive/ubuntu/intel-opencl/+files/$1
@@ -20,9 +23,10 @@ install_libs intel-opencl-clang_4.0.16-1~ppa1~bionic1_amd64.deb
 install_libs intel-igc-core_${IGC_VER}~ppa1~bionic1_amd64.deb
 install_libs intel-igc-opencl-dev_${IGC_VER}~ppa1~bionic1_amd64.deb
 install_libs intel-igc-opencl_${IGC_VER}~ppa1~bionic1_amd64.deb
+install_libs intel-gmmlib_${GMM_VER}~ppa1~bionic1_amd64.deb
+install_libs intel-gmmlib-dev_${GMM_VER}~ppa1~bionic1_amd64.deb
 
 cp -ar usr /
-ln -sf /usr/lib/x86_64-linux-gnu/*.so /usr/lib64
+ln -sf /usr/lib/x86_64-linux-gnu/*.so* /usr/lib64
 ln -s /usr/lib/x86_64-linux-gnu/pkgconfig/*.pc /usr/lib64/pkgconfig
 mkdir /root/build
-
