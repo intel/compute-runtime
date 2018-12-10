09505ce85312533f8b3c8cb19ca8b6c9dca7a590
Milczarek, Slawomir <slawomir.milczarek@intel.com>
2018-12-10 11:52:16 +0100

    Updated AubStream header with hardware context interface
    Change-Id: Iff5890e36d4dc0a46f7bb210404d4ff010ab9083

--- third_party/aub_stream/headers/hardware_context.h
+++ third_party/aub_stream/headers/hardware_context.h
@@ -15,7 +15,7 @@ struct HardwareContext {
     virtual void initialize() = 0;
     virtual void pollForCompletion() = 0;
     virtual void submit(uint64_t gfxAddress, const void *batchBuffer, size_t size, uint32_t memoryBank) = 0;
-    virtual void writeMemory(uint64_t gfxAddress, const void *memory, size_t size, uint32_t memoryBanks, int hint, size_t pageSize = 4096) = 0;
+    virtual void writeMemory(uint64_t gfxAddress, const void *memory, size_t size, uint32_t memoryBanks, int hint, size_t pageSize = 65536) = 0;
     virtual void freeMemory(uint64_t gfxAddress, size_t size) = 0;
     virtual void expectMemory(uint64_t gfxAddress, const void *memory, size_t size, uint32_t compareOperation) = 0;
     virtual ~HardwareContext() = default;
