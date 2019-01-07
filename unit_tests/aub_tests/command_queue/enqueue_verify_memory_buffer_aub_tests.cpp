2540e37c60ba832d2b2d33176f490e9e4f5b2c22
Filip Hazubski <filip.hazubski@intel.com>
2019-01-07 14:33:49 +0100

    Update test cases for clEnqueueVerifyMemory
    Change-Id: Ie166bb11e6e9ea385e94e9c98c8aa871e1c9148b
    Signed-off-by: Filip Hazubski <filip.hazubski@intel.com>

--- unit_tests/aub_tests/command_queue/enqueue_verify_memory_buffer_aub_tests.cpp
+++ unit_tests/aub_tests/command_queue/enqueue_verify_memory_buffer_aub_tests.cpp
@@ -17,16 +17,9 @@
 
 using namespace OCLRT;
 
-struct TestDataItem {
-    uint8_t *item;
-    uint8_t *itemWrongSmaller;
-    uint8_t *itemWrongGreater;
-    size_t size;
-};
-
 struct VerifyMemoryBufferHw
     : public CommandEnqueueAUBFixture,
-      public ::testing::TestWithParam<std::tuple<TestDataItem, size_t, uint64_t>> {
+      public ::testing::TestWithParam<std::tuple<size_t, uint64_t>> {
 
     void SetUp() override {
         CommandEnqueueAUBFixture::SetUp();
@@ -37,109 +30,92 @@ struct VerifyMemoryBufferHw
     }
 };
 
-cl_int testClInt = 5;
-cl_int testClIntSmaller = 4;
-cl_int testClIntGreater = 6;
-cl_float testClFloat = 20.0;
-cl_float testClFloatSmaller = 19.5;
-cl_float testClFloatGreater = 20.5;
-
-TestDataItem testDataItemTable[] = {
-    {reinterpret_cast<uint8_t *>(&testClInt),
-     reinterpret_cast<uint8_t *>(&testClIntSmaller),
-     reinterpret_cast<uint8_t *>(&testClIntGreater),
-     sizeof(testClInt)},
-    {reinterpret_cast<uint8_t *>(&testClFloat),
-     reinterpret_cast<uint8_t *>(&testClFloatSmaller),
-     reinterpret_cast<uint8_t *>(&testClFloatGreater),
-     sizeof(testClFloat)}};
-
 size_t testDataSizeTable[] = {
     16,
-    MemoryConstants::cacheLineSize};
+    MemoryConstants::megaByte};
 
 cl_mem_flags testFlagsTable[] = {
     0,
     CL_MEM_COPY_HOST_PTR};
 
 HWTEST_P(VerifyMemoryBufferHw, givenDifferentBuffersWhenValidatingMemoryThenSuccessIsReturned) {
-    const auto &testDataItem = std::get<0>(GetParam());
-    const auto &testDataSize = std::get<1>(GetParam());
-    EXPECT_FALSE(testDataSize < testDataItem.size);
-    const auto &flags = std::get<2>(GetParam());
+    cl_uint testItem = 5;
+    cl_uint testItemWrong1 = 4;
+    cl_uint testItemWrong2 = 6;
+    auto testItemSize = sizeof(testItem);
+
+    const auto &testDataSize = std::get<0>(GetParam());
+    EXPECT_FALSE(testDataSize < testItemSize);
+    const auto &flags = std::get<1>(GetParam());
     const auto usesHostPointer = ((flags & CL_MEM_USE_HOST_PTR) ||
                                   (flags & CL_MEM_COPY_HOST_PTR));
 
     DebugManagerStateRestore restore;
     DebugManager.flags.DisableZeroCopyForBuffers.set(true);
 
-    // for each buffer size check also one object smaller and one object greater
-    for (auto dataSize = testDataSize - testDataItem.size;
-         dataSize <= testDataSize + testDataItem.size;
-         dataSize += testDataItem.size) {
+    std::unique_ptr<uint8_t[]> bufferContent(new uint8_t[testDataSize]);
+    std::unique_ptr<uint8_t[]> validContent(new uint8_t[testDataSize]);
+    std::unique_ptr<uint8_t[]> invalidContent1(new uint8_t[testDataSize]);
+    std::unique_ptr<uint8_t[]> invalidContent2(new uint8_t[testDataSize]);
 
-        std::unique_ptr<uint8_t[]> bufferContent(new uint8_t[dataSize]);
-        std::unique_ptr<uint8_t[]> validContent(new uint8_t[dataSize]);
-        std::unique_ptr<uint8_t[]> invalidContent1(new uint8_t[dataSize]);
-        std::unique_ptr<uint8_t[]> invalidContent2(new uint8_t[dataSize]);
-
-        for (size_t offset = 0; offset < dataSize; offset += testDataItem.size) {
-            for (size_t itemOffset = 0; itemOffset < testDataItem.size; itemOffset++) {
-                bufferContent.get()[offset + itemOffset] = testDataItem.item[itemOffset];
-                validContent.get()[offset + itemOffset] = testDataItem.item[itemOffset];
-                invalidContent1.get()[offset + itemOffset] = testDataItem.item[itemOffset];
-                invalidContent2.get()[offset + itemOffset] = testDataItem.item[itemOffset];
-            }
+    auto pTestItem = reinterpret_cast<uint8_t *>(&testItem);
+    for (size_t offset = 0; offset < testDataSize; offset += testItemSize) {
+        for (size_t itemOffset = 0; itemOffset < testItemSize; itemOffset++) {
+            bufferContent.get()[offset + itemOffset] = pTestItem[itemOffset];
+            validContent.get()[offset + itemOffset] = pTestItem[itemOffset];
+            invalidContent1.get()[offset + itemOffset] = pTestItem[itemOffset];
+            invalidContent2.get()[offset + itemOffset] = pTestItem[itemOffset];
         }
+    }
 
-        // set last item for invalid contents
-        size_t offset = dataSize - testDataItem.size;
-        for (size_t itemOffset = 0; itemOffset < testDataItem.size; itemOffset++) {
-            invalidContent1.get()[offset + itemOffset] = testDataItem.itemWrongSmaller[itemOffset];
-            invalidContent2.get()[offset + itemOffset] = testDataItem.itemWrongGreater[itemOffset];
-        }
+    // set last item for invalid contents
+    auto pTestItemWrong1 = reinterpret_cast<uint8_t *>(&testItemWrong1);
+    auto pTestItemWrong2 = reinterpret_cast<uint8_t *>(&testItemWrong2);
+    size_t offset = testDataSize - testItemSize;
+    for (size_t itemOffset = 0; itemOffset < testItemSize; itemOffset++) {
+        invalidContent1.get()[offset + itemOffset] = pTestItemWrong1[itemOffset];
+        invalidContent2.get()[offset + itemOffset] = pTestItemWrong2[itemOffset];
+    }
 
-        MockContext context(&this->pCmdQ->getDevice());
-        cl_int retVal = CL_INVALID_VALUE;
+    MockContext context(&this->pCmdQ->getDevice());
+    cl_int retVal = CL_INVALID_VALUE;
 
-        std::unique_ptr<Buffer> buffer(Buffer::create(
-            &context,
-            flags,
-            dataSize,
-            (usesHostPointer ? bufferContent.get() : nullptr),
-            retVal));
-        EXPECT_NE(nullptr, buffer);
+    std::unique_ptr<Buffer> buffer(Buffer::create(
+        &context,
+        flags,
+        testDataSize,
+        (usesHostPointer ? bufferContent.get() : nullptr),
+        retVal));
+    EXPECT_NE(nullptr, buffer);
 
-        if (!usesHostPointer) {
-            retVal = pCmdQ->enqueueFillBuffer(
-                buffer.get(),
-                testDataItem.item,
-                testDataItem.size,
-                0,
-                dataSize,
-                0,
-                nullptr,
-                nullptr);
-            EXPECT_EQ(CL_SUCCESS, retVal);
-        }
-
-        auto mappedAddress = clEnqueueMapBuffer(pCmdQ, buffer.get(), CL_TRUE, CL_MAP_READ, 0, dataSize, 0, nullptr, nullptr, nullptr);
-
-        retVal = clEnqueueVerifyMemory(pCmdQ, mappedAddress, validContent.get(), dataSize, CL_MEM_COMPARE_EQUAL);
+    if (!usesHostPointer) {
+        retVal = pCmdQ->enqueueFillBuffer(
+            buffer.get(),
+            &testItem,
+            testItemSize,
+            0,
+            testDataSize,
+            0,
+            nullptr,
+            nullptr);
         EXPECT_EQ(CL_SUCCESS, retVal);
+    }
 
-        if (UnitTestHelper<FamilyType>::isExpectMemoryNotEqualSupported()) {
-            retVal = clEnqueueVerifyMemory(pCmdQ, mappedAddress, invalidContent1.get(), dataSize, CL_MEM_COMPARE_NOT_EQUAL);
-            EXPECT_EQ(CL_SUCCESS, retVal);
-            retVal = clEnqueueVerifyMemory(pCmdQ, mappedAddress, invalidContent2.get(), dataSize, CL_MEM_COMPARE_NOT_EQUAL);
-            EXPECT_EQ(CL_SUCCESS, retVal);
-        }
+    auto mappedAddress = clEnqueueMapBuffer(pCmdQ, buffer.get(), CL_TRUE, CL_MAP_READ, 0, testDataSize, 0, nullptr, nullptr, nullptr);
+
+    retVal = clEnqueueVerifyMemory(pCmdQ, mappedAddress, validContent.get(), testDataSize, CL_MEM_COMPARE_EQUAL);
+    EXPECT_EQ(CL_SUCCESS, retVal);
+
+    if (UnitTestHelper<FamilyType>::isExpectMemoryNotEqualSupported()) {
+        retVal = clEnqueueVerifyMemory(pCmdQ, mappedAddress, invalidContent1.get(), testDataSize, CL_MEM_COMPARE_NOT_EQUAL);
+        EXPECT_EQ(CL_SUCCESS, retVal);
+        retVal = clEnqueueVerifyMemory(pCmdQ, mappedAddress, invalidContent2.get(), testDataSize, CL_MEM_COMPARE_NOT_EQUAL);
+        EXPECT_EQ(CL_SUCCESS, retVal);
     }
 }
 
 INSTANTIATE_TEST_CASE_P(VerifyMemoryBuffer,
                         VerifyMemoryBufferHw,
                         ::testing::Combine(
-                            ::testing::ValuesIn(testDataItemTable),
                             ::testing::ValuesIn(testDataSizeTable),
                             ::testing::ValuesIn(testFlagsTable)));
