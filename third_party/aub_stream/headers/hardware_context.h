537697dcfd90242131d73bd69b0a0030e3ac84a1
Mrozek, Michal <michal.mrozek@intel.com>
2019-01-10 08:51:49 +0100

    Update interfaces.
    Change-Id: Ib038a98f51fdc8850ac5ee7d922c399535682081

--- third_party/aub_stream/headers/hardware_context.h
+++ third_party/aub_stream/headers/hardware_context.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Intel Corporation
+ * Copyright (C) 2018-2019 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
@@ -14,7 +14,7 @@ namespace AubDump {
 struct HardwareContext {
     virtual void initialize() = 0;
     virtual void pollForCompletion() = 0;
-    virtual void submit(uint64_t gfxAddress, const void *batchBuffer, size_t size, uint32_t memoryBank) = 0;
+    virtual void submit(uint64_t gfxAddress, const void *batchBuffer, size_t size, uint32_t memoryBank, size_t pageSize = 65536) = 0;
     virtual void writeMemory(uint64_t gfxAddress, const void *memory, size_t size, uint32_t memoryBanks, int hint, size_t pageSize = 65536) = 0;
     virtual void freeMemory(uint64_t gfxAddress, size_t size) = 0;
     virtual void expectMemory(uint64_t gfxAddress, const void *memory, size_t size, uint32_t compareOperation) = 0;
--- unit_tests/mocks/mock_aub_manager.h
+++ unit_tests/mocks/mock_aub_manager.h
@@ -16,8 +16,8 @@ struct MockHardwareContext : public HardwareContext {
 
     void initialize() override { initializeCalled = true; }
     void pollForCompletion() override { pollForCompletionCalled = true; };
-    void submit(uint64_t gfxAddress, const void *batchBuffer, size_t size, uint32_t memoryBank) override { submitCalled = true; }
-    void writeMemory(uint64_t gfxAddress, const void *memory, size_t size, uint32_t memoryBanks, int hint, size_t pageSize = 4096) override { writeMemoryCalled = true; }
+    void submit(uint64_t gfxAddress, const void *batchBuffer, size_t size, uint32_t memoryBank, size_t pageSize = 65536) override { submitCalled = true; }
+    void writeMemory(uint64_t gfxAddress, const void *memory, size_t size, uint32_t memoryBanks, int hint, size_t pageSize = 65536) override { writeMemoryCalled = true; }
     void freeMemory(uint64_t gfxAddress, size_t size) override { freeMemoryCalled = true; }
     void expectMemory(uint64_t gfxAddress, const void *memory, size_t size, uint32_t compareOperation) override { expectMemoryCalled = true; }
     void readMemory(uint64_t gfxAddress, void *memory, size_t size) override { readMemoryCalled = true; };
