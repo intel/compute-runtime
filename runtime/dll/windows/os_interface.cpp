3a5209ccf42e8922dbc0d41013ff918e51829de0
Hoppe, Mateusz <mateusz.hoppe@intel.com>
2019-01-25 13:49:29 +0100

    Disable osEnableLocalMemory on 32 bits
    Change-Id: Id405a57064d17293ede48f82992ab1bde4f3b9ce

--- runtime/dll/windows/os_interface.cpp
+++ runtime/dll/windows/os_interface.cpp
@@ -1,14 +1,14 @@
 /*
- * Copyright (C) 2018 Intel Corporation
+ * Copyright (C) 2018-2019 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
  */
 
+#include "runtime/memory_manager/memory_constants.h"
 #include "runtime/os_interface/windows/os_interface.h"
-
 namespace OCLRT {
 
-bool OSInterface::osEnableLocalMemory = true;
+bool OSInterface::osEnableLocalMemory = true && is64bit;
 
 } // namespace OCLRT
--- unit_tests/windows/os_interface_tests.cpp
+++ unit_tests/windows/os_interface_tests.cpp
@@ -1,13 +1,14 @@
 /*
- * Copyright (C) 2018 Intel Corporation
+ * Copyright (C) 2018-2019 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
  */
 
+#include "runtime/memory_manager/memory_constants.h"
 #include "runtime/os_interface/windows/os_interface.h"
 #include "test.h"
 
 TEST(osInterfaceTests, osInterfaceLocalMemoryEnabledByDefault) {
-    EXPECT_TRUE(OCLRT::OSInterface::osEnableLocalMemory);
+    EXPECT_EQ(is64bit, OCLRT::OSInterface::osEnableLocalMemory);
 }
