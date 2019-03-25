c5ddf11e78bf5d1c59c566a33d5f2f7760a66cfb
Mateusz Hoppe <mateusz.hoppe@intel.com>
2019-03-25 19:22:52 +0100

    Use GPU address in EnqueueCopyBufferToImageTest
    Change-Id: I2724fb9f2cac9222cacfff913e648df1d1531ccd
    Signed-off-by: Mateusz Hoppe <mateusz.hoppe@intel.com>

--- unit_tests/command_queue/enqueue_copy_buffer_to_image_tests.cpp
+++ unit_tests/command_queue/enqueue_copy_buffer_to_image_tests.cpp
@@ -171,7 +171,7 @@ HWTEST_F(EnqueueCopyBufferToImageTest, surfaceState) {
     EXPECT_TRUE(isRedescribedFormat);
     EXPECT_EQ(RENDER_SURFACE_STATE::SURFACE_HORIZONTAL_ALIGNMENT_HALIGN_4, surfaceState.getSurfaceHorizontalAlignment());
     EXPECT_EQ(RENDER_SURFACE_STATE::SURFACE_VERTICAL_ALIGNMENT_VALIGN_4, surfaceState.getSurfaceVerticalAlignment());
-    EXPECT_EQ(reinterpret_cast<uint64_t>(dstImage->getCpuAddress()), surfaceState.getSurfaceBaseAddress());
+    EXPECT_EQ(dstImage->getGraphicsAllocation()->getGpuAddress(), surfaceState.getSurfaceBaseAddress());
 }
 
 HWTEST_F(EnqueueCopyBufferToImageTest, pipelineSelect) {
