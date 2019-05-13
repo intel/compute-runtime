c86d9f4bd52775a6fcc37a1c9372e653a2c35453
Hoppe, Mateusz <mateusz.hoppe@intel.com>
2019-05-13 13:01:06 +0200

    Detect memory leaks only for tests that PASSED
    - with this change GTEST_SKIP() can be used without
    false-positive leak detections
    
    Related-To: NEO-3173
    
    Change-Id: If12ccb7a6f21811c9966638db93f097cab928b66
    Signed-off-by: Hoppe, Mateusz <mateusz.hoppe@intel.com>

--- unit_tests/memory_leak_listener.cpp
+++ unit_tests/memory_leak_listener.cpp
@@ -29,20 +29,22 @@ void MemoryLeakListener::OnTestEnd(const TestInfo &testInfo) {
     MemoryManagement::fastLeakDetectionEnabled = false;
     EXPECT_EQ(numInitialBaseObjects, numBaseObjects);
 
-    if (MemoryManagement::fastLeaksDetectionMode != MemoryManagement::LeakDetectionMode::STANDARD) {
-        if (MemoryManagement::fastLeaksDetectionMode == MemoryManagement::LeakDetectionMode::EXPECT_TO_LEAK) {
-            EXPECT_GT(MemoryManagement::fastEventsAllocatedCount, MemoryManagement::fastEventsDeallocatedCount);
+    if (testInfo.result()->Passed()) {
+        if (MemoryManagement::fastLeaksDetectionMode != MemoryManagement::LeakDetectionMode::STANDARD) {
+            if (MemoryManagement::fastLeaksDetectionMode == MemoryManagement::LeakDetectionMode::EXPECT_TO_LEAK) {
+                EXPECT_GT(MemoryManagement::fastEventsAllocatedCount, MemoryManagement::fastEventsDeallocatedCount);
+            }
+            MemoryManagement::fastLeaksDetectionMode = MemoryManagement::LeakDetectionMode::STANDARD;
+        } else if (NEO::captureCallStacks && (MemoryManagement::fastEventsAllocatedCount != MemoryManagement::fastEventsDeallocatedCount)) {
+            auto leak = MemoryManagementFixture::enumerateLeak(MemoryManagement::indexAllocation.load(), MemoryManagement::indexDeallocation.load(), true, true);
+            if (leak != MemoryManagement::failingAllocation) {
+                printf("\n %s ", printCallStack(MemoryManagement::eventsAllocated[leak]).c_str());
+            }
+            EXPECT_EQ(MemoryManagement::indexAllocation.load(), MemoryManagement::indexDeallocation.load());
+        } else if (MemoryManagement::fastEventsAllocatedCount != MemoryManagement::fastEventsDeallocatedCount) {
+            auto leak = MemoryManagement::detectLeaks();
+            EXPECT_EQ(leak, (int)MemoryManagement::failingAllocation) << "To locate call stack, change the value of captureCallStacks to true";
         }
-        MemoryManagement::fastLeaksDetectionMode = MemoryManagement::LeakDetectionMode::STANDARD;
-    } else if (NEO::captureCallStacks && (MemoryManagement::fastEventsAllocatedCount != MemoryManagement::fastEventsDeallocatedCount)) {
-        auto leak = MemoryManagementFixture::enumerateLeak(MemoryManagement::indexAllocation.load(), MemoryManagement::indexDeallocation.load(), true, true);
-        if (leak != MemoryManagement::failingAllocation) {
-            printf("\n %s ", printCallStack(MemoryManagement::eventsAllocated[leak]).c_str());
-        }
-        EXPECT_EQ(MemoryManagement::indexAllocation.load(), MemoryManagement::indexDeallocation.load());
-    } else if (MemoryManagement::fastEventsAllocatedCount != MemoryManagement::fastEventsDeallocatedCount) {
-        auto leak = MemoryManagement::detectLeaks();
-        EXPECT_EQ(leak, (int)MemoryManagement::failingAllocation) << "To locate call stack, change the value of captureCallStacks to true";
     }
     MemoryManagement::fastEventsAllocatedCount = 0;
     MemoryManagement::fastEventsDeallocatedCount = 0;
