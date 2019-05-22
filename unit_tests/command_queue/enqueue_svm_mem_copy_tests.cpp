3d3e6b9531ab7c0fef531a37f050c96918731f36
Koska, Andrzej <andrzej.koska@intel.com>
2019-05-22 09:21:22 +0200

    Testing enqueueSVMMemcpy with host pointer
    Related-To: NEO-3011
    Change-Id: I179089e078361dd2449f78e75f1d6edd3f5235de
    Signed-off-by: Koska, Andrzej <andrzej.koska@intel.com>

--- unit_tests/command_queue/enqueue_svm_mem_copy_tests.cpp
+++ unit_tests/command_queue/enqueue_svm_mem_copy_tests.cpp
@@ -123,3 +123,151 @@ HWTEST_F(EnqueueSvmMemCopyTest, givenEnqueueSVMMemcpyWhenUsingCopyBufferToBuffer
     auto kernel = mdi->begin()->getKernel();
     EXPECT_EQ("CopyBufferToBufferMiddle", kernel->getKernelInfo().name);
 }
+
+HWTEST_F(EnqueueSvmMemCopyTest, givenEnqueueSVMMemcpyWhenUsingCopyBufferToBufferBuilderAndSrcHostPtrThenItConfiguredWithBuiltinOpsAndProducesDispatchInfo) {
+    auto &builtIns = *pCmdQ->getDevice().getExecutionEnvironment()->getBuiltIns();
+    void *srcHostPtr = alignedMalloc(256, 64);
+
+    // retrieve original builder
+    auto &origBuilder = builtIns.getBuiltinDispatchInfoBuilder(
+        EBuiltInOps::CopyBufferToBuffer,
+        pCmdQ->getContext(),
+        pCmdQ->getDevice());
+    ASSERT_NE(nullptr, &origBuilder);
+
+    // substitute original builder with mock builder
+    auto oldBuilder = builtIns.setBuiltinDispatchInfoBuilder(
+        EBuiltInOps::CopyBufferToBuffer,
+        pCmdQ->getContext(),
+        pCmdQ->getDevice(),
+        std::unique_ptr<NEO::BuiltinDispatchInfoBuilder>(new MockBuiltinDispatchInfoBuilder(builtIns, &origBuilder)));
+    EXPECT_EQ(&origBuilder, oldBuilder.get());
+
+    // call enqueue on mock builder
+    auto retVal = pCmdQ->enqueueSVMMemcpy(
+        false,      // cl_bool  blocking_copy
+        dstSvmPtr,  // void *dst_ptr
+        srcHostPtr, // const void *src_ptr
+        256,        // size_t size
+        0,          // cl_uint num_events_in_wait_list
+        nullptr,    // cl_event *event_wait_list
+        nullptr     // cL_event *event
+    );
+    EXPECT_EQ(CL_SUCCESS, retVal);
+
+    // restore original builder and retrieve mock builder
+    auto newBuilder = builtIns.setBuiltinDispatchInfoBuilder(
+        EBuiltInOps::CopyBufferToBuffer,
+        pCmdQ->getContext(),
+        pCmdQ->getDevice(),
+        std::move(oldBuilder));
+    EXPECT_NE(nullptr, newBuilder);
+
+    // check if original builder is restored correctly
+    auto &restoredBuilder = builtIns.getBuiltinDispatchInfoBuilder(
+        EBuiltInOps::CopyBufferToBuffer,
+        pCmdQ->getContext(),
+        pCmdQ->getDevice());
+    EXPECT_EQ(&origBuilder, &restoredBuilder);
+
+    // use mock builder to validate builder's input / output
+    auto mockBuilder = static_cast<MockBuiltinDispatchInfoBuilder *>(newBuilder.get());
+
+    // validate builder's input - builtin ops
+    auto params = mockBuilder->getBuiltinOpParams();
+    EXPECT_EQ(srcHostPtr, params->srcPtr);
+    EXPECT_EQ(dstSvmPtr, params->dstPtr);
+    EXPECT_EQ(nullptr, params->srcMemObj);
+    EXPECT_EQ(nullptr, params->dstMemObj);
+    EXPECT_EQ(nullptr, params->srcSvmAlloc);
+    EXPECT_EQ(dstSvmAlloc, params->dstSvmAlloc);
+    EXPECT_EQ(Vec3<size_t>(0, 0, 0), params->srcOffset);
+    EXPECT_EQ(Vec3<size_t>(0, 0, 0), params->dstOffset);
+    EXPECT_EQ(Vec3<size_t>(256, 0, 0), params->size);
+
+    // validate builder's output - multi dispatch info
+    auto mdi = mockBuilder->getMultiDispatchInfo();
+    EXPECT_EQ(1u, mdi->size());
+
+    auto di = mdi->begin();
+    size_t middleElSize = 4 * sizeof(uint32_t);
+    EXPECT_EQ(Vec3<size_t>(256 / middleElSize, 1, 1), di->getGWS());
+
+    auto kernel = mdi->begin()->getKernel();
+    EXPECT_EQ("CopyBufferToBufferMiddle", kernel->getKernelInfo().name);
+    alignedFree(srcHostPtr);
+}
+
+HWTEST_F(EnqueueSvmMemCopyTest, givenEnqueueSVMMemcpyWhenUsingCopyBufferToBufferBuilderAndDstHostPtrThenItConfiguredWithBuiltinOpsAndProducesDispatchInfo) {
+    auto &builtIns = *pCmdQ->getDevice().getExecutionEnvironment()->getBuiltIns();
+    auto dstHostPtr = alignedMalloc(256, 64);
+
+    // retrieve original builder
+    auto &origBuilder = builtIns.getBuiltinDispatchInfoBuilder(
+        EBuiltInOps::CopyBufferToBuffer,
+        pCmdQ->getContext(),
+        pCmdQ->getDevice());
+    ASSERT_NE(nullptr, &origBuilder);
+
+    // substitute original builder with mock builder
+    auto oldBuilder = builtIns.setBuiltinDispatchInfoBuilder(
+        EBuiltInOps::CopyBufferToBuffer,
+        pCmdQ->getContext(),
+        pCmdQ->getDevice(),
+        std::unique_ptr<NEO::BuiltinDispatchInfoBuilder>(new MockBuiltinDispatchInfoBuilder(builtIns, &origBuilder)));
+    EXPECT_EQ(&origBuilder, oldBuilder.get());
+
+    // call enqueue on mock builder
+    auto retVal = pCmdQ->enqueueSVMMemcpy(
+        false,      // cl_bool  blocking_copy
+        dstHostPtr, // void *dst_ptr
+        srcSvmPtr,  // const void *src_ptr
+        256,        // size_t size
+        0,          // cl_uint num_events_in_wait_list
+        nullptr,    // cl_event *event_wait_list
+        nullptr     // cL_event *event
+    );
+    EXPECT_EQ(CL_SUCCESS, retVal);
+
+    // restore original builder and retrieve mock builder
+    auto newBuilder = builtIns.setBuiltinDispatchInfoBuilder(
+        EBuiltInOps::CopyBufferToBuffer,
+        pCmdQ->getContext(),
+        pCmdQ->getDevice(),
+        std::move(oldBuilder));
+    EXPECT_NE(nullptr, newBuilder);
+
+    // check if original builder is restored correctly
+    auto &restoredBuilder = builtIns.getBuiltinDispatchInfoBuilder(
+        EBuiltInOps::CopyBufferToBuffer,
+        pCmdQ->getContext(),
+        pCmdQ->getDevice());
+    EXPECT_EQ(&origBuilder, &restoredBuilder);
+
+    // use mock builder to validate builder's input / output
+    auto mockBuilder = static_cast<MockBuiltinDispatchInfoBuilder *>(newBuilder.get());
+
+    // validate builder's input - builtin ops
+    auto params = mockBuilder->getBuiltinOpParams();
+    EXPECT_EQ(srcSvmPtr, params->srcPtr);
+    EXPECT_EQ(dstHostPtr, params->dstPtr);
+    EXPECT_EQ(nullptr, params->srcMemObj);
+    EXPECT_EQ(nullptr, params->dstMemObj);
+    EXPECT_EQ(srcSvmAlloc, params->srcSvmAlloc);
+    EXPECT_EQ(nullptr, params->dstSvmAlloc);
+    EXPECT_EQ(Vec3<size_t>(0, 0, 0), params->srcOffset);
+    EXPECT_EQ(Vec3<size_t>(0, 0, 0), params->dstOffset);
+    EXPECT_EQ(Vec3<size_t>(256, 0, 0), params->size);
+
+    // validate builder's output - multi dispatch info
+    auto mdi = mockBuilder->getMultiDispatchInfo();
+    EXPECT_EQ(1u, mdi->size());
+
+    auto di = mdi->begin();
+    size_t middleElSize = 4 * sizeof(uint32_t);
+    EXPECT_EQ(Vec3<size_t>(256 / middleElSize, 1, 1), di->getGWS());
+
+    auto kernel = mdi->begin()->getKernel();
+    EXPECT_EQ("CopyBufferToBufferMiddle", kernel->getKernelInfo().name);
+    alignedFree(dstHostPtr);
+}
