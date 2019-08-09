c5f9915acd282552bc6ad595677af950b4d9625e
Mrozek, Michal <michal.mrozek@intel.com>
2019-08-09 12:57:27 +0200

    Remove redundant code.
    Change-Id: I4c4cb5c6a6c316ca3c25260d9bcb60baded3c738
    Signed-off-by: Mrozek, Michal <michal.mrozek@intel.com>

--- runtime/memory_manager/os_agnostic_memory_manager_allocate_in_device_pool.cpp
+++ runtime/memory_manager/os_agnostic_memory_manager_allocate_in_device_pool.cpp
@@ -24,7 +24,6 @@ GraphicsAllocation *OsAgnosticMemoryManager::allocateGraphicsMemoryInDevicePool(
                 }
                 uint64_t gpuAddress = reinterpret_cast<uint64_t>(allocationData.hostPtr);
                 allocation = new MemoryAllocation(allocationData.type, cpuAllocation, cpuAllocation, gpuAddress, allocationData.size, counter++, MemoryPool::LocalMemory, false, false, false);
-                allocation->setGpuBaseAddress(gpuAddress);
             } else {
                 allocation = allocateGraphicsMemory(allocationData);
             }
