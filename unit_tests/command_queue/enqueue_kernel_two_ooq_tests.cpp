439cbff6b71651557980eee33749a76ccf00b864
Pawel Wilma <pawel.wilma@intel.com>
2019-09-25 16:13:23 +0200

    Print additional logs in sporadically failing unit test
    Change-Id: Ib62afef404f018da8c5706c64813347b6dc042d0
    Signed-off-by: Pawel Wilma <pawel.wilma@intel.com>

--- unit_tests/command_queue/enqueue_kernel_two_ooq_tests.cpp
+++ unit_tests/command_queue/enqueue_kernel_two_ooq_tests.cpp
@@ -98,6 +98,15 @@ struct TwoOOQsTwoDependentWalkers : public HelloWorldTest<OOQFixtureFactory>,
         ASSERT_NE(cmdList.end(), itorWalker2);
     }
 
+    void hexDump(void *ptr, size_t size) {
+        uint8_t *byte = reinterpret_cast<uint8_t *>(ptr);
+        uint8_t bytesNum = 0;
+        while (bytesNum < size) {
+            std::cout << std::hex << "0x" << static_cast<uint32_t>(byte[bytesNum++]) << " ";
+        }
+        std::cout << std::endl;
+    }
+
     GenCmdList::iterator itorWalker1;
     GenCmdList::iterator itorWalker2;
     CommandQueue *pCmdQ2 = nullptr;
@@ -115,10 +124,43 @@ HWTEST_F(TwoOOQsTwoDependentWalkers, shouldHaveOnePS) {
 }
 
 HWCMDTEST_F(IGFX_GEN8_CORE, TwoOOQsTwoDependentWalkers, shouldHaveOneVFEState) {
+    using MEDIA_VFE_STATE = typename FamilyType::MEDIA_VFE_STATE;
     parseWalkers<FamilyType>();
 
-    auto numCommands = getCommandsList<typename FamilyType::MEDIA_VFE_STATE>().size();
+    auto commandsList = getCommandsList<MEDIA_VFE_STATE>();
+    auto numCommands = commandsList.size();
     EXPECT_EQ(1u, numCommands);
+
+    auto expectedCmd = MEDIA_VFE_STATE::sInit();
+
+    if (numCommands > 1) {
+        uint32_t commandIndex = 0;
+        for (auto &cmd : commandsList) {
+            auto offset = reinterpret_cast<uint8_t *>(cmd) - reinterpret_cast<uint8_t *>(*cmdList.begin());
+            std::cout << "MEDIA_VFE_STATE [" << commandIndex << "] : 0x" << std::hex << cmd << ". Byte offset in command buffer: 0x" << offset << std::endl;
+            commandIndex++;
+            if (memcmp(&expectedCmd, cmd, sizeof(MEDIA_VFE_STATE)) == 0) {
+                std::cout << "matches expected MEDIA_VFE_STATE command" << std::endl;
+            } else {
+                std::cout << "doesn't match expected MEDIA_VFE_STATE command." << std::endl;
+            }
+            std::cout << "Expected:" << std::endl;
+            hexDump(&expectedCmd, sizeof(MEDIA_VFE_STATE));
+            std::cout << "Actual:" << std::endl;
+            hexDump(cmd, sizeof(MEDIA_VFE_STATE));
+        }
+        std::cout << std::endl
+                  << "Command buffer content:" << std::endl;
+        auto it = cmdList.begin();
+        uint32_t cmdNum = 0;
+        std::string cmdBuffStr;
+        while (it != cmdList.end()) {
+            cmdBuffStr += std::to_string(cmdNum) + ":" + HardwareParse::getCommandName<FamilyType>(*it) + " ";
+            ++cmdNum;
+            ++it;
+        }
+        std::cout << cmdBuffStr << std::endl;
+    }
 }
 
 HWTEST_F(TwoOOQsTwoDependentWalkers, DISABLED_shouldHaveAPipecontrolBetweenWalkers) {
