cf246d73f5f9655de525aa5107ab98282bc04925
Filip Hazubski <filip.hazubski@intel.com>
2019-11-13 15:37:25 +0100

    Add wrapper for gen12lp tests
    Change-Id: I32a8ef7752721a2cc330aca6d2efa68b1959efd9
    Signed-off-by: Filip Hazubski <filip.hazubski@intel.com>

---
+++ unit_tests/gen12lp/gen12lp_tests_wrapper.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2019 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include "unit_tests/gen12lp/aub_command_stream_receiver_tests_gen12lp.inl"
+#include "unit_tests/gen12lp/coherency_tests_gen12lp.inl"
+#include "unit_tests/gen12lp/command_stream_receiver_hw_tests_gen12lp.inl"
+#include "unit_tests/gen12lp/command_stream_receiver_simulated_common_hw_tests_gen12lp.inl"
+#include "unit_tests/gen12lp/compute_mode_tests_gen12lp.inl"
+#include "unit_tests/gen12lp/enqueue_media_kernel_gen12lp.inl"
+#include "unit_tests/gen12lp/hardware_commands_helper_tests_gen12lp.inl"
+#include "unit_tests/gen12lp/hw_helper_tests_gen12lp.inl"
+#include "unit_tests/gen12lp/image_tests_gen12lp.inl"
+#include "unit_tests/gen12lp/kernel_tests_gen12lp.inl"
+#include "unit_tests/gen12lp/profiling_tests_gen12lp.inl"
+#include "unit_tests/gen12lp/sampler_tests_gen12lp.inl"
+#include "unit_tests/gen12lp/scheduler_source_tests_gen12lp.inl"
+#include "unit_tests/gen12lp/tbx_command_stream_receiver_tests_gen12lp.inl"
+#include "unit_tests/gen12lp/test_device_caps_gen12lp.inl"
+#include "unit_tests/gen12lp/test_device_queue_hw_gen12lp.inl"
+#include "unit_tests/gen12lp/test_platform_caps_gen12lp.inl"
+#include "unit_tests/gen12lp/test_preemption_gen12lp.inl"
+#include "unit_tests/gen12lp/test_sample_gen12lp.inl"
