a2a7501b79bddc8d18a002b225795ec4c3911904
Bill Jordan <bill.jordan@intel.com>
2020-03-23 11:02:24 -0400

    zetSysmanStandbySetMode is not settable from L0
    Change-Id: I05d60f960c9cb89b3d53259067e05e8f20e9b602
    Signed-off-by: Bill Jordan <bill.jordan@intel.com>

--- level_zero/tools/source/sysman/standby/linux/os_standby_imp.cpp
+++ level_zero/tools/source/sysman/standby/linux/os_standby_imp.cpp
@@ -56,10 +56,12 @@ ze_result_t LinuxStandbyImp::getMode(zet_standby_promo_mode_t &mode) {
 }
 
 ze_result_t LinuxStandbyImp::setMode(zet_standby_promo_mode_t mode) {
-    if (ZET_STANDBY_PROMO_MODE_DEFAULT == mode) {
-        return pSysfsAccess->write(standbyModeFile, standbyModeDefault);
-    }
-    return pSysfsAccess->write(standbyModeFile, standbyModeNever);
+    // standbyModeFile is not writable.
+    // Mode cannot be set from L0.
+    // To set the mode, user must reload
+    // the i915 module and set module parameter
+    // enable_rc6 appropriately.
+    return ZE_RESULT_ERROR_UNSUPPORTED_FEATURE;
 }
 
 LinuxStandbyImp::LinuxStandbyImp(OsSysman *pOsSysman) {
