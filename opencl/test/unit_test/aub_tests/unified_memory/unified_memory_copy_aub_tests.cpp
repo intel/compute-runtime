45d7e6f691c76ebad5fb15caf613d71df22c6321
Pawel Wilma <pawel.wilma@intel.com>
2020-03-25 12:57:36 +0100

    Re-enable disabled AUB tests
    Change-Id: I4ad368b7174401496993a6372aeb1034d0ead89e
    Signed-off-by: Pawel Wilma <pawel.wilma@intel.com>

--- opencl/test/unit_test/aub_tests/unified_memory/unified_memory_copy_aub_tests.cpp
+++ opencl/test/unit_test/aub_tests/unified_memory/unified_memory_copy_aub_tests.cpp
@@ -39,7 +39,7 @@ class UnifiedMemoryCopyAubTest : public UnifiedMemoryAubFixture,
     }
 };
 
-HWTEST_P(UnifiedMemoryCopyAubTest, DISABLED_givenTwoUnifiedMemoryAllocsWhenCopyingOneToAnotherThenValuesMatch) {
+HWTEST_P(UnifiedMemoryCopyAubTest, givenTwoUnifiedMemoryAllocsWhenCopyingOneToAnotherThenValuesMatch) {
     clEnqueueMemcpyINTEL(this->pCmdQ, true, dstPtr, srcPtr, dataSize, 0, nullptr, nullptr);
     expectMemory<FamilyType>(dstPtr, srcValues.data(), dataSize);
 }
