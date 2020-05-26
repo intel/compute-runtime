c18939c7aa34ea4efe5735a09b3b8ed761de6ae0
davidoli <david.olien@intel.com>
2020-05-26 13:22:30 -0700

    Simple fix for potential memory leak.
    Change-Id: I3cb96ce810e5cd83fe3ce55182f39c6fc2264629
    Signed-off-by: davidoli <david.olien@intel.com>

--- level_zero/tools/source/pin/pin.cpp
+++ level_zero/tools/source/pin/pin.cpp
@@ -19,21 +19,23 @@ const std::string gtPinOpenFunctionName = "OpenGTPin";
 namespace L0 {
 
 void PinContext::init(ze_init_flag_t flag, ze_result_t &result) {
+    std::unique_ptr<NEO::OsLibrary> hGtPinLibrary = nullptr;
     result = ZE_RESULT_SUCCESS;
 
     if (!getenv_tobool("ZE_ENABLE_PROGRAM_INSTRUMENTATION")) {
         return;
     }
 
-    NEO::OsLibrary *hPin = NEO::OsLibrary::load(gtPinLibraryFilename.c_str());
-    if (hPin == nullptr) {
+    hGtPinLibrary.reset(NEO::OsLibrary::load(gtPinLibraryFilename.c_str()));
+    if (hGtPinLibrary.get() == nullptr) {
         NEO::printDebugString(NEO::DebugManager.flags.PrintDebugMessages.get(), stderr, "Unable to find gtpin library %s\n", gtPinLibraryFilename.c_str());
         result = ZE_RESULT_ERROR_UNKNOWN;
         return;
     }
 
-    OpenGTPin_fn openGTPin = reinterpret_cast<OpenGTPin_fn>(hPin->getProcAddress(gtPinOpenFunctionName.c_str()));
+    OpenGTPin_fn openGTPin = reinterpret_cast<OpenGTPin_fn>(hGtPinLibrary.get()->getProcAddress(gtPinOpenFunctionName.c_str()));
     if (openGTPin == nullptr) {
+        hGtPinLibrary.reset(nullptr);
         NEO::printDebugString(NEO::DebugManager.flags.PrintDebugMessages.get(), stderr, "Unable to find gtpin library open function symbol %s\n", gtPinOpenFunctionName.c_str());
         result = ZE_RESULT_ERROR_UNKNOWN;
         return;
@@ -41,6 +43,7 @@ void PinContext::init(ze_init_flag_t flag, ze_result_t &result) {
 
     uint32_t openResult = openGTPin(nullptr);
     if (openResult != 0) {
+        hGtPinLibrary.reset(nullptr);
         NEO::printDebugString(NEO::DebugManager.flags.PrintDebugMessages.get(), stderr, "gtpin library open %s failed with status %u\n", gtPinOpenFunctionName.c_str(), openResult);
         result = ZE_RESULT_ERROR_UNKNOWN;
         return;
