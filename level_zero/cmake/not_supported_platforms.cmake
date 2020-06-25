d34ddb94d9b364669401ff3a42aa4e30f525fd54
Mateusz Jablonski <mateusz.jablonski@intel.com>
2020-06-25 13:40:21 +0200

    Disable BUILD_WITH_L0 when selected platforms are not supported
    Change-Id: Ic945fd091ac1e12182503f5a9333d42582f3f9f5
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

---
+++ level_zero/cmake/not_supported_platforms.cmake
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2020 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+#
+
+set(L0_NOT_SUPPORTED_PRODUCT_FAMILIES
+  "IGFX_BROXTON"
+  "IGFX_LAKEFIELD"
+  "IGFX_ELKHARTLAKE"
+  "IGFX_BROADWELL"
+)
