c3a18804926b151e132e838b82de94fd1be25c4f
Igor Venevtsev <igor.venevtsev@intel.com>
2020-07-08 15:42:16 +0200

    Do not allocate PRIVATE_SURFACE large than 4GB
    Related-To: NEO-4479
    
    Change-Id: I97ffa25e9af94e0125e1eaaebeddfdac49205241
    Signed-off-by: Igor Venevtsev <igor.venevtsev@intel.com>

--- opencl/source/kernel/kernel.cpp
+++ opencl/source/kernel/kernel.cpp
@@ -276,7 +276,7 @@ cl_int Kernel::initialize() {
         if (privateSurfaceSize) {
             privateSurfaceSize *= device.getSharedDeviceInfo().computeUnitsUsedForScratch * getKernelInfo().getMaxSimdSize();
             DEBUG_BREAK_IF(privateSurfaceSize == 0);
-            if ((is32Bit() || device.getMemoryManager()->peekForce32BitAllocations()) && (privateSurfaceSize > std::numeric_limits<uint32_t>::max())) {
+            if (privateSurfaceSize > std::numeric_limits<uint32_t>::max()) {
                 retVal = CL_OUT_OF_RESOURCES;
                 break;
             }
