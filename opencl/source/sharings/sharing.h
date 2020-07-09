d20a19658ddafc1fc65b255c3bfba15f6b493ec1
Artur Harasimiuk <artur.harasimiuk@intel.com>
2020-07-09 17:06:59 +0200

    fix for UninitializedObject reported by clang-tidy
    fixes: 2 uninitialized fields at the end of the constructor call
    [clang-analyzer-optin.cplusplus.UninitializedObject]
    
    Change-Id: I3332ad70fa661513c3e67f263ef3cbf20ec26485
    Signed-off-by: Artur Harasimiuk <artur.harasimiuk@intel.com>

--- opencl/source/sharings/sharing.h
+++ opencl/source/sharings/sharing.h
@@ -22,8 +22,8 @@ enum SynchronizeStatus {
 struct UpdateData {
     UpdateData(uint32_t inRootDeviceIndex) : rootDeviceIndex(inRootDeviceIndex){};
     const uint32_t rootDeviceIndex;
-    SynchronizeStatus synchronizationStatus;
-    osHandle sharedHandle;
+    SynchronizeStatus synchronizationStatus = SHARED_OBJECT_NOT_CHANGED;
+    osHandle sharedHandle = 0;
     MemObj *memObject = nullptr;
     void *updateData = nullptr;
 };
