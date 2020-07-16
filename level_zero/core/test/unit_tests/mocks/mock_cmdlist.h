70ccc2a225549768d85c6a0a488055e8e81c778c
Jaime Arteaga <jaime.a.arteaga.molina@intel.com>
2020-07-16 09:20:18 -0700

    Cleanup mock_cmdlist.h
    Change-Id: I467a63a822e76f8c512680629b7b8518b283f6e5
    Signed-off: Jaime Arteaga <jaime.a.arteaga.molina@intel.com>

--- level_zero/core/test/unit_tests/mocks/mock_cmdlist.h
+++ level_zero/core/test/unit_tests/mocks/mock_cmdlist.h
@@ -12,11 +12,6 @@
 #include "level_zero/core/test/unit_tests/mocks/mock_device.h"
 #include "level_zero/core/test/unit_tests/white_box.h"
 
-#if defined(__clang__)
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Winconsistent-missing-override"
-#endif
-
 namespace NEO {
 class GraphicsAllocation;
 }
@@ -79,99 +74,241 @@ struct Mock<CommandList> : public CommandList {
     Mock(Device *device = nullptr);
     ~Mock() override;
 
-    MOCK_METHOD0(close, ze_result_t());
-    MOCK_METHOD0(destroy, ze_result_t());
-    MOCK_METHOD2(appendCommandLists,
-                 ze_result_t(uint32_t numCommandLists, ze_command_list_handle_t *phCommandLists));
-    MOCK_METHOD5(appendLaunchKernel,
-                 ze_result_t(ze_kernel_handle_t hFunction,
-                             const ze_group_count_t *pThreadGroupDimensions,
-                             ze_event_handle_t hEvent, uint32_t numWaitEvents,
-                             ze_event_handle_t *phWaitEvents));
-    MOCK_METHOD5(appendLaunchCooperativeKernel,
-                 ze_result_t(ze_kernel_handle_t hKernel,
-                             const ze_group_count_t *pLaunchFuncArgs,
-                             ze_event_handle_t hSignalEvent,
-                             uint32_t numWaitEvents,
-                             ze_event_handle_t *phWaitEvents));
-    MOCK_METHOD5(appendLaunchKernelIndirect,
-                 ze_result_t(ze_kernel_handle_t hFunction,
-                             const ze_group_count_t *pDispatchArgumentsBuffer,
-                             ze_event_handle_t hEvent, uint32_t numWaitEvents,
-                             ze_event_handle_t *phWaitEvents));
-    MOCK_METHOD7(appendLaunchMultipleKernelsIndirect,
-                 ze_result_t(uint32_t numFunctions, const ze_kernel_handle_t *phFunctions,
-                             const uint32_t *pNumLaunchArguments,
-                             const ze_group_count_t *pLaunchArgumentsBuffer,
-                             ze_event_handle_t hEvent, uint32_t numWaitEvents,
-                             ze_event_handle_t *phWaitEvents));
-    MOCK_METHOD1(appendEventReset, ze_result_t(ze_event_handle_t hEvent));
-    MOCK_METHOD3(appendBarrier, ze_result_t(ze_event_handle_t hSignalEvent, uint32_t numWaitEvents,
-                                            ze_event_handle_t *phWaitEvents));
-    MOCK_METHOD6(appendMemoryRangesBarrier,
-                 ze_result_t(uint32_t numRanges, const size_t *pRangeSizes, const void **pRanges,
-                             ze_event_handle_t hSignalEvent, uint32_t numWaitEvents,
-                             ze_event_handle_t *phWaitEvents));
-    MOCK_METHOD6(appendImageCopyFromMemory,
-                 ze_result_t(ze_image_handle_t hDstImage, const void *srcptr,
-                             const ze_image_region_t *pDstRegion, ze_event_handle_t hEvent,
-                             uint32_t numWaitEvents, ze_event_handle_t *phWaitEvents));
-    MOCK_METHOD6(appendImageCopyToMemory,
-                 ze_result_t(void *dstptr, ze_image_handle_t hSrcImage,
-                             const ze_image_region_t *pSrcRegion, ze_event_handle_t hEvent,
-                             uint32_t numWaitEvents, ze_event_handle_t *phWaitEvents));
-    MOCK_METHOD7(appendImageCopyRegion,
-                 ze_result_t(ze_image_handle_t hDstImage, ze_image_handle_t hSrcImage,
-                             const ze_image_region_t *pDstRegion, const ze_image_region_t *pSrcRegion,
-                             ze_event_handle_t hSignalEvent, uint32_t numWaitEvents,
-                             ze_event_handle_t *phWaitEvents));
-    MOCK_METHOD5(appendImageCopy,
-                 ze_result_t(ze_image_handle_t hDstImage, ze_image_handle_t hSrcImage,
-                             ze_event_handle_t hEvent, uint32_t numWaitEvents,
-                             ze_event_handle_t *phWaitEvents));
-    MOCK_METHOD4(appendMemAdvise, ze_result_t(ze_device_handle_t hDevice, const void *ptr,
-                                              size_t size, ze_memory_advice_t advice));
-    MOCK_METHOD6(appendMemoryCopy, ze_result_t(void *dstptr, const void *srcptr, size_t size,
-                                               ze_event_handle_t hEvent, uint32_t numWaitEvents,
-                                               ze_event_handle_t *phWaitEvents));
-    MOCK_METHOD4(appendPageFaultCopy, ze_result_t(NEO::GraphicsAllocation *dstptr, NEO::GraphicsAllocation *srcptr, size_t size, bool flushHost));
-    MOCK_METHOD9(appendMemoryCopyRegion, ze_result_t(void *dstptr,
-                                                     const ze_copy_region_t *dstRegion,
-                                                     uint32_t dstPitch,
-                                                     uint32_t dstSlicePitch,
-                                                     const void *srcptr,
-                                                     const ze_copy_region_t *srcRegion,
-                                                     uint32_t srcPitch,
-                                                     uint32_t srcSlicePitch,
-                                                     ze_event_handle_t hSignalEvent));
-    MOCK_METHOD2(appendMemoryPrefetch, ze_result_t(const void *ptr, size_t count));
-    MOCK_METHOD5(appendMemoryFill,
-                 ze_result_t(void *ptr, const void *pattern,
-                             size_t pattern_size, size_t size, ze_event_handle_t hEvent));
-    MOCK_METHOD1(appendSignalEvent, ze_result_t(ze_event_handle_t hEvent));
-    MOCK_METHOD2(appendWaitOnEvents, ze_result_t(uint32_t numEvents, ze_event_handle_t *phEvent));
-    MOCK_METHOD4(appendWriteGlobalTimestamp, ze_result_t(uint64_t *dstptr, ze_event_handle_t hSignalEvent,
-                                                         uint32_t numWaitEvents, ze_event_handle_t *phWaitEvents));
-    MOCK_METHOD2(reserveSpace, ze_result_t(size_t size, void **ptr));
-    MOCK_METHOD0(reset, ze_result_t());
-    MOCK_METHOD0(resetParameters, ze_result_t());
-
-    MOCK_METHOD0(appendMetricMemoryBarrier, ze_result_t());
-    MOCK_METHOD2(appendMetricTracerMarker,
-                 ze_result_t(zet_metric_tracer_handle_t hMetricTracer, uint32_t value));
-    MOCK_METHOD1(appendMetricQueryBegin, ze_result_t(zet_metric_query_handle_t hMetricQuery));
-    MOCK_METHOD2(appendMetricQueryEnd, ze_result_t(zet_metric_query_handle_t hMetricQuery,
-                                                   ze_event_handle_t hCompletionEvent));
-    MOCK_METHOD2(appendMILoadRegImm, ze_result_t(uint32_t reg, uint32_t value));
-    MOCK_METHOD2(appendMILoadRegReg, ze_result_t(uint32_t reg1, uint32_t reg2));
-    MOCK_METHOD2(appendMILoadRegMem, ze_result_t(uint32_t reg1, uint64_t address));
-    MOCK_METHOD2(appendMIStoreRegMem, ze_result_t(uint32_t reg1, uint64_t address));
-    MOCK_METHOD2(appendMIMath, ze_result_t(void *aluArray, size_t aluCount));
-    MOCK_METHOD3(appendMIBBStart, ze_result_t(uint64_t address, size_t predication, bool secondLevel));
-    MOCK_METHOD0(appendMIBBEnd, ze_result_t());
-    MOCK_METHOD0(appendMINoop, ze_result_t());
-    MOCK_METHOD1(executeCommandListImmediate, ze_result_t(bool perforMigration));
-    MOCK_METHOD2(initialize, bool(L0::Device *device, bool onlyCopyBlit));
+    MOCK_METHOD(ze_result_t,
+                close,
+                (),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                destroy,
+                (),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendLaunchKernel,
+                (ze_kernel_handle_t hFunction,
+                 const ze_group_count_t *pThreadGroupDimensions,
+                 ze_event_handle_t hEvent, uint32_t numWaitEvents,
+                 ze_event_handle_t *phWaitEvents),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendLaunchCooperativeKernel,
+                (ze_kernel_handle_t hKernel,
+                 const ze_group_count_t *pLaunchFuncArgs,
+                 ze_event_handle_t hSignalEvent,
+                 uint32_t numWaitEvents,
+                 ze_event_handle_t *phWaitEvents),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendLaunchKernelIndirect,
+                (ze_kernel_handle_t hFunction,
+                 const ze_group_count_t *pDispatchArgumentsBuffer,
+                 ze_event_handle_t hEvent,
+                 uint32_t numWaitEvents,
+                 ze_event_handle_t *phWaitEvents),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendLaunchMultipleKernelsIndirect,
+                (uint32_t numFunctions,
+                 const ze_kernel_handle_t *phFunctions,
+                 const uint32_t *pNumLaunchArguments,
+                 const ze_group_count_t *pLaunchArgumentsBuffer,
+                 ze_event_handle_t hEvent,
+                 uint32_t numWaitEvents,
+                 ze_event_handle_t *phWaitEvents),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendEventReset,
+                (ze_event_handle_t hEvent),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendBarrier,
+                (ze_event_handle_t hSignalEvent,
+                 uint32_t numWaitEvents,
+                 ze_event_handle_t *phWaitEvents),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMemoryRangesBarrier,
+                (uint32_t numRanges,
+                 const size_t *pRangeSizes,
+                 const void **pRanges,
+                 ze_event_handle_t hSignalEvent,
+                 uint32_t numWaitEvents,
+                 ze_event_handle_t *phWaitEvents),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendImageCopyFromMemory,
+                (ze_image_handle_t hDstImage,
+                 const void *srcptr,
+                 const ze_image_region_t *pDstRegion,
+                 ze_event_handle_t hEvent,
+                 uint32_t numWaitEvents,
+                 ze_event_handle_t *phWaitEvents),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendImageCopyToMemory,
+                (void *dstptr,
+                 ze_image_handle_t hSrcImage,
+                 const ze_image_region_t *pSrcRegion,
+                 ze_event_handle_t hEvent,
+                 uint32_t numWaitEvents,
+                 ze_event_handle_t *phWaitEvents),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendImageCopyRegion,
+                (ze_image_handle_t hDstImage,
+                 ze_image_handle_t hSrcImage,
+                 const ze_image_region_t *pDstRegion,
+                 const ze_image_region_t *pSrcRegion,
+                 ze_event_handle_t hSignalEvent,
+                 uint32_t numWaitEvents,
+                 ze_event_handle_t *phWaitEvents),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendImageCopy,
+                (ze_image_handle_t hDstImage,
+                 ze_image_handle_t hSrcImage,
+                 ze_event_handle_t hEvent,
+                 uint32_t numWaitEvents,
+                 ze_event_handle_t *phWaitEvents),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMemAdvise,
+                (ze_device_handle_t hDevice,
+                 const void *ptr,
+                 size_t size,
+                 ze_memory_advice_t advice),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMemoryCopy,
+                (void *dstptr,
+                 const void *srcptr,
+                 size_t size,
+                 ze_event_handle_t hEvent,
+                 uint32_t numWaitEvents,
+                 ze_event_handle_t *phWaitEvents),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendPageFaultCopy,
+                (NEO::GraphicsAllocation * dstptr,
+                 NEO::GraphicsAllocation *srcptr,
+                 size_t size,
+                 bool flushHost),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMemoryCopyRegion,
+                (void *dstptr,
+                 const ze_copy_region_t *dstRegion,
+                 uint32_t dstPitch,
+                 uint32_t dstSlicePitch,
+                 const void *srcptr,
+                 const ze_copy_region_t *srcRegion,
+                 uint32_t srcPitch,
+                 uint32_t srcSlicePitch,
+                 ze_event_handle_t hSignalEvent),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMemoryPrefetch,
+                (const void *ptr,
+                 size_t count),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMemoryFill,
+                (void *ptr,
+                 const void *pattern,
+                 size_t pattern_size,
+                 size_t size,
+                 ze_event_handle_t hEvent),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendSignalEvent,
+                (ze_event_handle_t hEvent),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendWaitOnEvents,
+                (uint32_t numEvents,
+                 ze_event_handle_t *phEvent),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendWriteGlobalTimestamp,
+                (uint64_t * dstptr,
+                 ze_event_handle_t hSignalEvent,
+                 uint32_t numWaitEvents,
+                 ze_event_handle_t *phWaitEvents),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                reserveSpace,
+                (size_t size,
+                 void **ptr),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                reset,
+                (),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMetricMemoryBarrier,
+                (),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMetricTracerMarker,
+                (zet_metric_tracer_handle_t hMetricTracer,
+                 uint32_t value),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMetricQueryBegin,
+                (zet_metric_query_handle_t hMetricQuery),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMetricQueryEnd,
+                (zet_metric_query_handle_t hMetricQuery,
+                 ze_event_handle_t hCompletionEvent),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMILoadRegImm,
+                (uint32_t reg,
+                 uint32_t value),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMILoadRegReg,
+                (uint32_t reg1,
+                 uint32_t reg2),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMILoadRegMem,
+                (uint32_t reg1,
+                 uint64_t address),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMIStoreRegMem,
+                (uint32_t reg1,
+                 uint64_t address),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMIMath,
+                (void *aluArray,
+                 size_t aluCount),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMIBBStart,
+                (uint64_t address,
+                 size_t predication,
+                 bool secondLevel),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMIBBEnd,
+                (),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                appendMINoop,
+                (),
+                (override));
+    MOCK_METHOD(ze_result_t,
+                executeCommandListImmediate,
+                (bool perforMigration),
+                (override));
+    MOCK_METHOD(bool,
+                initialize,
+                (L0::Device * device,
+                 bool onlyCopyBlit),
+                (override));
 
     uint8_t *batchBuffer = nullptr;
     NEO::GraphicsAllocation *mockAllocation = nullptr;
