bb6ed003e8cdf781ebc660c012c0e14dc050d52e
Jan Beich <jbeich@FreeBSD.org>
2020-09-20 15:41:43 +0000

    Don't use VM when ppGTT is invalid/unassigned
    Signed-off-by: Jan Beich <jbeich@FreeBSD.org>

--- shared/source/os_interface/linux/drm_query_exp.cpp
+++ shared/source/os_interface/linux/drm_query_exp.cpp
@@ -65,6 +65,10 @@ int Drm::createDrmVirtualMemory(uint32_t &drmVmId) {
     drm_i915_gem_vm_control ctl = {};
     auto ret = SysCalls::ioctl(getFileDescriptor(), DRM_IOCTL_I915_GEM_VM_CREATE, &ctl);
     if (ret == 0) {
+        if (ctl.vm_id == 0) {
+            // 0 is reserved for invalid/unassigned ppgtt
+            return -1;
+        }
         drmVmId = ctl.vm_id;
     }
     return ret;
