4d4f73580b361c500446c0d2ac7cca70d7a88aaf
Bartosz Dunajski <bartosz.dunajski@intel.com>
2020-10-21 15:17:46 +0200

    Update max blit width
    Change-Id: I94fcc325bc9838182458973afb411dece80f57f7
    Signed-off-by: Bartosz Dunajski <bartosz.dunajski@intel.com>

--- opencl/test/unit_test/command_stream/command_stream_receiver_hw_2_tests.cpp
+++ opencl/test/unit_test/command_stream/command_stream_receiver_hw_2_tests.cpp
@@ -1228,6 +1228,8 @@ TEST_F(ScratchSpaceControllerTest, whenScratchSpaceControllerIsDestroyedThenItRe
 }
 
 TEST(BcsConstantsTests, givenBlitConstantsThenTheyHaveDesiredValues) {
-    EXPECT_EQ(BlitterConstants::maxBlitWidth, 0x3FC0u);
+    EXPECT_EQ(BlitterConstants::maxBlitWidth, 0x3F80u);
     EXPECT_EQ(BlitterConstants::maxBlitHeight, 0x3FC0u);
+    EXPECT_EQ(BlitterConstants::maxBlitSetWidth, 0x1FF80u);
+    EXPECT_EQ(BlitterConstants::maxBlitSetHeight, 0x1FFC0u);
 }
