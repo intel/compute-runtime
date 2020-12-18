ff3d4799cac05482daeae4f1a23d978c66654ffb
Katarzyna Cencelewska <katarzyna.cencelewska@intel.com>
2020-12-18 17:25:12 +0100

    Refactor cmake
    Signed-off-by: Katarzyna Cencelewska <katarzyna.cencelewska@intel.com>

--- opencl/source/gen12lp/additional_files_gen12lp.cmake
+++ opencl/source/gen12lp/additional_files_gen12lp.cmake
@@ -8,7 +8,6 @@ set(RUNTIME_SRCS_ADDITIONAL_FILES_GEN12LP
     ${CMAKE_CURRENT_SOURCE_DIR}/gen12lp/definitions${BRANCH_DIR_SUFFIX}/command_queue_helpers_gen12lp.inl
     ${CMAKE_CURRENT_SOURCE_DIR}/gen12lp/definitions${BRANCH_DIR_SUFFIX}/hardware_commands_helper_gen12lp.inl
 )
-
 include_directories(${NEO_SOURCE_DIR}/opencl/source/gen12lp/definitions${BRANCH_DIR_SUFFIX}/)
 
 target_sources(${NEO_STATIC_LIB_NAME} PRIVATE ${RUNTIME_SRCS_ADDITIONAL_FILES_GEN12LP})
