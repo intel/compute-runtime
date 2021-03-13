65acd98937332d2772a9fbc75bb12e2285e2702a
Jaime Arteaga <jaime.a.arteaga.molina@intel.com>
2021-03-13 00:25:57 +0000

    Add zeKernelSetGlobalOffsetExp to ze_core_loader
    Signed-off-by: Jaime Arteaga <jaime.a.arteaga.molina@intel.com>

--- level_zero/api/core/ze_core_loader.cpp
+++ level_zero/api/core/ze_core_loader.cpp
@@ -565,3 +565,19 @@ zeGetSamplerProcAddrTable(
     }
     return result;
 }
+
+ZE_APIEXPORT ze_result_t ZE_APICALL
+zeGetKernelExpProcAddrTable(
+    ze_api_version_t version,
+    ze_kernel_exp_dditable_t *pDdiTable) {
+    if (nullptr == pDdiTable)
+        return ZE_RESULT_ERROR_INVALID_ARGUMENT;
+    if (ZE_MAJOR_VERSION(driver_ddiTable.version) != ZE_MAJOR_VERSION(version) ||
+        ZE_MINOR_VERSION(driver_ddiTable.version) > ZE_MINOR_VERSION(version))
+        return ZE_RESULT_ERROR_UNSUPPORTED_VERSION;
+
+    ze_result_t result = ZE_RESULT_SUCCESS;
+    pDdiTable->pfnSetGlobalOffsetExp = zeKernelSetGlobalOffsetExp;
+    driver_ddiTable.core_ddiTable.KernelExp = *pDdiTable;
+    return result;
+}
