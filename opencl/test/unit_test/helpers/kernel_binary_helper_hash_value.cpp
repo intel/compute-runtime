00fc92129dd6eab3929d7a888e5170879c281e22
Szymon Morek <szymon.morek@intel.com>
2021-05-26 15:17:34 +0000

    Fix builtin kernel copy_kernel_timestamps
    Resolves: NEO-5905
    
    Signed-off-by: Szymon Morek <szymon.morek@intel.com>

--- opencl/test/unit_test/helpers/kernel_binary_helper_hash_value.cpp
+++ opencl/test/unit_test/helpers/kernel_binary_helper_hash_value.cpp
@@ -7,5 +7,5 @@
 
 #include "opencl/test/unit_test/helpers/kernel_binary_helper.h"
 
-const std::string KernelBinaryHelper::BUILT_INS("9007865385029577975");
-const std::string KernelBinaryHelper::BUILT_INS_WITH_IMAGES("17984918533494000167_images");
+const std::string KernelBinaryHelper::BUILT_INS("7836643072362489210");
+const std::string KernelBinaryHelper::BUILT_INS_WITH_IMAGES("10613492505254921609_images");
