2588997e329a123d44449e957ddd80e4f77ccaf0
Jaime Arteaga <jaime.a.arteaga.molina@intel.com>
2021-07-21 09:21:23 +0000

    Remove memory.cpp from L0 core source
    It was only hosting two methods, which are better in
    driver_handl_imp.cpp
    
    Signed-off-by: Jaime Arteaga <jaime.a.arteaga.molina@intel.com>

--- level_zero/core/source/driver/driver_handle_imp.cpp
+++ level_zero/core/source/driver/driver_handle_imp.cpp
@@ -383,6 +383,46 @@ bool DriverHandleImp::isRemoteResourceNeeded(void *ptr, NEO::GraphicsAllocation 
     return (alloc == nullptr || (allocData && ((allocData->gpuAllocations.getGraphicsAllocations().size() - 1) < device->getRootDeviceIndex())));
 }
 
+void *DriverHandleImp::importFdHandle(ze_device_handle_t hDevice, ze_ipc_memory_flags_t flags, uint64_t handle, NEO::GraphicsAllocation **pAlloc) {
+    auto neoDevice = Device::fromHandle(hDevice)->getNEODevice();
+    NEO::osHandle osHandle = static_cast<NEO::osHandle>(handle);
+    NEO::AllocationProperties unifiedMemoryProperties{neoDevice->getRootDeviceIndex(),
+                                                      MemoryConstants::pageSize,
+                                                      NEO::GraphicsAllocation::AllocationType::BUFFER,
+                                                      neoDevice->getDeviceBitfield()};
+    unifiedMemoryProperties.subDevicesBitfield = neoDevice->getDeviceBitfield();
+    NEO::GraphicsAllocation *alloc =
+        this->getMemoryManager()->createGraphicsAllocationFromSharedHandle(osHandle,
+                                                                           unifiedMemoryProperties,
+                                                                           false,
+                                                                           false);
+    if (alloc == nullptr) {
+        return nullptr;
+    }
+
+    NEO::SvmAllocationData allocData(neoDevice->getRootDeviceIndex());
+    allocData.gpuAllocations.addAllocation(alloc);
+    allocData.cpuAllocation = nullptr;
+    allocData.size = alloc->getUnderlyingBufferSize();
+    allocData.memoryType = InternalMemoryType::DEVICE_UNIFIED_MEMORY;
+    allocData.device = neoDevice;
+    if (flags & ZE_DEVICE_MEM_ALLOC_FLAG_BIAS_UNCACHED) {
+        allocData.allocationFlagsProperty.flags.locallyUncachedResource = 1;
+    }
+
+    if (flags & ZE_IPC_MEMORY_FLAG_BIAS_UNCACHED) {
+        allocData.allocationFlagsProperty.flags.locallyUncachedResource = 1;
+    }
+
+    this->getSvmAllocsManager()->insertSVMAlloc(allocData);
+
+    if (pAlloc) {
+        *pAlloc = alloc;
+    }
+
+    return reinterpret_cast<void *>(alloc->getGpuAddress());
+}
+
 NEO::GraphicsAllocation *DriverHandleImp::getPeerAllocation(Device *device,
                                                             NEO::SvmAllocationData *allocData,
                                                             void *ptr,
@@ -420,6 +460,23 @@ NEO::GraphicsAllocation *DriverHandleImp::getPeerAllocation(Device *device,
     }
 
     return alloc;
+}
+
+ze_result_t DriverHandleImp::checkMemoryAccessFromDevice(Device *device, const void *ptr) {
+    auto allocation = svmAllocsManager->getSVMAlloc(ptr);
+    if (allocation == nullptr) {
+        return ZE_RESULT_ERROR_INVALID_ARGUMENT;
+    }
+
+    if (allocation->memoryType == InternalMemoryType::HOST_UNIFIED_MEMORY ||
+        allocation->memoryType == InternalMemoryType::SHARED_UNIFIED_MEMORY)
+        return ZE_RESULT_SUCCESS;
+
+    if (allocation->gpuAllocations.getGraphicsAllocation(device->getRootDeviceIndex()) != nullptr) {
+        return ZE_RESULT_SUCCESS;
+    }
+
+    return ZE_RESULT_ERROR_INVALID_ARGUMENT;
 }
 
 } // namespace L0
