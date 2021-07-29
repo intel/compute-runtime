28153e994a25ac43aa1aedac7780684f5f750186
Bartosz Dunajski <bartosz.dunajski@intel.com>
2021-07-29 11:45:02 +0000

    Add xe_hp_core test excludes
    Signed-off-by: Bartosz Dunajski <bartosz.dunajski@intel.com>

---
+++ opencl/test/unit_test/xe_hp_core/excludes_xe_hp_core.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2021 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include "test.h"
+
+HWTEST_EXCLUDE_PRODUCT(BufferSetSurfaceTests, givenAlignedCacheableReadOnlyBufferThenChoseOclBufferPolicy, IGFX_XE_HP_CORE);
+HWTEST_EXCLUDE_PRODUCT(BufferSetSurfaceTests, givenBufferSetSurfaceThatMemoryIsUnalignedToCachelineButReadOnlyThenL3CacheShouldBeStillOn, IGFX_XE_HP_CORE);
+HWTEST_EXCLUDE_PRODUCT(ProfilingWithPerfCountersTests, GivenCommandQueueWithProfilingPerfCountersWhenWalkerIsDispatchedThenRegisterStoresArePresentInCS, IGFX_XE_HP_CORE);
