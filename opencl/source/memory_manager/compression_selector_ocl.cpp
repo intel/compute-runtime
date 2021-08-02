15f0642d6030db66cc766cf9ae084fd803c6d8d0
Milczarek, Slawomir <slawomir.milczarek@intel.com>
2021-08-02 17:20:25 +0000

    Compression selector to check for stateless compression
    Related-To: NEO-5107
    
    Signed-off-by: Milczarek, Slawomir <slawomir.milczarek@intel.com>

--- opencl/source/memory_manager/compression_selector_ocl.cpp
+++ opencl/source/memory_manager/compression_selector_ocl.cpp
@@ -5,11 +5,24 @@
  *
  */
 
+#include "shared/source/helpers/hw_helper.h"
 #include "shared/source/memory_manager/compression_selector.h"
 
 namespace NEO {
 bool CompressionSelector::preferRenderCompressedBuffer(const AllocationProperties &properties, const HardwareInfo &hwInfo) {
-    return GraphicsAllocation::AllocationType::BUFFER_COMPRESSED == properties.allocationType;
+    switch (properties.allocationType) {
+    case GraphicsAllocation::AllocationType::BUFFER_COMPRESSED:
+        return true;
+    case GraphicsAllocation::AllocationType::GLOBAL_SURFACE:
+    case GraphicsAllocation::AllocationType::CONSTANT_SURFACE:
+    case GraphicsAllocation::AllocationType::SVM_GPU:
+    case GraphicsAllocation::AllocationType::PRINTF_SURFACE: {
+        auto &hwHelper = HwHelper::get(hwInfo.platform.eRenderCoreFamily);
+        return hwHelper.allowStatelessCompression(hwInfo);
+    }
+    default:
+        return false;
+    }
 }
 
 } // namespace NEO
