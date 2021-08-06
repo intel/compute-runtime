7e74eb82cd05355f00a84e1db683c35e234c1f80
Dominik Dabek <dominik.dabek@intel.com>
2021-08-06 11:38:44 +0000

    Clean up get_devices.cpp
    Signed-off-by: Dominik Dabek <dominik.dabek@intel.com>

--- opencl/source/dll/get_devices.cpp
+++ opencl/source/dll/get_devices.cpp
@@ -7,8 +7,6 @@
 
 #include "shared/source/execution_environment/execution_environment.h"
 #include "shared/source/execution_environment/root_device_environment.h"
-#include "shared/source/helpers/constants.h"
-#include "shared/source/os_interface/device_factory.h"
 
 #include "opencl/source/command_stream/create_command_stream_impl.h"
 
