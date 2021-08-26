e775dfa9a562dd0c0311985cfb242aad58cf232a
Mateusz Hoppe <mateusz.hoppe@intel.com>
2021-08-26 17:35:15 +0000

    Add SysCall pwrite()
    Signed-off-by: Mateusz Hoppe <mateusz.hoppe@intel.com>

--- shared/source/os_interface/linux/sys_calls.h
+++ shared/source/os_interface/linux/sys_calls.h
@@ -25,6 +25,7 @@ int readlink(const char *path, char *buf, size_t bufsize);
 int poll(struct pollfd *pollFd, unsigned long int numberOfFds, int timeout);
 int fstat(int fd, struct stat *buf);
 ssize_t pread(int fd, void *buf, size_t count, off_t offset);
+ssize_t pwrite(int fd, const void *buf, size_t count, off_t offset);
 void *mmap(void *addr, size_t size, int prot, int flags, int fd, off_t off);
 int munmap(void *addr, size_t size);
 } // namespace SysCalls
--- shared/source/os_interface/linux/sys_calls_linux.cpp
+++ shared/source/os_interface/linux/sys_calls_linux.cpp
@@ -71,6 +71,10 @@ ssize_t pread(int fd, void *buf, size_t count, off_t offset) {
     return ::pread(fd, buf, count, offset);
 }
 
+ssize_t pwrite(int fd, const void *buf, size_t count, off_t offset) {
+    return ::pwrite(fd, buf, count, offset);
+}
+
 void *mmap(void *addr, size_t size, int prot, int flags, int fd, off_t off) {
     return ::mmap(addr, size, prot, flags, fd, off);
 }
