51a5d5cec3706d5e0b0f2034945056020ceb866d
Mateusz Jablonski <mateusz.jablonski@intel.com>
2021-08-31 09:54:02 +0000

    Correct include for cmdlist on ADLS
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

--- level_zero/core/source/gen12lp/adls/cmdlist_adls.cpp
+++ level_zero/core/source/gen12lp/adls/cmdlist_adls.cpp
@@ -5,12 +5,13 @@
  *
  */
 
-#include "level_zero/core/source/cmdlist/cmdlist_extended/cmdlist_extended.inl"
 #include "level_zero/core/source/cmdlist/cmdlist_hw.inl"
 #include "level_zero/core/source/cmdlist/cmdlist_hw_base.inl"
 #include "level_zero/core/source/cmdlist/cmdlist_hw_immediate.inl"
 #include "level_zero/core/source/gen12lp/cmdlist_gen12lp.h"
 #include "level_zero/core/source/gen12lp/definitions/cache_flush_gen12lp.inl"
+
+#include "cmdlist_extended.inl"
 
 namespace L0 {
 template struct CommandListCoreFamily<IGFX_GEN12LP_CORE>;
