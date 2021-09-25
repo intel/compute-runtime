3bb82e7658452a02c5eebb5b0e9b2de1464d4b42
Artur Harasimiuk <artur.harasimiuk@intel.com>
2021-09-25 21:21:18 +0200

    tweaks
    Signed-off-by: Artur Harasimiuk <artur.harasimiuk@intel.com>

--- scripts/actions/run-action-build.sh
+++ scripts/actions/run-action-build.sh
@@ -17,8 +17,8 @@ rm -rf build
 mkdir build
 
 skip_unit_tests="FALSE"
-if [ "${WORKFLOW}" = "CI" ]; then
+if [ "${WORKFLOW^^}" = "CI" ]; then
     skip_unit_tests="TRUE"
 fi
-docker run --rm -u `id -u`:`id -g` -v `pwd`:/src -w /src/build -e CC=${CC} -e CXX=${CXX} -t ${IMAGE} cmake -GNinja ../neo -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DSKIP_UNIT_TESTS=${skip_unit_tests} -DDO_NOT_RUN_AUB_TESTS=TRUE
+docker run --rm -u `id -u`:`id -g` -v `pwd`:/src -w /src/build -e CC=${CC} -e CXX=${CXX} -t ${IMAGE} cmake -GNinja ../neo -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DSKIP_UNIT_TESTS=${skip_unit_tests} -DDO_NOT_RUN_AUB_TESTS=TRUE -DDONT_CARE_OF_VIRTUALS=TRUE
 docker run --rm -u `id -u`:`id -g` -v `pwd`:/src -w /src/build -t ${IMAGE} ninja package
