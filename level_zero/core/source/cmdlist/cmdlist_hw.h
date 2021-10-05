3016ed1c395711a1a2b48d05836733f455c3899c
Maciej Plewka <maciej.plewka@intel.com>
2021-10-05 16:46:00 +0000

    Make clearComputeModePropertiesIfNeeded method virtual
    Signed-off-by: Maciej Plewka <maciej.plewka@intel.com>

--- level_zero/core/source/cmdlist/cmdlist_hw.h
+++ level_zero/core/source/cmdlist/cmdlist_hw.h
@@ -224,7 +224,7 @@ struct CommandListCoreFamily : CommandListImp {
     ze_result_t appendLaunchKernelSplit(ze_kernel_handle_t hKernel, const ze_group_count_t *pThreadGroupDimensions, ze_event_handle_t hEvent);
     ze_result_t prepareIndirectParams(const ze_group_count_t *pThreadGroupDimensions);
     void updateStreamProperties(Kernel &kernel, bool isMultiOsContextCapable, bool isCooperative);
-    void clearComputeModePropertiesIfNeeded(bool requiresCoherency, uint32_t numGrfRequired, uint32_t threadArbitrationPolicy);
+    virtual void clearComputeModePropertiesIfNeeded(bool requiresCoherency, uint32_t numGrfRequired, uint32_t threadArbitrationPolicy);
     void clearCommandsToPatch();
 
     void applyMemoryRangesBarrier(uint32_t numRanges, const size_t *pRangeSizes,
