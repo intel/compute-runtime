dc9bcb99c99493b75cebc23a5cf9d6e661f61f14
Jaime Arteaga <jaime.a.arteaga.molina@intel.com>
2021-10-06 00:37:16 +0000

    Avoid deadlock when evicting unused allocations
    Signed-off-by: Jaime Arteaga <jaime.a.arteaga.molina@intel.com>

--- shared/source/os_interface/linux/drm_buffer_object.cpp
+++ shared/source/os_interface/linux/drm_buffer_object.cpp
@@ -151,12 +151,12 @@ int BufferObject::exec(uint32_t used, size_t startOffset, unsigned int flags, bo
     int ret = this->drm->ioctl(DRM_IOCTL_I915_GEM_EXECBUFFER2, &execbuf);
 
     if (ret != 0) {
-        static_cast<DrmMemoryOperationsHandler *>(this->drm->getRootDeviceEnvironment().memoryOperationsInterface.get())->evictUnusedAllocations(false);
+        static_cast<DrmMemoryOperationsHandler *>(this->drm->getRootDeviceEnvironment().memoryOperationsInterface.get())->evictUnusedAllocations(false, true);
         ret = this->drm->ioctl(DRM_IOCTL_I915_GEM_EXECBUFFER2, &execbuf);
     }
 
     if (ret != 0) {
-        static_cast<DrmMemoryOperationsHandler *>(this->drm->getRootDeviceEnvironment().memoryOperationsInterface.get())->evictUnusedAllocations(true);
+        static_cast<DrmMemoryOperationsHandler *>(this->drm->getRootDeviceEnvironment().memoryOperationsInterface.get())->evictUnusedAllocations(true, true);
         ret = this->drm->ioctl(DRM_IOCTL_I915_GEM_EXECBUFFER2, &execbuf);
     }
 
