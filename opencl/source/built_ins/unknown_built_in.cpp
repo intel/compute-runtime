b488d67a0ef6b8b668b7a0120f5fac786e806a2e
Mateusz Jablonski <mateusz.jablonski@intel.com>
2021-10-07 10:07:49 +0000

    Move unknown builtin name to shared
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

--- opencl/source/built_ins/unknown_built_in.cpp
+++ opencl/source/built_ins/unknown_built_in.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019-2020 Intel Corporation
+ * Copyright (C) 2019-2021 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
@@ -10,10 +10,6 @@
 #include "opencl/source/built_ins/builtins_dispatch_builder.h"
 
 namespace NEO {
-
-const char *getUnknownBuiltinAsString(EBuiltInOps::Type builtin) {
-    return "unknown";
-}
 
 BuiltinDispatchInfoBuilder &BuiltInDispatchBuilderOp::getUnknownDispatchInfoBuilder(EBuiltInOps::Type operation, ClDevice &device) {
     throw std::runtime_error("getBuiltinDispatchInfoBuilder failed");
