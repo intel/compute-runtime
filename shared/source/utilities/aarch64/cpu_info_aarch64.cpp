091b3ac4f68e3ba0c0e5c097775bf78c4da84908
Artur Harasimiuk <artur.harasimiuk@intel.com>
2021-10-14 14:41:00 +0200

    cpu_info refactor
    Related-To: NEO-5853
    Signed-off-by: Artur Harasimiuk <artur.harasimiuk@intel.com>

---
+++ shared/source/utilities/aarch64/cpu_info_aarch64.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2021 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include "shared/source/utilities/cpu_info.h"
+
+namespace NEO {
+void CpuInfo::detect() const {
+}
+} // namespace NEO
