edc8f6c1cd57443e2ad2502ae185816b9c431e1d
Mateusz Jablonski <mateusz.jablonski@intel.com>
2021-11-15 13:28:48 +0000

    Remove gmock from mock context
    Related-To: NEO-4914
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

--- level_zero/core/test/unit_tests/mocks/mock_context.h
+++ level_zero/core/test/unit_tests/mocks/mock_context.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Intel Corporation
+ * Copyright (C) 2020-2021 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
@@ -7,12 +7,11 @@
 
 #pragma once
 
+#include "shared/test/common/test_macros/mock_method_macros.h"
+
 #include "level_zero/core/source/context/context.h"
-#include "level_zero/core/source/driver/driver_handle.h"
 #include "level_zero/core/test/unit_tests/mock.h"
 #include "level_zero/core/test/unit_tests/white_box.h"
-
-#include "gmock/gmock.h"
 
 namespace L0 {
 namespace ult {
@@ -27,204 +26,40 @@ struct Mock<Context> : public Context {
     Mock() = default;
     ~Mock() override = default;
 
-    MOCK_METHOD(ze_result_t,
-                destroy,
-                (),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                getStatus,
-                (),
-                (override));
-    MOCK_METHOD(DriverHandle *,
-                getDriverHandle,
-                (),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                allocHostMem,
-                (const ze_host_mem_alloc_desc_t *hostDesc,
-                 size_t size,
-                 size_t alignment,
-                 void **ptr),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                allocDeviceMem,
-                (ze_device_handle_t hDevice,
-                 const ze_device_mem_alloc_desc_t *deviceDesc,
-                 size_t size,
-                 size_t alignment,
-                 void **ptr),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                allocSharedMem,
-                (ze_device_handle_t hDevice,
-                 const ze_device_mem_alloc_desc_t *deviceDesc,
-                 const ze_host_mem_alloc_desc_t *hostDesc,
-                 size_t size,
-                 size_t alignment,
-                 void **ptr),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                makeMemoryResident,
-                (ze_device_handle_t hDevice,
-                 void *ptr,
-                 size_t size),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                evictMemory,
-                (ze_device_handle_t hDevice,
-                 void *ptr,
-                 size_t size),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                makeImageResident,
-                (ze_device_handle_t hDevice,
-                 ze_image_handle_t hImage),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                evictImage,
-                (ze_device_handle_t hDevice,
-                 ze_image_handle_t hImage),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                freeMem,
-                (const void *ptr),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                getMemAllocProperties,
-                (const void *ptr,
-                 ze_memory_allocation_properties_t *pMemAllocProperties,
-                 ze_device_handle_t *phDevice),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                getMemAddressRange,
-                (const void *ptr,
-                 void **pBase,
-                 size_t *pSize),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                getIpcMemHandle,
-                (const void *ptr,
-                 ze_ipc_mem_handle_t *pIpcHandle),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                closeIpcMemHandle,
-                (const void *ptr),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                openIpcMemHandle,
-                (ze_device_handle_t hDevice,
-                 ze_ipc_mem_handle_t handle,
-                 ze_ipc_memory_flags_t flags,
-                 void **ptr),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                createModule,
-                (ze_device_handle_t hDevice,
-                 const ze_module_desc_t *desc,
-                 ze_module_handle_t *phModule,
-                 ze_module_build_log_handle_t *phBuildLog),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                createSampler,
-                (ze_device_handle_t hDevice,
-                 const ze_sampler_desc_t *pDesc,
-                 ze_sampler_handle_t *phSampler),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                createCommandQueue,
-                (ze_device_handle_t hDevice,
-                 const ze_command_queue_desc_t *desc,
-                 ze_command_queue_handle_t *commandQueue),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                createCommandList,
-                (ze_device_handle_t hDevice,
-                 const ze_command_list_desc_t *desc,
-                 ze_command_list_handle_t *commandList),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                createCommandListImmediate,
-                (ze_device_handle_t hDevice,
-                 const ze_command_queue_desc_t *desc,
-                 ze_command_list_handle_t *commandList),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                activateMetricGroups,
-                (zet_device_handle_t hDevice,
-                 uint32_t count,
-                 zet_metric_group_handle_t *phMetricGroups),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                reserveVirtualMem,
-                (const void *pStart,
-                 size_t size,
-                 void **pptr),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                freeVirtualMem,
-                (const void *ptr,
-                 size_t size),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                queryVirtualMemPageSize,
-                (ze_device_handle_t hDevice,
-                 size_t size,
-                 size_t *pagesize),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                createPhysicalMem,
-                (ze_device_handle_t hDevice,
-                 ze_physical_mem_desc_t *desc,
-                 ze_physical_mem_handle_t *phPhysicalMemory),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                destroyPhysicalMem,
-                (ze_physical_mem_handle_t hPhysicalMemory),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                mapVirtualMem,
-                (const void *ptr,
-                 size_t size,
-                 ze_physical_mem_handle_t hPhysicalMemory,
-                 size_t offset,
-                 ze_memory_access_attribute_t access),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                unMapVirtualMem,
-                (const void *ptr,
-                 size_t size),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                setVirtualMemAccessAttribute,
-                (const void *ptr,
-                 size_t size,
-                 ze_memory_access_attribute_t access),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                getVirtualMemAccessAttribute,
-                (const void *ptr,
-                 size_t size,
-                 ze_memory_access_attribute_t *access,
-                 size_t *outSize),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                openEventPoolIpcHandle,
-                (ze_ipc_event_pool_handle_t hIpc,
-                 ze_event_pool_handle_t *phEventPool),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                createEventPool,
-                (const ze_event_pool_desc_t *desc,
-                 uint32_t numDevices,
-                 ze_device_handle_t *phDevices,
-                 ze_event_pool_handle_t *phEventPool),
-                (override));
-    MOCK_METHOD(ze_result_t,
-                createImage,
-                (ze_device_handle_t hDevice,
-                 const ze_image_desc_t *desc,
-                 ze_image_handle_t *phImage),
-                (override));
+    ADDMETHOD_NOBASE(destroy, ze_result_t, ZE_RESULT_SUCCESS, ());
+    ADDMETHOD_NOBASE(getStatus, ze_result_t, ZE_RESULT_SUCCESS, ());
+    ADDMETHOD_NOBASE(getDriverHandle, DriverHandle *, nullptr, ());
+    ADDMETHOD_NOBASE(allocHostMem, ze_result_t, ZE_RESULT_SUCCESS, (const ze_host_mem_alloc_desc_t *hostDesc, size_t size, size_t alignment, void **ptr));
+    ADDMETHOD_NOBASE(allocDeviceMem, ze_result_t, ZE_RESULT_SUCCESS, (ze_device_handle_t hDevice, const ze_device_mem_alloc_desc_t *deviceDesc, size_t size, size_t alignment, void **ptr));
+    ADDMETHOD_NOBASE(allocSharedMem, ze_result_t, ZE_RESULT_SUCCESS, (ze_device_handle_t hDevice, const ze_device_mem_alloc_desc_t *deviceDesc, const ze_host_mem_alloc_desc_t *hostDesc, size_t size, size_t alignment, void **ptr));
+    ADDMETHOD_NOBASE(makeMemoryResident, ze_result_t, ZE_RESULT_SUCCESS, (ze_device_handle_t hDevice, void *ptr, size_t size));
+    ADDMETHOD_NOBASE(evictMemory, ze_result_t, ZE_RESULT_SUCCESS, (ze_device_handle_t hDevice, void *ptr, size_t size));
+    ADDMETHOD_NOBASE(makeImageResident, ze_result_t, ZE_RESULT_SUCCESS, (ze_device_handle_t hDevice, ze_image_handle_t hImage));
+    ADDMETHOD_NOBASE(evictImage, ze_result_t, ZE_RESULT_SUCCESS, (ze_device_handle_t hDevice, ze_image_handle_t hImage));
+    ADDMETHOD_NOBASE(freeMem, ze_result_t, ZE_RESULT_SUCCESS, (const void *ptr));
+    ADDMETHOD_NOBASE(getMemAllocProperties, ze_result_t, ZE_RESULT_SUCCESS, (const void *ptr, ze_memory_allocation_properties_t *pMemAllocProperties, ze_device_handle_t *phDevice));
+    ADDMETHOD_NOBASE(getMemAddressRange, ze_result_t, ZE_RESULT_SUCCESS, (const void *ptr, void **pBase, size_t *pSize));
+    ADDMETHOD_NOBASE(getIpcMemHandle, ze_result_t, ZE_RESULT_SUCCESS, (const void *ptr, ze_ipc_mem_handle_t *pIpcHandle));
+    ADDMETHOD_NOBASE(closeIpcMemHandle, ze_result_t, ZE_RESULT_SUCCESS, (const void *ptr));
+    ADDMETHOD_NOBASE(openIpcMemHandle, ze_result_t, ZE_RESULT_SUCCESS, (ze_device_handle_t hDevice, ze_ipc_mem_handle_t handle, ze_ipc_memory_flags_t flags, void **ptr));
+    ADDMETHOD_NOBASE(createModule, ze_result_t, ZE_RESULT_SUCCESS, (ze_device_handle_t hDevice, const ze_module_desc_t *desc, ze_module_handle_t *phModule, ze_module_build_log_handle_t *phBuildLog));
+    ADDMETHOD_NOBASE(createSampler, ze_result_t, ZE_RESULT_SUCCESS, (ze_device_handle_t hDevice, const ze_sampler_desc_t *pDesc, ze_sampler_handle_t *phSampler));
+    ADDMETHOD_NOBASE(createCommandQueue, ze_result_t, ZE_RESULT_SUCCESS, (ze_device_handle_t hDevice, const ze_command_queue_desc_t *desc, ze_command_queue_handle_t *commandQueue));
+    ADDMETHOD_NOBASE(createCommandList, ze_result_t, ZE_RESULT_SUCCESS, (ze_device_handle_t hDevice, const ze_command_list_desc_t *desc, ze_command_list_handle_t *commandList));
+    ADDMETHOD_NOBASE(createCommandListImmediate, ze_result_t, ZE_RESULT_SUCCESS, (ze_device_handle_t hDevice, const ze_command_queue_desc_t *desc, ze_command_list_handle_t *commandList));
+    ADDMETHOD_NOBASE(activateMetricGroups, ze_result_t, ZE_RESULT_SUCCESS, (zet_device_handle_t hDevice, uint32_t count, zet_metric_group_handle_t *phMetricGroups));
+    ADDMETHOD_NOBASE(reserveVirtualMem, ze_result_t, ZE_RESULT_SUCCESS, (const void *pStart, size_t size, void **pptr));
+    ADDMETHOD_NOBASE(freeVirtualMem, ze_result_t, ZE_RESULT_SUCCESS, (const void *ptr, size_t size));
+    ADDMETHOD_NOBASE(queryVirtualMemPageSize, ze_result_t, ZE_RESULT_SUCCESS, (ze_device_handle_t hDevice, size_t size, size_t *pagesize));
+    ADDMETHOD_NOBASE(createPhysicalMem, ze_result_t, ZE_RESULT_SUCCESS, (ze_device_handle_t hDevice, ze_physical_mem_desc_t *desc, ze_physical_mem_handle_t *phPhysicalMemory));
+    ADDMETHOD_NOBASE(destroyPhysicalMem, ze_result_t, ZE_RESULT_SUCCESS, (ze_physical_mem_handle_t hPhysicalMemory));
+    ADDMETHOD_NOBASE(mapVirtualMem, ze_result_t, ZE_RESULT_SUCCESS, (const void *ptr, size_t size, ze_physical_mem_handle_t hPhysicalMemory, size_t offset, ze_memory_access_attribute_t access));
+    ADDMETHOD_NOBASE(unMapVirtualMem, ze_result_t, ZE_RESULT_SUCCESS, (const void *ptr, size_t size));
+    ADDMETHOD_NOBASE(setVirtualMemAccessAttribute, ze_result_t, ZE_RESULT_SUCCESS, (const void *ptr, size_t size, ze_memory_access_attribute_t access));
+    ADDMETHOD_NOBASE(getVirtualMemAccessAttribute, ze_result_t, ZE_RESULT_SUCCESS, (const void *ptr, size_t size, ze_memory_access_attribute_t *access, size_t *outSize));
+    ADDMETHOD_NOBASE(openEventPoolIpcHandle, ze_result_t, ZE_RESULT_SUCCESS, (ze_ipc_event_pool_handle_t hIpc, ze_event_pool_handle_t *phEventPool));
+    ADDMETHOD_NOBASE(createEventPool, ze_result_t, ZE_RESULT_SUCCESS, (const ze_event_pool_desc_t *desc, uint32_t numDevices, ze_device_handle_t *phDevices, ze_event_pool_handle_t *phEventPool));
+    ADDMETHOD_NOBASE(createImage, ze_result_t, ZE_RESULT_SUCCESS, (ze_device_handle_t hDevice, const ze_image_desc_t *desc, ze_image_handle_t *phImage));
 };
 
 } // namespace ult
