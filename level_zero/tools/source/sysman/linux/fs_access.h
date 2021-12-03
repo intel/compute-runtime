feae44bce89bd575d24830430f361228d636fc5c
Joshua Santosh Ranjan <joshua.santosh.ranjan@intel.com>
2021-12-03 15:23:55 +0000

    Added Fabric RAS error support
    fabric error counts are read from sysfs nodes
    
    Related-To: LOCI-2613
    
    Signed-off-by: Joshua Santosh Ranjan <joshua.santosh.ranjan@intel.com>

--- level_zero/tools/source/sysman/linux/fs_access.h
+++ level_zero/tools/source/sysman/linux/fs_access.h
@@ -105,7 +105,7 @@ class SysfsAccess : protected FsAccess {
     ze_result_t write(const std::string file, std::vector<std::string> val);
 
     MOCKABLE_VIRTUAL ze_result_t scanDirEntries(const std::string path, std::vector<std::string> &list);
-    ze_result_t readSymLink(const std::string path, std::string &buf) override;
+    MOCKABLE_VIRTUAL ze_result_t readSymLink(const std::string path, std::string &buf) override;
     ze_result_t getRealPath(const std::string path, std::string &buf) override;
     MOCKABLE_VIRTUAL ze_result_t bindDevice(const std::string device);
     MOCKABLE_VIRTUAL ze_result_t unbindDevice(const std::string device);
