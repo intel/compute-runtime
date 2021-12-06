fca0d03c90f672bd8d5236b147d224e3d1e03078
Hazubski, Filip <filip.hazubski@intel.com>
2021-12-06 16:54:19 +0000

    Remove reporting extension cl_intel_global_float_atomics
    Signed-off-by: Hazubski, Filip <filip.hazubski@intel.com>

--- shared/source/xe_hpc_core/hw_helper_xe_hpc_core.cpp
+++ shared/source/xe_hpc_core/hw_helper_xe_hpc_core.cpp
@@ -276,7 +276,6 @@ std::string HwHelperHw<Family>::getExtensions() const {
 
     extensions += "cl_intel_create_buffer_with_properties ";
     extensions += "cl_intel_dot_accumulate ";
-    extensions += "cl_intel_global_float_atomics ";
     extensions += "cl_intel_subgroup_local_block_io ";
     extensions += "cl_intel_subgroup_matrix_multiply_accumulate_for_PVC ";
     extensions += "cl_khr_subgroup_named_barrier ";
