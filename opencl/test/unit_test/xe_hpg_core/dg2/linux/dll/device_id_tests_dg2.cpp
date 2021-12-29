7e03d9220e426cbfab677128335ae400520a962d
Mateusz Jablonski <mateusz.jablonski@intel.com>
2021-12-29 16:35:40 +0000

    Add DG2 device ids
    Related-To: NEO-5555
    
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

--- opencl/test/unit_test/xe_hpg_core/dg2/linux/dll/device_id_tests_dg2.cpp
+++ opencl/test/unit_test/xe_hpg_core/dg2/linux/dll/device_id_tests_dg2.cpp
@@ -10,9 +10,25 @@
 using namespace NEO;
 
 TEST_F(DeviceIdTests, GivenDg2SupportedDeviceIdThenDeviceDescriptorTableExists) {
-    std::array<DeviceDescriptor, 2> expectedDescriptors = {{
+    std::array<DeviceDescriptor, 18> expectedDescriptors = {{
         {0x4F80, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
+        {0x4F81, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
+        {0x4F82, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
+        {0x4F83, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
+        {0x4F84, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
         {0x4F87, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
+        {0x4F88, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
+        {0x5690, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
+        {0x5691, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
+        {0x5692, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
+        {0x5693, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
+        {0x5694, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
+        {0x5695, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
+        {0x56A0, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
+        {0x56A1, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
+        {0x56A2, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
+        {0x56A5, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
+        {0x56A6, &DG2_CONFIG::hwInfo, &DG2_CONFIG::setupHardwareInfo, GTTYPE_GT4},
     }};
 
     testImpl(expectedDescriptors);
