17b0ddd2a3e8db7778cc5dbdc4c5d7a8f4c6c19c
Artur Harasimiuk <artur.harasimiuk@intel.com>
2022-01-20 00:17:33 +0000

    ULT execution refactor
    Add targets to run shared, OpenCL and L0 tests separately. Bind all
    these to run_unit_tests.
    
    Signed-off-by: Artur Harasimiuk <artur.harasimiuk@intel.com>

--- cmake/run_ult_target.cmake
+++ cmake/run_ult_target.cmake
@@ -11,31 +11,51 @@ list(GET unit_test_config 2 subslices)
 list(GET unit_test_config 3 eu_per_ss)
 list(GET unit_test_config 4 revision_id)
 
-add_custom_target(run_${product}_${revision_id}_unit_tests ALL DEPENDS unit_tests)
-set_target_properties(run_${product}_${revision_id}_unit_tests PROPERTIES FOLDER "${PLATFORM_SPECIFIC_TEST_TARGETS_FOLDER}/${product}/${revision_id}")
 if(NOT NEO_SKIP_SHARED_UNIT_TESTS)
   unset(GTEST_OUTPUT)
   if(DEFINED GTEST_OUTPUT_DIR)
     set(GTEST_OUTPUT "--gtest_output=json:${GTEST_OUTPUT_DIR}/shared_${product}_${revision_id}_unit_tests_results.json")
     message(STATUS "GTest output set to ${GTEST_OUTPUT}")
   endif()
+
+  if(NOT TARGET run_shared_tests)
+    add_custom_target(run_shared_tests)
+    set_target_properties(run_shared_tests PROPERTIES FOLDER ${PLATFORM_SPECIFIC_TEST_TARGETS_FOLDER})
+    add_dependencies(run_unit_tests run_shared_tests)
+  endif()
+
+  add_custom_target(run_${product}_${revision_id}_shared_tests DEPENDS unit_tests)
+  set_target_properties(run_${product}_${revision_id}_shared_tests PROPERTIES FOLDER "${PLATFORM_SPECIFIC_TEST_TARGETS_FOLDER}/${product}/${revision_id}")
+
   add_custom_command(
-                     TARGET run_${product}_${revision_id}_unit_tests
+                     TARGET run_${product}_${revision_id}_shared_tests
                      POST_BUILD
                      COMMAND WORKING_DIRECTORY ${TargetDir}
                      COMMAND echo Running neo_shared_tests ${target} ${slices}x${subslices}x${eu_per_ss} in ${TargetDir}/${product}
                      COMMAND ${NEO_RUN_INTERCEPTOR_LIST} $<TARGET_FILE:neo_shared_tests> --product ${product} --slices ${slices} --subslices ${subslices} --eu_per_ss ${eu_per_ss} ${GTEST_EXCEPTION_OPTIONS} --gtest_repeat=${GTEST_REPEAT} ${GTEST_SHUFFLE} ${GTEST_OUTPUT} ${NEO_TESTS_LISTENER_OPTION} ${GTEST_FILTER_OPTION} --rev_id ${revision_id}
   )
 
+  add_dependencies(run_shared_tests run_${product}_${revision_id}_shared_tests)
 endif()
+
 if(NOT NEO_SKIP_OCL_UNIT_TESTS)
   unset(GTEST_OUTPUT)
   if(DEFINED GTEST_OUTPUT_DIR)
     set(GTEST_OUTPUT "--gtest_output=json:${GTEST_OUTPUT_DIR}/ocl_${product}_${revision_id}_unit_tests_results.json")
     message(STATUS "GTest output set to ${GTEST_OUTPUT}")
   endif()
+
+  if(NOT TARGET run_ocl_tests)
+    add_custom_target(run_ocl_tests)
+    set_target_properties(run_ocl_tests PROPERTIES FOLDER ${PLATFORM_SPECIFIC_TEST_TARGETS_FOLDER})
+    add_dependencies(run_unit_tests run_ocl_tests)
+  endif()
+
+  add_custom_target(run_${product}_${revision_id}_ocl_tests DEPENDS unit_tests)
+  set_target_properties(run_${product}_${revision_id}_ocl_tests PROPERTIES FOLDER "${PLATFORM_SPECIFIC_TEST_TARGETS_FOLDER}/${product}/${revision_id}")
+
   add_custom_command(
-                     TARGET run_${product}_${revision_id}_unit_tests
+                     TARGET run_${product}_${revision_id}_ocl_tests
                      POST_BUILD
                      COMMAND WORKING_DIRECTORY ${TargetDir}
                      COMMAND echo Running igdrcl_tests ${target} ${slices}x${subslices}x${eu_per_ss} in ${TargetDir}/${product}
@@ -49,7 +69,7 @@ if(NOT NEO_SKIP_OCL_UNIT_TESTS)
       message(STATUS "GTest output set to ${GTEST_OUTPUT}")
     endif()
     add_custom_command(
-                       TARGET run_${product}_${revision_id}_unit_tests
+                       TARGET run_${product}_${revision_id}_ocl_tests
                        POST_BUILD
                        COMMAND WORKING_DIRECTORY ${TargetDir}
                        COMMAND echo Running igdrcl_tests with App Verifier
@@ -57,6 +77,8 @@ if(NOT NEO_SKIP_OCL_UNIT_TESTS)
                        COMMAND echo App Verifier returned: %errorLevel%
     )
   endif()
+
+  add_dependencies(run_ocl_tests run_${product}_${revision_id}_ocl_tests)
 endif()
 
 if(NOT NEO_SKIP_L0_UNIT_TESTS AND BUILD_WITH_L0)
@@ -75,8 +97,18 @@ if(NOT NEO_SKIP_L0_UNIT_TESTS AND BUILD_WITH_L0)
     set(GTEST_OUTPUT_EXP "--gtest_output=json:${GTEST_OUTPUT_DIR}/ze_intel_gpu_exp_${product}_${revision_id}_unit_tests_results.json")
     message(STATUS "GTest output exp set to ${GTEST_OUTPUT_EXP}")
   endif()
+
+  if(NOT TARGET run_l0_tests)
+    add_custom_target(run_l0_tests)
+    set_target_properties(run_l0_tests PROPERTIES FOLDER ${PLATFORM_SPECIFIC_TEST_TARGETS_FOLDER})
+    add_dependencies(run_unit_tests run_l0_tests)
+  endif()
+
+  add_custom_target(run_${product}_${revision_id}_l0_tests DEPENDS unit_tests)
+  set_target_properties(run_${product}_${revision_id}_l0_tests PROPERTIES FOLDER "${PLATFORM_SPECIFIC_TEST_TARGETS_FOLDER}/${product}/${revision_id}")
+
   add_custom_command(
-                     TARGET run_${product}_${revision_id}_unit_tests
+                     TARGET run_${product}_${revision_id}_l0_tests
                      POST_BUILD
                      COMMAND WORKING_DIRECTORY ${TargetDir}
                      COMMAND echo Running ze_intel_gpu_core_tests ${target} ${slices}x${subslices}x${eu_per_ss} in ${TargetDir}/${product}
@@ -86,6 +118,6 @@ if(NOT NEO_SKIP_L0_UNIT_TESTS AND BUILD_WITH_L0)
                      COMMAND echo Running ze_intel_gpu_exp_tests ${target} ${slices}x${subslices}x${eu_per_ss} in ${TargetDir}/${product}
                      COMMAND ${NEO_RUN_INTERCEPTOR_LIST} $<TARGET_FILE:ze_intel_gpu_exp_tests> --product ${product} --slices ${slices} --subslices ${subslices} --eu_per_ss ${eu_per_ss} ${GTEST_EXCEPTION_OPTIONS} --gtest_repeat=${GTEST_REPEAT} ${GTEST_SHUFFLE} ${GTEST_OUTPUT_EXP} ${NEO_TESTS_LISTENER_OPTION} ${GTEST_FILTER_OPTION} --rev_id ${revision_id}
   )
-endif()
 
-add_dependencies(run_unit_tests run_${product}_${revision_id}_unit_tests)
+  add_dependencies(run_l0_tests run_${product}_${revision_id}_l0_tests)
+endif()
