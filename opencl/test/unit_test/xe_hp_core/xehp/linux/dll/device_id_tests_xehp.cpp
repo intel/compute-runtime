09346690e827fe94bd0829db9626ca39ce6875ca
Bartosz Dunajski <bartosz.dunajski@intel.com>
2022-01-26 13:19:44 +0000

    Add XE_HP_SDV device ids
    Signed-off-by: Bartosz Dunajski <bartosz.dunajski@intel.com>

--- opencl/test/unit_test/xe_hp_core/xehp/linux/dll/device_id_tests_xehp.cpp
+++ opencl/test/unit_test/xe_hp_core/xehp/linux/dll/device_id_tests_xehp.cpp
@@ -10,7 +10,22 @@
 using namespace NEO;
 
 TEST_F(DeviceIdTests, GivenXeHpSdvSupportedDeviceIdThenConfigIsCorrect) {
-    std::array<DeviceDescriptor, 1> expectedDescriptors = {{{0x0201, &XE_HP_SDV_CONFIG::hwInfo, &XE_HP_SDV_CONFIG::setupHardwareInfo}}};
+    std::array<DeviceDescriptor, 16> expectedDescriptors = {{{0x0201, &XE_HP_SDV_CONFIG::hwInfo, &XE_HP_SDV_CONFIG::setupHardwareInfo},
+                                                             {0x0202, &XE_HP_SDV_CONFIG::hwInfo, &XE_HP_SDV_CONFIG::setupHardwareInfo},
+                                                             {0x0203, &XE_HP_SDV_CONFIG::hwInfo, &XE_HP_SDV_CONFIG::setupHardwareInfo},
+                                                             {0x0204, &XE_HP_SDV_CONFIG::hwInfo, &XE_HP_SDV_CONFIG::setupHardwareInfo},
+                                                             {0x0205, &XE_HP_SDV_CONFIG::hwInfo, &XE_HP_SDV_CONFIG::setupHardwareInfo},
+                                                             {0x0206, &XE_HP_SDV_CONFIG::hwInfo, &XE_HP_SDV_CONFIG::setupHardwareInfo},
+                                                             {0x0207, &XE_HP_SDV_CONFIG::hwInfo, &XE_HP_SDV_CONFIG::setupHardwareInfo},
+                                                             {0x0208, &XE_HP_SDV_CONFIG::hwInfo, &XE_HP_SDV_CONFIG::setupHardwareInfo},
+                                                             {0x0209, &XE_HP_SDV_CONFIG::hwInfo, &XE_HP_SDV_CONFIG::setupHardwareInfo},
+                                                             {0x020A, &XE_HP_SDV_CONFIG::hwInfo, &XE_HP_SDV_CONFIG::setupHardwareInfo},
+                                                             {0x020B, &XE_HP_SDV_CONFIG::hwInfo, &XE_HP_SDV_CONFIG::setupHardwareInfo},
+                                                             {0x020C, &XE_HP_SDV_CONFIG::hwInfo, &XE_HP_SDV_CONFIG::setupHardwareInfo},
+                                                             {0x020D, &XE_HP_SDV_CONFIG::hwInfo, &XE_HP_SDV_CONFIG::setupHardwareInfo},
+                                                             {0x020E, &XE_HP_SDV_CONFIG::hwInfo, &XE_HP_SDV_CONFIG::setupHardwareInfo},
+                                                             {0x020F, &XE_HP_SDV_CONFIG::hwInfo, &XE_HP_SDV_CONFIG::setupHardwareInfo},
+                                                             {0x0210, &XE_HP_SDV_CONFIG::hwInfo, &XE_HP_SDV_CONFIG::setupHardwareInfo}}};
 
     testImpl(expectedDescriptors);
 }
