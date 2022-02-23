faba32daf04bcf4baf8e046804347eed6d3de9a0
Compute-Runtime-Validation <compute-runtime-validation@intel.com>
2022-02-23 06:04:41 +0100

    Revert "Fix generating localIDs in zebin"
    This reverts commit 441fe1f589a9ae361086035a607d0e27217f626f.
    
    Signed-off-by: Compute-Runtime-Validation <compute-runtime-validation@intel.com>

--- shared/test/unit_test/device_binary_format/zebin_decoder_tests.cpp
+++ shared/test/unit_test/device_binary_format/zebin_decoder_tests.cpp
@@ -3808,7 +3808,7 @@ kernels:
     EXPECT_TRUE(warnings.empty()) << warnings;
 }
 
-TEST(PopulateArgDescriptorPerThreadPayload, GivenArgTypeLocalIdWhenSizeIsValidThenCalculateNumChannelAndSetEmitLocalIdAccordingly) {
+TEST(PopulateArgDescriptorPerThreadPayload, GivenArgTypeLocalIdWhenSizeIsValidThenCalculateNumChannelAccordingly) {
     uint32_t simdSizes[] = {8, 16, 32};
     uint32_t numChannelsOpts[] = {1, 2, 3};
 
@@ -3849,11 +3849,6 @@ TEST(PopulateArgDescriptorPerThreadPayload, GivenArgTypeLocalIdWhenSizeIsValidTh
             ASSERT_EQ(1U, programInfo.kernelInfos.size());
             EXPECT_EQ(numChannels, programInfo.kernelInfos[0]->kernelDescriptor.kernelAttributes.numLocalIdChannels) << warnings << "simd : " << simdSize << ", num channels : " << numChannels;
             EXPECT_EQ(simdSize, programInfo.kernelInfos[0]->kernelDescriptor.kernelAttributes.simdSize) << warnings << "simd : " << simdSize << ", num channels : " << numChannels;
-
-            const auto &emitLocalId = programInfo.kernelInfos[0]->kernelDescriptor.kernelAttributes.localId;
-            EXPECT_EQ(static_cast<uint8_t>(numChannels > 0), emitLocalId[0]);
-            EXPECT_EQ(static_cast<uint8_t>(numChannels > 1), emitLocalId[1]);
-            EXPECT_EQ(static_cast<uint8_t>(numChannels > 2), emitLocalId[2]);
         }
     }
 }
@@ -3924,7 +3919,7 @@ kernels:
     EXPECT_TRUE(warnings.empty()) << warnings;
 }
 
-TEST(PopulateArgDescriptorPerThreadPayload, GivenArgTypePackedLocalIdWhenSizeIsValidThenCalculateNumChannelAndSetEmitLocalIdAccordingly) {
+TEST(PopulateArgDescriptorPerThreadPayload, GivenArgTypePackedLocalIdWhenSizeIsValidThenCalculateNumChannelAccordingly) {
     uint32_t simdSizes[] = {1};
     uint32_t numChannelsOpts[] = {1, 2, 3};
 
@@ -3965,11 +3960,6 @@ TEST(PopulateArgDescriptorPerThreadPayload, GivenArgTypePackedLocalIdWhenSizeIsV
             ASSERT_EQ(1U, programInfo.kernelInfos.size());
             EXPECT_EQ(numChannels, programInfo.kernelInfos[0]->kernelDescriptor.kernelAttributes.numLocalIdChannels) << warnings << "simd : " << simdSize << ", num channels : " << numChannels;
             EXPECT_EQ(simdSize, programInfo.kernelInfos[0]->kernelDescriptor.kernelAttributes.simdSize) << warnings << "simd : " << simdSize << ", num channels : " << numChannels;
-
-            const auto &emitLocalId = programInfo.kernelInfos[0]->kernelDescriptor.kernelAttributes.localId;
-            EXPECT_EQ(static_cast<uint8_t>(numChannels > 0), emitLocalId[0]);
-            EXPECT_EQ(static_cast<uint8_t>(numChannels > 1), emitLocalId[1]);
-            EXPECT_EQ(static_cast<uint8_t>(numChannels > 2), emitLocalId[2]);
         }
     }
 }
