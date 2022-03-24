a783f463c42a11e74cbbee9a8006ca48bc5beba9
Mateusz Hoppe <mateusz.hoppe@intel.com>
2022-03-24 19:20:25 +0000

    Enable bindless aub test for all platforms
    Related-To: NEO-6515
    
    Signed-off-by: Mateusz Hoppe <mateusz.hoppe@intel.com>

--- level_zero/core/test/aub_tests/bindless/bindless_kernel_aub_tests.cpp
+++ level_zero/core/test/aub_tests/bindless/bindless_kernel_aub_tests.cpp
@@ -58,7 +58,7 @@ struct L0BindlessAub : Test<AUBFixtureL0> {
     ModuleImp *module = nullptr;
 };
 
-HWTEST2_F(L0BindlessAub, GivenBindlessKernelWhenExecutedThenOutputIsCorrect, IsAtMostXeHpgCore) {
+HWTEST_F(L0BindlessAub, GivenBindlessKernelWhenExecutedThenOutputIsCorrect) {
     constexpr size_t bufferSize = MemoryConstants::pageSize;
     const uint32_t groupSize[] = {32, 1, 1};
     const uint32_t groupCount[] = {bufferSize / 32, 1, 1};
