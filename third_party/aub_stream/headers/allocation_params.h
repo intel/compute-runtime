489dd4dd03c9b7f3753fb15b23778b9677c6ac28
Bartosz Dunajski <bartosz.dunajski@intel.com>
2022-04-12 13:27:35 +0000

    Update aub_stream interface
    Signed-off-by: Bartosz Dunajski <bartosz.dunajski@intel.com>

--- third_party/aub_stream/headers/allocation_params.h
+++ third_party/aub_stream/headers/allocation_params.h
@@ -15,6 +15,7 @@ struct AllocationParams {
     AllocationParams() = delete;
     AllocationParams(uint64_t gfxAddress, const void *memory, size_t size, uint32_t memoryBanks, int hint, size_t pageSize)
         : gfxAddress(gfxAddress), size(size), pageSize(pageSize), memoryBanks(memoryBanks), hint(hint), memory(memory) {
+        additionalParams = {};
     }
     uint64_t gfxAddress = 0;
     size_t size = 0;
@@ -24,7 +25,9 @@ struct AllocationParams {
     const void *memory = nullptr;
 
     struct AdditionalParams {
-        bool compressionEnabled = false;
+        bool compressionEnabled :1;
+        bool uncached : 1;
+        bool padding : 6;
     } additionalParams;
 };
 
