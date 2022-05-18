ab4a45b2719b55f63dfb41af575512f851ae637d
Bartosz Dunajski <bartosz.dunajski@intel.com>
2022-05-18 11:11:04 +0000

    Update PVC device ids
    Signed-off-by: Bartosz Dunajski <bartosz.dunajski@intel.com>

--- opencl/test/unit_test/xe_hpc_core/pvc/linux/dll/device_id_tests_pvc.cpp
+++ opencl/test/unit_test/xe_hpc_core/pvc/linux/dll/device_id_tests_pvc.cpp
@@ -10,11 +10,15 @@
 using namespace NEO;
 
 TEST_F(DeviceIdTests, GivenPvcSupportedDeviceIdThenConfigIsCorrect) {
-    std::array<DeviceDescriptor, 4> expectedDescriptors = {{
+    std::array<DeviceDescriptor, 8> expectedDescriptors = {{
         {0x0BD0, &PVC_CONFIG::hwInfo, &PVC_CONFIG::setupHardwareInfo},
         {0x0BD5, &PVC_CONFIG::hwInfo, &PVC_CONFIG::setupHardwareInfo},
         {0x0BD6, &PVC_CONFIG::hwInfo, &PVC_CONFIG::setupHardwareInfo},
+        {0x0BD7, &PVC_CONFIG::hwInfo, &PVC_CONFIG::setupHardwareInfo},
         {0x0BD8, &PVC_CONFIG::hwInfo, &PVC_CONFIG::setupHardwareInfo},
+        {0x0BD9, &PVC_CONFIG::hwInfo, &PVC_CONFIG::setupHardwareInfo},
+        {0x0BDA, &PVC_CONFIG::hwInfo, &PVC_CONFIG::setupHardwareInfo},
+        {0x0BDB, &PVC_CONFIG::hwInfo, &PVC_CONFIG::setupHardwareInfo},
     }};
 
     testImpl(expectedDescriptors);
