625f88d64db5130bf91f09ab0691196245b05573
Baj, Tomasz <tomasz.baj@intel.com>
2022-09-28 14:33:16 +0000

    Move API specific logic directly to getDefaultCompilerCacheConfig function
    Signed-off-by: Baj, Tomasz <tomasz.baj@intel.com>

--- opencl/test/unit_test/compiler_interface/default_cl_cache_config_tests.cpp
+++ opencl/test/unit_test/compiler_interface/default_cl_cache_config_tests.cpp
@@ -1,23 +1,22 @@
 /*
- * Copyright (C) 2019-2021 Intel Corporation
+ * Copyright (C) 2019-2022 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
  */
 
+#include "shared/source/compiler_interface/default_cache_config.h"
 #include "shared/test/common/test_macros/test.h"
 
-#include "opencl/source/compiler_interface/default_cl_cache_config.h"
-
 TEST(CompilerCache, GivenDefaultClCacheConfigThenValuesAreProperlyPopulated) {
-    auto cacheConfig = NEO::getDefaultClCompilerCacheConfig();
+    auto cacheConfig = NEO::getDefaultCompilerCacheConfig();
     EXPECT_STREQ("cl_cache", cacheConfig.cacheDir.c_str());
     EXPECT_STREQ(".cl_cache", cacheConfig.cacheFileExtension.c_str());
     EXPECT_TRUE(cacheConfig.enabled);
 }
 
 TEST(CompilerCacheTests, GivenExistingConfigWhenLoadingFromCacheThenBinaryIsLoaded) {
-    NEO::CompilerCache cache(NEO::getDefaultClCompilerCacheConfig());
+    NEO::CompilerCache cache(NEO::getDefaultCompilerCacheConfig());
     static const char *hash = "SOME_HASH";
     std::unique_ptr<char> data(new char[32]);
     for (size_t i = 0; i < 32; i++)
