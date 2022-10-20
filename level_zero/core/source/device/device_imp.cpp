4a3862b51490c42af16f3fce418b3c4c0bc67f8a
Compute-Runtime-Validation <compute-runtime-validation@intel.com>
2022-10-20 06:18:43 +0200

    Revert "Add ze_eu_count_t to get total number of EUs"
    This reverts commit 2b963c7359fd37d3542378829ed194325ed35f9b.
    
    Signed-off-by: Compute-Runtime-Validation <compute-runtime-validation@intel.com>

--- level_zero/core/source/device/device_imp.cpp
+++ level_zero/core/source/device/device_imp.cpp
@@ -744,19 +744,6 @@ ze_result_t DeviceImp::getProperties(ze_device_properties_t *pDeviceProperties) 
         }
     }
 
-    if (pDeviceProperties->pNext) {
-        ze_base_desc_t *extendedDesc = reinterpret_cast<ze_base_desc_t *>(pDeviceProperties->pNext);
-        if (extendedDesc->stype == ZE_STRUCTURE_TYPE_EU_COUNT_EXT) {
-            ze_eu_count_ext_t *zeEuCountDesc = reinterpret_cast<ze_eu_count_ext_t *>(extendedDesc);
-            uint32_t numTotalEUs = hardwareInfo.gtSystemInfo.MaxEuPerSubSlice * hardwareInfo.gtSystemInfo.SubSliceCount * hardwareInfo.gtSystemInfo.SliceCount;
-
-            if (isImplicitScalingCapable()) {
-                numTotalEUs *= neoDevice->getNumGenericSubDevices();
-            }
-            zeEuCountDesc->numTotalEUs = numTotalEUs;
-        }
-    }
-
     return ZE_RESULT_SUCCESS;
 }
 
