8ffad108f35c63faf693ac96fab7d38082968a26
ocldev <ocldev@intel.com>
2022-11-04 10:47:42 +0100

    buildConfig update
    Signed-off-by: ocldev <ocldev@intel.com>

--- Jenkinsfile
+++ Jenkinsfile
@@ -8,3 +8,8 @@
  */
 
 dependenciesRevision='a43fff6b811898c051b3896847d8cd196323dfc2-1977'
+buildConfig = [
+	'master-build': ['gcc7', 'gcc8', 'gcc9', 'gcc11'],
+	'master-gate': ['gcc7', 'gcc8', 'gcc9', 'gcc10', 'gcc11', 'gcc12'],
+	'branch': 'master',
+]
--- version.cmake
+++ version.cmake
@@ -4,74 +4,14 @@
 # SPDX-License-Identifier: MIT
 #
 
-if(UNIX)
-  find_program(GIT NAMES git)
-  find_program(PYTHON_EXECUTABLE NAMES "python3")
-  if((NOT "${GIT}" STREQUAL "GIT-NOTFOUND") AND (NOT "${PYTHON_EXECUTABLE}" STREQUAL "PYTHON_EXECUTABLE-NOTFOUND"))
-    if(IS_DIRECTORY ${NEO_SOURCE_DIR}/.git)
-      set(GIT_arg --git-dir=${NEO_SOURCE_DIR}/.git show -s --format=%ct)
-      execute_process(
-                      COMMAND ${GIT} ${GIT_arg}
-                      OUTPUT_VARIABLE GIT_output
-                      OUTPUT_STRIP_TRAILING_WHITESPACE
-      )
-      set(PYTHON_arg ${CMAKE_CURRENT_SOURCE_DIR}/scripts/neo_ww_calculator.py ${GIT_output})
-      execute_process(
-                      COMMAND ${PYTHON_EXECUTABLE} ${PYTHON_arg}
-                      OUTPUT_VARIABLE VERSION_output
-                      OUTPUT_STRIP_TRAILING_WHITESPACE
-      )
-      string(REPLACE "." ";" VERSION_list ${VERSION_output})
-    endif()
-  else()
-    message(WARNING "Unable to determine OpenCL major.minor version. Defaulting to 1.0")
-  endif()
-
-  if(NOT DEFINED NEO_OCL_VERSION_MAJOR)
-    if(NOT DEFINED VERSION_list)
-      set(NEO_OCL_VERSION_MAJOR 1)
-    else()
-      list(GET VERSION_list 0 NEO_OCL_VERSION_MAJOR)
-      message(STATUS "Computed OpenCL version major is: ${NEO_OCL_VERSION_MAJOR}")
-    endif()
-  endif()
-
-  if(NOT DEFINED NEO_OCL_VERSION_MINOR)
-    if(NOT DEFINED VERSION_list)
-      set(NEO_OCL_VERSION_MINOR 0)
-    else()
-      list(GET VERSION_list 1 NEO_OCL_VERSION_MINOR)
-      message(STATUS "Computed OpenCL version minor is: ${NEO_OCL_VERSION_MINOR}")
-    endif()
-  endif()
-else()
-  if(NOT DEFINED NEO_OCL_VERSION_MAJOR)
-    set(NEO_OCL_VERSION_MAJOR 1)
-  endif()
-
-  if(NOT DEFINED NEO_OCL_VERSION_MINOR)
-    set(NEO_OCL_VERSION_MINOR 0)
-  endif()
-endif()
+set(NEO_OCL_VERSION_MAJOR 22)
+set(NEO_OCL_VERSION_MINOR 43)
 
 if(NOT DEFINED NEO_VERSION_BUILD)
-  set(NEO_VERSION_BUILD 0)
-  set(NEO_REVISION 0)
+  set(NEO_VERSION_BUILD 24595)
+  set(NEO_REVISION 24595)
 else()
-  find_program(GIT NAMES git)
-  if(NOT "${GIT}" STREQUAL "GIT-NOTFOUND")
-    if(IS_DIRECTORY ${NEO_SOURCE_DIR}/.git)
-      set(GIT_arg --git-dir=${NEO_SOURCE_DIR}/.git rev-parse HEAD)
-      execute_process(
-                      COMMAND ${GIT} ${GIT_arg}
-                      OUTPUT_VARIABLE NEO_REVISION
-                      OUTPUT_STRIP_TRAILING_WHITESPACE
-      )
-    endif()
-  endif()
-endif()
-if(NOT DEFINED NEO_REVISION)
-  set(NEO_REVISION 0)
+  set(NEO_REVISION NEO_VERSION_BUILD)
 endif()
 
 # OpenCL pacakge version
