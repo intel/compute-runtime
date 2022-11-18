f6eb2e753ab4f2d40adf796554834c2bef9d0541
Fabian Zwolinski <fabian.zwolinski@intel.com>
2022-11-18 11:52:23 +0000

    Error check total SLM size not only inline
    Related-To: NEO-7280
    Signed-off-by: Fabian Zwolinski <fabian.zwolinski@intel.com>

--- opencl/source/api/api.cpp
+++ opencl/source/api/api.cpp
@@ -3510,10 +3510,10 @@ cl_int CL_API_CALL clEnqueueNDRangeKernel(cl_command_queue commandQueue,
     Kernel *pKernel = pMultiDeviceKernel->getKernel(pCommandQueue->getDevice().getRootDeviceIndex());
 
     auto localMemSize = static_cast<uint32_t>(pCommandQueue->getDevice().getDeviceInfo().localMemSize);
-    auto slmInlineSize = pKernel->getDescriptor().kernelAttributes.slmInlineSize;
+    auto slmTotalSize = pKernel->getSlmTotalSize();
 
-    if (slmInlineSize > 0 && localMemSize < slmInlineSize) {
-        PRINT_DEBUG_STRING(NEO::DebugManager.flags.PrintDebugMessages.get(), stderr, "Size of SLM (%u) larger than available (%u)\n", slmInlineSize, localMemSize);
+    if (slmTotalSize > 0 && localMemSize < slmTotalSize) {
+        PRINT_DEBUG_STRING(NEO::DebugManager.flags.PrintDebugMessages.get(), stderr, "Size of SLM (%u) larger than available (%u)\n", slmTotalSize, localMemSize);
         retVal = CL_OUT_OF_RESOURCES;
         TRACING_EXIT(ClEnqueueNdRangeKernel, &retVal);
         return retVal;
