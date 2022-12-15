1292ea26f598074f224567c03a19ec9f40592c3a
Spruit, Neil R <neil.r.spruit@intel.com>
2022-12-15 08:15:18 -0800

    Fix Device Allocated IPC Events for L0
    - Given Device Allocated IPC Event Pool, the open IPC event pool process
      must allocate the event pool as a device buffer or usage of the memory
    will fail.
    - Pass deviceAlloc flag thru the IPC Event Pool Handle to enable the
      child processes to allocate the correct memory type.
    - Pass isHostVisible flag thru IPCEvent Pool Handle to indicate if the
      event pool must be allocated as host visible regardless of the
    allocation type.
    
    Related-To: LOCI-3764
    
    Signed-off-by: Spruit, Neil R <neil.r.spruit@intel.com>

--- level_zero/core/source/event/event.h
+++ level_zero/core/source/event/event.h
@@ -312,6 +312,8 @@ struct EventPool : _ze_event_pool_handle_t {
 
     std::unique_ptr<NEO::MultiGraphicsAllocation> eventPoolAllocations;
     ze_event_pool_flags_t eventPoolFlags;
+    bool isDeviceEventPoolAllocation = false;
+    bool isHostVisibleEventPoolAllocation = false;
 
   protected:
     EventPool() = default;
