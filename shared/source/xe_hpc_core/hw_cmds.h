a572f6ce956055fbadc55b58d94864580333a1fa
Warchulski, Jaroslaw <jaroslaw.warchulski@intel.com>
2023-01-03 11:01:07 +0000

    Add missing include
    Related-To: NEO-5548
    Signed-off-by: Warchulski, Jaroslaw <jaroslaw.warchulski@intel.com>

--- shared/source/xe_hpc_core/hw_cmds.h
+++ shared/source/xe_hpc_core/hw_cmds.h
@@ -1,11 +1,13 @@
 /*
- * Copyright (C) 2021-2022 Intel Corporation
+ * Copyright (C) 2021-2023 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
  */
 
 #pragma once
+
+#include "shared/source/xe_hpc_core/hw_cmds_xe_hpc_core_base.h"
 
 #ifdef SUPPORT_PVC
 #include "hw_cmds_pvc.h"
