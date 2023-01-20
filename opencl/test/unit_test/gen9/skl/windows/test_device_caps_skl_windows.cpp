71cf32b1e6989f4f329b44330b09eb1d3c05a6fb
Zbigniew Zdanowicz <zbigniew.zdanowicz@intel.com>
2023-01-20 05:21:04 +0000

    add missing includes
    Signed-off-by: Zbigniew Zdanowicz <zbigniew.zdanowicz@intel.com>

--- opencl/test/unit_test/gen9/skl/windows/test_device_caps_skl_windows.cpp
+++ opencl/test/unit_test/gen9/skl/windows/test_device_caps_skl_windows.cpp
@@ -1,11 +1,12 @@
 /*
- * Copyright (C) 2018-2022 Intel Corporation
+ * Copyright (C) 2018-2023 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
  */
 
 #include "shared/source/gen9/hw_cmds_skl.h"
+#include "shared/test/common/mocks/mock_device.h"
 #include "shared/test/common/test_macros/header/per_product_test_definitions.h"
 #include "shared/test/common/test_macros/test.h"
 
