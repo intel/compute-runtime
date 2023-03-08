49b01a8ea4d072b8aaf120b0af558cd26d32a802
Kamil Kopryk <kamil.kopryk@intel.com>
2023-03-08 16:37:44 +0000

    refactor: rename product helper uuid files
    Related-To: NEO-7703
    Signed-off-by: Kamil Kopryk <kamil.kopryk@intel.com>

--- shared/source/xe_hp_core/linux/hw_info_config_xe_hp_sdv.cpp
+++ shared/source/xe_hp_core/linux/hw_info_config_xe_hp_sdv.cpp
@@ -28,7 +28,7 @@ const std::map<std::string, std::pair<uint32_t, uint32_t>> guidUuidOffsetMap = {
     // add new values for guid in the form of {"guid", {offset, size}} for each platform
     {"0xfdc76195", {64u, 8u}}};
 
-#include "shared/source/os_interface/linux/hw_info_config_uuid_xehp_and_later.inl"
+#include "shared/source/os_interface/linux/product_helper_uuid_xehp_and_later.inl"
 } // namespace NEO
 
 #include "shared/source/xe_hp_core/os_agnostic_product_helper_xe_hp_core.inl"
--- shared/source/xe_hpc_core/linux/hw_info_config_pvc.cpp
+++ shared/source/xe_hpc_core/linux/hw_info_config_pvc.cpp
@@ -29,7 +29,7 @@ namespace NEO {
 const std::map<std::string, std::pair<uint32_t, uint32_t>> guidUuidOffsetMap = {
     // add new values for guid in the form of {"guid", {offset, size}} for each platform
     {"0x41fe79a5", {64u, 8u}}};
-#include "shared/source/os_interface/linux/hw_info_config_uuid_xehp_and_later.inl"
+#include "shared/source/os_interface/linux/product_helper_uuid_xehp_and_later.inl"
 #include "shared/source/os_interface/linux/product_helper_xe_hpc_and_later.inl"
 } // namespace NEO
 #include "shared/source/xe_hpc_core/os_agnostic_product_helper_xe_hpc_core.inl"
