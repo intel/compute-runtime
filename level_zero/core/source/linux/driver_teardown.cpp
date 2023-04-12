41ad05eb527ef0239560965ad25268a689671902
Compute-Runtime-Validation <compute-runtime-validation@intel.com>
2023-04-12 03:53:42 +0200

    Revert "l0_feature: Use L0 Loader teardown callback"
    This reverts commit d31b950b9a76f8b8b19d89d434cffb58a9ca2396.
    
    Signed-off-by: Compute-Runtime-Validation <compute-runtime-validation@intel.com>

--- level_zero/core/source/linux/driver_teardown.cpp
+++ level_zero/core/source/linux/driver_teardown.cpp
@@ -5,12 +5,18 @@
  *
  */
 
-#include "level_zero/core/source/global_teardown.h"
+#include "level_zero/core/source/driver/driver_handle_imp.h"
+#include "level_zero/sysman/source/sysman_driver_handle_imp.h"
 
 using namespace L0;
 
 void __attribute__((destructor)) driverHandleDestructor() {
-    std::string loaderLibraryName = "lib" + L0::loaderLibraryFilename + ".so.1";
-    loaderDriverTeardown(loaderLibraryName);
-    globalDriverTeardown();
+    if (GlobalDriver != nullptr) {
+        delete GlobalDriver;
+        GlobalDriver = nullptr;
+    }
+    if (Sysman::GlobalSysmanDriver != nullptr) {
+        delete Sysman::GlobalSysmanDriver;
+        Sysman::GlobalSysmanDriver = nullptr;
+    }
 }
--- level_zero/core/source/windows/driver_teardown.cpp
+++ level_zero/core/source/windows/driver_teardown.cpp
@@ -5,7 +5,8 @@
  *
  */
 
-#include "level_zero/core/source/global_teardown.h"
+#include "level_zero/core/source/driver/driver_handle_imp.h"
+#include "level_zero/sysman/source/sysman_driver_handle_imp.h"
 #include "level_zero/tools/source/sysman/os_sysman_driver.h"
 #include "level_zero/tools/source/sysman/sysman.h"
 
@@ -15,9 +16,14 @@ using namespace L0;
 
 BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
     if (fdwReason == DLL_PROCESS_DETACH) {
-        std::string loaderLibraryName = L0::loaderLibraryFilename + ".dll";
-        loaderDriverTeardown(loaderLibraryName);
-        globalDriverTeardown();
+        if (GlobalDriver != nullptr) {
+            delete GlobalDriver;
+            GlobalDriver = nullptr;
+        }
+        if (Sysman::GlobalSysmanDriver != nullptr) {
+            delete Sysman::GlobalSysmanDriver;
+            Sysman::GlobalSysmanDriver = nullptr;
+        }
         if (GlobalOsSysmanDriver != nullptr) {
             delete GlobalOsSysmanDriver;
             GlobalOsSysmanDriver = nullptr;
