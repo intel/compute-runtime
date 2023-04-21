intel/compute-runtime

commit 77b88f19a136a0b8817b04001f322bd4a03cee6c

Cleanup includes 23
Cleaned up files:
opencl/source/execution_environment/cl_execution_environment.h
opencl/source/helpers/cl_validators.h
opencl/test/unit_test/mocks/mock_cl_device.h
opencl/test/unit_test/mocks/mock_context.h
shared/source/helpers/cache_policy.h
shared/source/image/image_surface_state.h

Related-To: NEO-5548
Signed-off-by: Warchulski, Jaroslaw <jaroslaw.warchulski@intel.com>

opencl/source/execution_environment/cl_execution_environment.h
@@ -1,17 +1,19 @@
 /*
- * Copyright (C) 2020-2022 Intel Corporation
+ * Copyright (C) 2020-2023 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
  */
 
 #pragma once
-#include "shared/source/built_ins/builtinops/built_in_ops.h"
 #include "shared/source/execution_environment/execution_environment.h"
 
-#include <mutex>
 #include <utility>
 #include <vector>
+
+namespace std {
+struct once_flag;
+}
 
 namespace NEO {
 

opencl/source/built_ins/vme_dispatch_builder.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include "shared/source/built_ins/built_ins.h"
+#include "shared/source/helpers/basic_math.h"
 
 #include "opencl/source/accelerators/intel_accelerator.h"
 #include "opencl/source/accelerators/intel_motion_estimation.h"

opencl/source/command_queue/command_queue.cpp
@@ -11,6 +11,8 @@
 #include "shared/source/command_stream/command_stream_receiver.h"
 #include "shared/source/debugger/debugger_l0.h"
 #include "shared/source/execution_environment/root_device_environment.h"
+#include "shared/source/gmm_helper/gmm.h"
+#include "shared/source/gmm_helper/resource_info.h"
 #include "shared/source/helpers/aligned_memory.h"
 #include "shared/source/helpers/array_count.h"
 #include "shared/source/helpers/bit_helpers.h"

opencl/source/context/context.cpp
@@ -23,6 +23,7 @@
 #include "opencl/source/command_queue/command_queue.h"
 #include "opencl/source/execution_environment/cl_execution_environment.h"
 #include "opencl/source/gtpin/gtpin_notify.h"
+#include "opencl/source/helpers/cl_validators.h"
 #include "opencl/source/helpers/get_info_status_mapper.h"
 #include "opencl/source/helpers/surface_formats.h"
 #include "opencl/source/mem_obj/image.h"

opencl/source/context/context.h
@@ -15,6 +15,7 @@
 #include "opencl/source/cl_device/cl_device_vector.h"
 #include "opencl/source/context/context_type.h"
 #include "opencl/source/context/driver_diagnostics.h"
+#include "opencl/source/gtpin/gtpin_notify.h"
 #include "opencl/source/helpers/base_object.h"
 #include "opencl/source/helpers/destructor_callbacks.h"
 #include "opencl/source/mem_obj/map_operations_handler.h"

opencl/source/helpers/cl_validators.h
@@ -1,12 +1,11 @@
 /*
- * Copyright (C) 2018-2022 Intel Corporation
+ * Copyright (C) 2018-2023 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
  */
 
 #pragma once
-#include "opencl/source/api/cl_types.h"
 #include "opencl/source/helpers/base_object.h"
 #include "opencl/source/helpers/error_mappers.h"
 

opencl/source/kernel/kernel.cpp
@@ -12,7 +12,9 @@
 #include "shared/source/debug_settings/debug_settings_manager.h"
 #include "shared/source/execution_environment/execution_environment.h"
 #include "shared/source/execution_environment/root_device_environment.h"
+#include "shared/source/gmm_helper/gmm.h"
 #include "shared/source/gmm_helper/gmm_helper.h"
+#include "shared/source/gmm_helper/resource_info.h"
 #include "shared/source/helpers/aligned_memory.h"
 #include "shared/source/helpers/basic_math.h"
 #include "shared/source/helpers/bindless_heaps_helper.h"
@@ -42,6 +44,7 @@
 #include "opencl/source/context/context.h"
 #include "opencl/source/gtpin/gtpin_notify.h"
 #include "opencl/source/helpers/cl_hw_helper.h"
+#include "opencl/source/helpers/cl_validators.h"
 #include "opencl/source/helpers/dispatch_info.h"
 #include "opencl/source/helpers/get_info_status_mapper.h"
 #include "opencl/source/helpers/sampler_helpers.h"

opencl/source/mem_obj/buffer.cpp
@@ -8,6 +8,7 @@
 #include "shared/source/command_container/implicit_scaling.h"
 #include "shared/source/execution_environment/execution_environment.h"
 #include "shared/source/execution_environment/root_device_environment.h"
+#include "shared/source/gmm_helper/gmm_helper.h"
 #include "shared/source/helpers/local_memory_access_modes.h"
 #include "shared/source/memory_manager/host_ptr_manager.h"
 #include "shared/source/memory_manager/memory_operations_handler.h"
@@ -16,6 +17,7 @@
 #include "opencl/source/command_queue/command_queue.h"
 #include "opencl/source/context/context.h"
 #include "opencl/source/helpers/cl_memory_properties_helpers.h"
+#include "opencl/source/helpers/cl_validators.h"
 #include "opencl/source/mem_obj/mem_obj_helper.h"
 #include "opencl/source/sharings/unified/unified_buffer.h"
 

commit f9fc3e2f93a1aaadd8b32b2ec2afe1edb62b7398

cl_execution_environment: don't redeclare an STL type
Depending on the compiler and STL used, `std::once_flag` may actually be aliased into an internal namespace (e.g. `std::__1::once_flag`). Declaring it directly within `std` here may conflict with that if the actual `<mutex>` header is included in the same translation unit.

This regressed in 77b88f19a136a0b8817b04001f322bd4a03cee6c.

Signed-off-by: rcombs <rcombs@rcombs.me>

opencl/source/execution_environment/cl_execution_environment.h
@@ -8,12 +8,9 @@
 #pragma once
 #include "shared/source/execution_environment/execution_environment.h"
 
+#include <mutex>
 #include <utility>
 #include <vector>
-
-namespace std {
-struct once_flag;
-}
 
 namespace NEO {
 