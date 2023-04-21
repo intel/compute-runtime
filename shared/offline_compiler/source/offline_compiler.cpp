d8e4b324ad70f631d610cab7377a423bbfc93dfc
Compute-Runtime-Validation <compute-runtime-validation@intel.com>
2023-04-21 05:46:45 +0200

    Revert "refactor: Remove not needed ocloc message"
    This reverts commit 86337dc2eeb5deb04a03e7c369723635608024ac.
    
    Signed-off-by: Compute-Runtime-Validation <compute-runtime-validation@intel.com>

--- shared/offline_compiler/source/offline_compiler.cpp
+++ shared/offline_compiler/source/offline_compiler.cpp
@@ -640,6 +640,9 @@ int OfflineCompiler::initialize(size_t numArgs, const std::vector<std::string> &
 
         preferredIntermediateRepresentation = fclFacade->getPreferredIntermediateRepresentation();
     } else {
+        if (!isQuiet()) {
+            argHelper->printf("Compilation from IR - skipping loading of FCL\n");
+        }
         preferredIntermediateRepresentation = IGC::CodeType::spirV;
     }
 
