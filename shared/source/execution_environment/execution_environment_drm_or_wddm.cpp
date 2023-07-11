4e464e9d6f71c9d4aa1baf342b189b6154ac9785
Mateusz Jablonski <mateusz.jablonski@intel.com>
2023-07-11 10:55:25 +0000

    fix: adjust root device indices when filtering root device environments
    Related-To: NEO-8166
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

--- shared/source/execution_environment/execution_environment_drm_or_wddm.cpp
+++ shared/source/execution_environment/execution_environment_drm_or_wddm.cpp
@@ -8,16 +8,17 @@
 #include "shared/source/execution_environment/execution_environment.h"
 #include "shared/source/execution_environment/root_device_environment.h"
 #include "shared/source/helpers/driver_model_type.h"
+#include "shared/source/os_interface/linux/drm_memory_operations_handler.h"
 #include "shared/source/os_interface/os_interface.h"
 
 namespace NEO {
 
-void ExecutionEnvironment::sortNeoDevices() {
-
+void ExecutionEnvironment::adjustRootDeviceEnvironments() {
     if (rootDeviceEnvironments[0]->osInterface->getDriverModel()->getDriverModelType() == DriverModelType::DRM) {
-        return ExecutionEnvironment::sortNeoDevicesDRM();
-    } else {
-        return ExecutionEnvironment::sortNeoDevicesWDDM();
+        for (auto rootDeviceIndex = 0u; rootDeviceIndex < rootDeviceEnvironments.size(); rootDeviceIndex++) {
+            auto drmMemoryOperationsHandler = static_cast<DrmMemoryOperationsHandler *>(rootDeviceEnvironments[rootDeviceIndex]->memoryOperationsInterface.get());
+            drmMemoryOperationsHandler->setRootDeviceIndex(rootDeviceIndex);
+        }
     }
 }
 
