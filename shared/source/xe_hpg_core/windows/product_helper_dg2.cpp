cb2b4214b4ac9cbd788b92ac5e1af68108142d14
Kulkarni, Ashwin Kumar <ashwin.kumar.kulkarni@intel.com>
2023-07-19 17:00:21 +0000

    feature: support UUID feature for windows
    Related-To: LOCI-3523
    
    Signed-off-by: Kulkarni, Ashwin Kumar <ashwin.kumar.kulkarni@intel.com>

--- shared/source/xe_hpg_core/windows/product_helper_dg2.cpp
+++ shared/source/xe_hpg_core/windows/product_helper_dg2.cpp
@@ -13,6 +13,7 @@
 #include "shared/source/os_interface/product_helper.inl"
 #include "shared/source/os_interface/product_helper_dg2_and_later.inl"
 #include "shared/source/os_interface/product_helper_xehp_and_later.inl"
+#include "shared/source/os_interface/windows/wddm/wddm.h"
 #include "shared/source/xe_hpg_core/hw_cmds_dg2.h"
 
 constexpr static auto gfxProduct = IGFX_DG2;
@@ -22,6 +23,28 @@ constexpr static auto gfxProduct = IGFX_DG2;
 #include "windows_product_helper_dg2_extra.inl"
 
 namespace NEO {
+
+template <>
+bool ProductHelperHw<gfxProduct>::getUuid(NEO::DriverModel *driverModel, uint32_t subDeviceCount, uint32_t deviceIndex, std::array<uint8_t, ProductHelper::uuidSize> &uuid) const {
+
+    if (driverModel->getDriverModelType() != DriverModelType::WDDM) {
+        return false;
+    }
+
+    NEO::Wddm *pWddm = driverModel->as<NEO::Wddm>();
+    GT_SYSTEM_INFO *gtInfo = pWddm->getGtSysInfo();
+
+    uint64_t uuidValue = gtInfo->DeviceUUID;
+    if (uuidValue == 0) {
+        return false;
+    }
+
+    uuid.fill(0);
+    memcpy_s(uuid.data(), uuid.size(), &uuidValue, sizeof(uuidValue));
+
+    return true;
+}
+
 template <>
 int ProductHelperHw<gfxProduct>::configureHardwareCustom(HardwareInfo *hwInfo, OSInterface *osIface) const {
     if (allowCompression(*hwInfo)) {
