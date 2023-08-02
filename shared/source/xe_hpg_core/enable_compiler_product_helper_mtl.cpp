fbac74e3c4b04d7468a2b237fb2510a250fb44c0
Mateusz Jablonski <mateusz.jablonski@intel.com>
2023-08-02 14:59:11 +0000

    fix: add fallback in setting hw ip version for MTL
    setup hw ip version based on device id and revision id
    
    Related-To: NEO-8231
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

--- shared/source/xe_hpg_core/enable_compiler_product_helper_mtl.cpp
+++ shared/source/xe_hpg_core/enable_compiler_product_helper_mtl.cpp
@@ -26,6 +26,49 @@ uint32_t CompilerProductHelperHw<gfxProduct>::getDefaultHwIpVersion() const {
     return AOT::MTL_M_A0;
 }
 
+template <>
+uint32_t CompilerProductHelperHw<gfxProduct>::getProductConfigFromHwInfo(const HardwareInfo &hwInfo) const {
+    if (hwInfo.ipVersion.value) {
+        return hwInfo.ipVersion.value;
+    }
+    switch (hwInfo.platform.usDeviceID) {
+    case 0x7D40:
+    case 0x7D45: {
+        switch (hwInfo.platform.usRevId) {
+        case 0x0:
+        case 0x2:
+            return AOT::MTL_M_A0;
+        case 0x3:
+        case 0x8:
+            return AOT::MTL_M_B0;
+        }
+        break;
+    }
+    case 0x7D55:
+    case 0X7DD5: {
+        switch (hwInfo.platform.usRevId) {
+        case 0x0:
+        case 0x2:
+            return AOT::MTL_P_A0;
+        case 0x3:
+        case 0x8:
+            return AOT::MTL_P_B0;
+        }
+        break;
+    }
+    case 0x7D60: {
+        switch (hwInfo.platform.usRevId) {
+        case 0x0:
+            return AOT::MTL_M_A0;
+        case 0x2:
+            return AOT::MTL_M_B0;
+        }
+        break;
+    }
+    }
+    return getDefaultHwIpVersion();
+}
+
 static EnableCompilerProductHelper<gfxProduct> enableCompilerProductHelperMTL;
 
 } // namespace NEO
