1d9c29f9a9e622d8175847b9f0a15f2a8c08f4c6
Mateusz Jablonski <mateusz.jablonski@intel.com>
2023-08-02 14:59:11 +0000

    fix: add fallback in setting hw ip version for MTL
    setup hw ip version based on device id and revision id
    
    Related-To: NEO-8231
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

--- shared/source/xe_hpg_core/enable_compiler_product_helper_mtl.cpp
+++ shared/source/xe_hpg_core/enable_compiler_product_helper_mtl.cpp
@@ -11,4 +11,49 @@ constexpr auto gfxProduct = IGFX_METEORLAKE;
 
 #include "shared/source/xe_hpg_core/xe_lpg/compiler_product_helper_xe_lpg.inl"
 
+namespace NEO {
+template <>
+uint32_t CompilerProductHelperHw<gfxProduct>::getProductConfigFromHwInfo(const HardwareInfo &hwInfo) const {
+    if (hwInfo.ipVersion.value) {
+        return hwInfo.ipVersion.value;
+    }
+    switch (hwInfo.platform.usDeviceID) {
+    case 0x7D40:
+    case 0x7D45: {
+        switch (hwInfo.platform.usRevId) {
+        case 0x0:
+        case 0x2:
+            return AOT::MTL_M_A0;
+        case 0x3:
+        case 0x8:
+            return AOT::MTL_M_B0;
+        }
+        break;
+    }
+    case 0x7D55:
+    case 0X7DD5: {
+        switch (hwInfo.platform.usRevId) {
+        case 0x0:
+        case 0x2:
+            return AOT::MTL_P_A0;
+        case 0x3:
+        case 0x8:
+            return AOT::MTL_P_B0;
+        }
+        break;
+    }
+    case 0x7D60: {
+        switch (hwInfo.platform.usRevId) {
+        case 0x0:
+            return AOT::MTL_M_A0;
+        case 0x2:
+            return AOT::MTL_M_B0;
+        }
+        break;
+    }
+    }
+    return getDefaultHwIpVersion();
+}
+} // namespace NEO
+
 static NEO::EnableCompilerProductHelper<gfxProduct> enableCompilerProductHelperMTL;
--- shared/test/unit_test/xe_hpg_core/mtl/product_helper_tests_mtl.cpp
+++ shared/test/unit_test/xe_hpg_core/mtl/product_helper_tests_mtl.cpp
@@ -5,13 +5,17 @@
  *
  */
 
+#include "shared/source/helpers/compiler_product_helper.h"
 #include "shared/source/memory_manager/allocation_properties.h"
 #include "shared/source/memory_manager/allocation_type.h"
 #include "shared/source/os_interface/product_helper.h"
 #include "shared/source/xe_hpg_core/hw_cmds_mtl.h"
+#include "shared/test/common/helpers/default_hw_info.h"
 #include "shared/test/common/test_macros/header/per_product_test_definitions.h"
 #include "shared/test/common/test_macros/test.h"
 #include "shared/test/unit_test/os_interface/product_helper_tests.h"
+
+#include "platforms.h"
 
 using namespace NEO;
 
@@ -19,6 +23,66 @@ using MtlProductHelper = ProductHelperTest;
 
 MTLTEST_F(MtlProductHelper, givenProductHelperWhenCheckDirectSubmissionSupportedThenTrueIsReturned) {
     EXPECT_TRUE(productHelper->isDirectSubmissionSupported(releaseHelper));
+}
+
+MTLTEST_F(MtlProductHelper, givenMtlWithoutHwIpVersionInHwInfoWhenGettingIpVersionThenCorrectValueIsReturnedBasedOnDeviceIdAndRevId) {
+    auto hwInfo = *defaultHwInfo;
+    hwInfo.ipVersion = {};
+
+    auto mtlMDeviceIds = {0x7D40, 0x7D45};
+    auto mtlPDeviceIds = {0x7D55, 0X7DD5};
+
+    hwInfo.platform.usDeviceID = 0x7D60;
+    hwInfo.platform.usRevId = 0;
+
+    EXPECT_EQ(AOT::MTL_M_A0, compilerProductHelper->getHwIpVersion(hwInfo));
+
+    hwInfo.platform.usRevId = 2;
+
+    EXPECT_EQ(AOT::MTL_M_B0, compilerProductHelper->getHwIpVersion(hwInfo));
+
+    hwInfo.platform.usRevId = 0xdead;
+
+    EXPECT_EQ(compilerProductHelper->getDefaultHwIpVersion(), compilerProductHelper->getHwIpVersion(hwInfo));
+
+    for (auto &deviceId : mtlMDeviceIds) {
+        hwInfo.platform.usDeviceID = deviceId;
+        for (auto &revision : {0, 2}) {
+            hwInfo.platform.usRevId = revision;
+
+            EXPECT_EQ(AOT::MTL_M_A0, compilerProductHelper->getHwIpVersion(hwInfo));
+        }
+        for (auto &revision : {3, 8}) {
+            hwInfo.platform.usRevId = revision;
+
+            EXPECT_EQ(AOT::MTL_M_B0, compilerProductHelper->getHwIpVersion(hwInfo));
+        }
+        hwInfo.platform.usRevId = 0xdead;
+
+        EXPECT_EQ(compilerProductHelper->getDefaultHwIpVersion(), compilerProductHelper->getHwIpVersion(hwInfo));
+    }
+
+    for (auto &deviceId : mtlPDeviceIds) {
+        hwInfo.platform.usDeviceID = deviceId;
+        for (auto &revision : {0, 2}) {
+            hwInfo.platform.usRevId = revision;
+
+            EXPECT_EQ(AOT::MTL_P_A0, compilerProductHelper->getHwIpVersion(hwInfo));
+        }
+        for (auto &revision : {3, 8}) {
+            hwInfo.platform.usRevId = revision;
+
+            EXPECT_EQ(AOT::MTL_P_B0, compilerProductHelper->getHwIpVersion(hwInfo));
+        }
+        hwInfo.platform.usRevId = 0xdead;
+
+        EXPECT_EQ(compilerProductHelper->getDefaultHwIpVersion(), compilerProductHelper->getHwIpVersion(hwInfo));
+    }
+
+    hwInfo.platform.usDeviceID = 0;
+    hwInfo.platform.usRevId = 0xdead;
+
+    EXPECT_EQ(compilerProductHelper->getDefaultHwIpVersion(), compilerProductHelper->getHwIpVersion(hwInfo));
 }
 
 MTLTEST_F(MtlProductHelper, givenProductHelperWhenCheckOverrideAllocationCacheableThenTrueIsReturnedForCommandBuffer) {
