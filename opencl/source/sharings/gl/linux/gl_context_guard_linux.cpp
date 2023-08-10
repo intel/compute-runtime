0cd8d3b509be31cdf346283d411cd0d779230bd3
Sylvain Munaut <tnt@246tNt.com>
2023-08-10 09:15:28 +0200

    linux/clgl: Disable code in gl_context_guard_linux.cpp
    This doesn't work and AFAICT it's not needed for the MESA GL interop
    anyway, so use a dummy implementation
    
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- opencl/source/sharings/gl/linux/gl_context_guard_linux.cpp
+++ opencl/source/sharings/gl/linux/gl_context_guard_linux.cpp
@@ -10,29 +10,8 @@
 
 namespace NEO {
 GLContextGuard::GLContextGuard(GLSharingFunctions &sharingFcns) : sharingFunctions(&sharingFcns) {
-    auto &sharing = *static_cast<GLSharingFunctionsLinux *>(sharingFunctions);
-
-    currentContextHandle = sharing.getCurrentContext();
-    currentDisplayHandle = sharing.getCurrentDisplay();
-    auto ctxToMakeCurrent = sharing.getContextHandle();
-
-    if (currentContextHandle == 0) {
-        ctxToMakeCurrent = sharing.getBackupContextHandle();
-    }
-
-    if (currentContextHandle != sharing.getContextHandle() && currentContextHandle != sharing.getBackupContextHandle()) {
-        if (sharing.makeCurrent(ctxToMakeCurrent) == GL_FALSE) {
-            while (sharing.makeCurrent(sharing.getBackupContextHandle()) == GL_FALSE) {
-                ;
-            }
-        }
-    }
 }
 
 GLContextGuard::~GLContextGuard() {
-    auto &sharing = *static_cast<GLSharingFunctionsLinux *>(sharingFunctions);
-    if (currentContextHandle != sharing.getContextHandle()) {
-        sharing.makeCurrent(currentContextHandle, currentDisplayHandle);
-    }
 }
 } // namespace NEO
