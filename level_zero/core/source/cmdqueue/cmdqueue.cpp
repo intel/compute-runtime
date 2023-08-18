02994e13101c05a1f2cb8c3817abd071f86d4409
Dunajski, Bartosz <bartosz.dunajski@intel.com>
2023-08-18 13:15:45 +0000

    fix: stall RelaxedOrdering when submitting regular cmd list
    Source: 9d943e484e03b39fdb53b311109a5f2f5b7fabc8
    Signed-off-by: Dunajski, Bartosz <bartosz.dunajski@intel.com>

--- level_zero/core/source/cmdqueue/cmdqueue.cpp
+++ level_zero/core/source/cmdqueue/cmdqueue.cpp
@@ -123,7 +123,7 @@ NEO::SubmissionStatus CommandQueueImp::submitBatchBuffer(size_t offset, NEO::Res
 
     NEO::BatchBuffer batchBuffer(this->startingCmdBuffer->getGraphicsAllocation(), offset, 0, 0, nullptr, false, false,
                                  NEO::QueueThrottle::HIGH, NEO::QueueSliceCount::defaultSliceCount,
-                                 this->startingCmdBuffer->getUsed(), this->startingCmdBuffer, endingCmdPtr, csr->getNumClients(), false, false);
+                                 this->startingCmdBuffer->getUsed(), this->startingCmdBuffer, endingCmdPtr, csr->getNumClients(), true, false);
 
     if (this->startingCmdBuffer != &this->commandStream) {
         this->csr->makeResident(*this->commandStream.getGraphicsAllocation());
