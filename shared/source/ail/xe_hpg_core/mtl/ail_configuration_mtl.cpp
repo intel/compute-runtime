bef6b641489280b9991c42ca7e6eaba6279f3439
Compute-Runtime-Validation <compute-runtime-validation@intel.com>
2023-08-21 12:47:11 +0200

    Revert "feature: add AIL for selected apps on MTL"
    This reverts commit 68909a895af954ecad2a1627ee5df4fd84d9d016.
    
    Signed-off-by: Compute-Runtime-Validation <compute-runtime-validation@intel.com>

--- shared/source/ail/xe_hpg_core/mtl/ail_configuration_mtl.cpp
+++ shared/source/ail/xe_hpg_core/mtl/ail_configuration_mtl.cpp
@@ -6,8 +6,6 @@
  */
 
 #include "shared/source/ail/ail_configuration_base.inl"
-#include "shared/source/helpers/debug_helpers.h"
-#include "shared/source/helpers/hw_info.h"
 
 #include <map>
 #include <vector>
@@ -15,29 +13,7 @@
 namespace NEO {
 static EnableAIL<IGFX_METEORLAKE> enableAILMTL;
 
-std::map<std::string_view, std::vector<AILEnumeration>> applicationMapMTL = {
-    {"Photoshop", {AILEnumeration::DISABLE_DEFAULT_CCS}},
-    {"AfterFX (Beta)", {AILEnumeration::DISABLE_DEFAULT_CCS}},
-    {"AfterFX", {AILEnumeration::DISABLE_DEFAULT_CCS}},
-    {"Adobe Premiere Pro", {AILEnumeration::DISABLE_DEFAULT_CCS}},
-};
-
-template <>
-inline void AILConfigurationHw<IGFX_METEORLAKE>::applyExt(RuntimeCapabilityTable &runtimeCapabilityTable) {
-
-    auto search = applicationMapMTL.find(processName);
-
-    if (search != applicationMapMTL.end()) {
-        for (size_t i = 0; i < search->second.size(); ++i) {
-            switch (search->second[i]) {
-            case AILEnumeration::DISABLE_DEFAULT_CCS:
-                runtimeCapabilityTable.defaultEngineType = aub_stream::ENGINE_RCS;
-            default:
-                break;
-            }
-        }
-    }
-}
+std::map<std::string_view, std::vector<AILEnumeration>> applicationMapMTL = {};
 
 template class AILConfigurationHw<IGFX_METEORLAKE>;
 
