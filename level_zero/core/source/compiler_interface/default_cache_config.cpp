f19898d1783c01767ac9a044316852aa9db7b707
Compute-Runtime-Validation <compute-runtime-validation@intel.com>
2023-09-22 12:25:38 +0200

    Revert "fix: enable cache env variables for level-zero"
    This reverts commit 74689fa58aa95dd94edb5193e73155f83f781f68.
    
    Signed-off-by: Compute-Runtime-Validation <compute-runtime-validation@intel.com>

--- level_zero/core/source/compiler_interface/default_cache_config.cpp
+++ level_zero/core/source/compiler_interface/default_cache_config.cpp
@@ -9,9 +9,7 @@
 
 #include "shared/source/compiler_interface/default_cache_config.h"
 
-#include "shared/source/compiler_interface/os_compiler_cache_helper.h"
 #include "shared/source/helpers/constants.h"
-#include "shared/source/os_interface/debug_env_reader.h"
 #include "shared/source/os_interface/sys_calls_common.h"
 #include "shared/source/utilities/debug_settings_reader.h"
 
@@ -20,44 +18,13 @@
 #include <string>
 
 namespace NEO {
-std::string NeoCachePersistent = "NEO_CACHE_PERSISTENT";
-std::string NeoCacheMaxSize = "NEO_CACHE_MAX_SIZE";
-std::string NeoCacheDir = "NEO_CACHE_DIR";
-std::string L0CacheDir = "l0_cache_dir";
-
 CompilerCacheConfig getDefaultCompilerCacheConfig() {
-    CompilerCacheConfig ret;
-    NEO::EnvironmentVariableReader envReader;
+    NEO::CompilerCacheConfig ret;
 
-    if (envReader.getSetting(NeoCachePersistent.c_str(), defaultCacheEnabled()) != 0) {
-        ret.enabled = true;
-        std::string emptyString = "";
-        ret.cacheDir = envReader.getSetting(NeoCacheDir.c_str(), emptyString);
-
-        if (ret.cacheDir.empty()) {
-            if (!checkDefaultCacheDirSettings(ret.cacheDir, envReader)) {
-                ret.enabled = false;
-                return ret;
-            }
-        } else {
-            if (!NEO::SysCalls::pathExists(ret.cacheDir)) {
-                ret.cacheDir = "";
-                ret.enabled = false;
-                return ret;
-            }
-        }
-
-        ret.cacheFileExtension = ".l0_cache";
-        ret.cacheSize = static_cast<size_t>(envReader.getSetting(NeoCacheMaxSize.c_str(), static_cast<int64_t>(MemoryConstants::gigaByte)));
-
-        if (ret.cacheSize == 0u) {
-            ret.cacheSize = std::numeric_limits<size_t>::max();
-        }
-
-        return ret;
-    }
-
-    ret.cacheDir = envReader.getSetting(L0CacheDir.c_str(), static_cast<std::string>(L0_CACHE_LOCATION));
+    std::string keyName = L0::registryPath;
+    keyName += "l0_cache_dir";
+    std::unique_ptr<NEO::SettingsReader> settingsReader(NEO::SettingsReader::createOsReader(false, keyName));
+    ret.cacheDir = settingsReader->getSetting(settingsReader->appSpecificLocation(keyName), static_cast<std::string>(L0_CACHE_LOCATION));
 
     if (NEO::SysCalls::pathExists(ret.cacheDir)) {
         ret.enabled = true;
