8a280972d3165161f723ad01d4df9736ab117adf
Katarzyna Cencelewska <katarzyna.cencelewska@intel.com>
2023-09-25 17:06:15 +0000

    fix: add check to copy/fill buffer builtins that pointers aligned to 4 bytes
    it is a compiler hint to provide information about alignment
    
    Source: ebc024f0f11c21b796219c4153c1a06d8b88a514
    Resolves: NEO-7973
    Signed-off-by: Katarzyna Cencelewska <katarzyna.cencelewska@intel.com>

--- shared/test/common/helpers/kernel_binary_helper_hash_value.cpp
+++ shared/test/common/helpers/kernel_binary_helper_hash_value.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2022 Intel Corporation
+ * Copyright (C) 2020-2023 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
@@ -7,5 +7,5 @@
 
 #include "shared/test/common/helpers/kernel_binary_helper.h"
 
-const std::string KernelBinaryHelper::BUILT_INS("11060315779005710265");
-const std::string KernelBinaryHelper::BUILT_INS_WITH_IMAGES("7912693229131574095_images");
+const std::string KernelBinaryHelper::BUILT_INS("1535368482363896758");
+const std::string KernelBinaryHelper::BUILT_INS_WITH_IMAGES("9730917519387864121_images");
