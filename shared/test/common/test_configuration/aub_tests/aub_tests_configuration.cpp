a94c54e353e10e5c6bd0dc06f74fe3efb8361025
Mateusz Jablonski <mateusz.jablonski@intel.com>
2023-12-18 14:40:42 +0000

    test: increase per aub test case timeout to 32 sec
    Related-To: NEO-9730
    
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

--- shared/test/common/test_configuration/aub_tests/aub_tests_configuration.cpp
+++ shared/test/common/test_configuration/aub_tests/aub_tests_configuration.cpp
@@ -14,7 +14,7 @@ unsigned int ultIterationMaxTime = 360;
 #else
 unsigned int ultIterationMaxTime = 180;
 #endif
-unsigned int testCaseMaxTimeInMs = 20000;
+unsigned int testCaseMaxTimeInMs = 32000;
 bool useMockGmm = false;
 const char *executionDirectorySuffix = "_aub";
 const char *executionName = "AUB";
