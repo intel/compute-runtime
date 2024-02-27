3b07779806af346ababd5763329c13175ae558d6
John Falkowski <john.falkowski@intel.com>
2024-02-27 03:39:05 +0000

    feature: get kernel max group size
    Implementing support for ze_kernel_max_group_size_ext_properties_t
    
    Resolves: NEO-9617
    
    Source: cebedb50c75ceffba4d4cf303ae8f1781376c3d8
    
    Signed-off-by: John Falkowski <john.falkowski@intel.com>

--- level_zero/core/source/kernel/kernel_imp.cpp
+++ level_zero/core/source/kernel/kernel_imp.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2023 Intel Corporation
+ * Copyright (C) 2020-2024 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
@@ -881,6 +881,9 @@ ze_result_t KernelImp::getProperties(ze_kernel_properties_t *pKernelProperties) 
             if (gfxCoreHelper.isFusedEuDispatchEnabled(this->module->getDevice()->getHwInfo(), kernelDescriptor.kernelAttributes.flags.requiresDisabledEUFusion)) {
                 preferredGroupSizeProperties->preferredMultiple *= 2;
             }
+        } else if (extendedProperties->stype == ZE_STRUCTURE_TYPE_KERNEL_MAX_GROUP_SIZE_EXT_PROPERTIES) {
+            ze_kernel_max_group_size_properties_ext_t *properties = reinterpret_cast<ze_kernel_max_group_size_properties_ext_t *>(extendedProperties);
+            properties->maxGroupSize = maxKernelWorkGroupSize;
         }
         getExtendedKernelProperties(extendedProperties);
 
