8520181008a95a749b51f1bc43d30c8ef7877fd9
Filip Hazubski <filip.hazubski@intel.com>
2024-03-11 09:55:31 +0000

    fix: Add ATS-M device id
    Add device id 0x56C2
    
    Signed-off-by: Filip Hazubski <filip.hazubski@intel.com>

--- shared/source/xe_hpg_core/dg2/definitions/device_ids_configs_dg2_base.h
+++ shared/source/xe_hpg_core/dg2/definitions/device_ids_configs_dg2_base.h
@@ -22,7 +22,8 @@ const std::vector<unsigned short> dg2G10DeviceIds{
     0x56A0,
     0x56A1,
     0x56A2,
-    0x56C0};
+    0x56C0,
+    0x56C2};
 
 const std::vector<unsigned short> dg2G11DeviceIds{
     0x4F87,
