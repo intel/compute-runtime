33e48c7843462e070a1d824333e3c3c61394d42c
Lukasz Jobczyk <lukasz.jobczyk@intel.com>
2024-05-31 10:05:44 +0000

    fix: Add AIL for svchost on ARL
    Signed-off-by: Lukasz Jobczyk <lukasz.jobczyk@intel.com>

--- shared/source/ail/xe_hpg_core/arl/ail_configuration_arl.cpp
+++ shared/source/ail/xe_hpg_core/arl/ail_configuration_arl.cpp
@@ -1,19 +1,38 @@
 /*
- * Copyright (C) 2023 Intel Corporation
+ * Copyright (C) 2023-2024 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
  */
 
 #include "shared/source/ail/ail_configuration_base.inl"
+#include "shared/source/helpers/hw_info.h"
+
+#include "aubstream/engine_node.h"
 
 #include <map>
 #include <vector>
 
 namespace NEO {
+
+std::map<std::string_view, std::vector<AILEnumeration>> applicationMapARL = {{"svchost", {AILEnumeration::disableDirectSubmission}}};
+
 static EnableAIL<IGFX_ARROWLAKE> enableAILARL;
 
-std::map<std::string_view, std::vector<AILEnumeration>> applicationMapARL = {};
+template <>
+void AILConfigurationHw<IGFX_ARROWLAKE>::applyExt(RuntimeCapabilityTable &runtimeCapabilityTable) {
+    auto search = applicationMapARL.find(processName);
+    if (search != applicationMapARL.end()) {
+        for (size_t i = 0; i < search->second.size(); ++i) {
+            switch (search->second[i]) {
+            case AILEnumeration::disableDirectSubmission:
+                runtimeCapabilityTable.directSubmissionEngines.data[aub_stream::ENGINE_CCS].engineSupported = false;
+            default:
+                break;
+            }
+        }
+    }
+}
 
 template class AILConfigurationHw<IGFX_ARROWLAKE>;
 
---
+++ shared/test/unit_test/xe_hpg_core/arl/ail_tests_arl.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2024 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include "shared/test/common/helpers/default_hw_info.h"
+#include "shared/test/common/mocks/mock_ail_configuration.h"
+#include "shared/test/common/test_macros/hw_test.h"
+
+namespace NEO {
+
+using AILTestsARL = ::testing::Test;
+
+HWTEST2_F(AILTestsARL, givenArlWhenSvchostAppIsDetectedThenDisableDirectSubmission, IsARL) {
+    AILWhitebox<productFamily> ail;
+
+    auto capabilityTable = defaultHwInfo->capabilityTable;
+    auto defaultEngineSupportedValue = capabilityTable.directSubmissionEngines.data[aub_stream::ENGINE_CCS].engineSupported;
+
+    ail.processName = "UnknownProcess";
+    ail.apply(capabilityTable);
+    EXPECT_EQ(defaultEngineSupportedValue, capabilityTable.directSubmissionEngines.data[aub_stream::ENGINE_CCS].engineSupported);
+
+    ail.processName = "svchost";
+    ail.apply(capabilityTable);
+    EXPECT_FALSE(capabilityTable.directSubmissionEngines.data[aub_stream::ENGINE_CCS].engineSupported);
+}
+
+} // namespace NEO
