55404fd88827b96a7aa551176a808edab7348154
Mateusz Jablonski <mateusz.jablonski@intel.com>
2024-07-16 15:20:04 +0000

    refactor: move linux kernel headers to separate folders per source
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

--- cmake/find_drm.cmake
+++ cmake/find_drm.cmake
@@ -5,31 +5,31 @@
 #
 
 if(NOT DEFINED NEO_DRM_HEADERS_DIR OR NEO_DRM_HEADERS_DIR STREQUAL "")
-  get_filename_component(NEO_DRM_HEADERS_DIR "${NEO_SOURCE_DIR}/third_party/uapi/drm" ABSOLUTE)
+  get_filename_component(NEO_DRM_HEADERS_DIR "${NEO_SOURCE_DIR}/third_party/uapi/upstream/drm" ABSOLUTE)
 endif()
 
 message(STATUS "drm includes dir: ${NEO_DRM_HEADERS_DIR}")
 
 if(NOT DEFINED NEO_I915_HEADERS_DIR OR NEO_I915_HEADERS_DIR STREQUAL "")
-  get_filename_component(NEO_I915_HEADERS_DIR "${NEO_SOURCE_DIR}/third_party/uapi/i915" ABSOLUTE)
+  get_filename_component(NEO_I915_HEADERS_DIR "${NEO_SOURCE_DIR}/third_party/uapi/upstream/i915" ABSOLUTE)
 endif()
 
 message(STATUS "i915 includes dir: ${NEO_I915_HEADERS_DIR}")
 
 if(NOT DEFINED NEO_I915_PRELIM_HEADERS_DIR OR NEO_I915_PRELIM_HEADERS_DIR STREQUAL "")
-  get_filename_component(NEO_I915_PRELIM_HEADERS_DIR "${NEO_SOURCE_DIR}/third_party${BRANCH_DIR_SUFFIX}uapi/i915/prelim" ABSOLUTE)
+  get_filename_component(NEO_I915_PRELIM_HEADERS_DIR "${NEO_SOURCE_DIR}/third_party/drm-uapi-helper" ABSOLUTE)
 endif()
 
 message(STATUS "i915 prelim includes dir: ${NEO_I915_PRELIM_HEADERS_DIR}")
 
 if(NOT DEFINED NEO_XE_HEADERS_DIR OR NEO_XE_HEADERS_DIR STREQUAL "")
-  get_filename_component(NEO_XE_HEADERS_DIR "${NEO_SOURCE_DIR}/third_party${BRANCH_DIR_SUFFIX}uapi/xe" ABSOLUTE)
+  get_filename_component(NEO_XE_HEADERS_DIR "${NEO_SOURCE_DIR}/third_party/uapi/upstream/xe" ABSOLUTE)
 endif()
 
 message(STATUS "xe includes dir: ${NEO_XE_HEADERS_DIR}")
 
 if(NOT DEFINED NEO_IAF_HEADERS_DIR OR NEO_IAF_HEADERS_DIR STREQUAL "")
-  get_filename_component(NEO_IAF_HEADERS_DIR "${NEO_SOURCE_DIR}/third_party/uapi/iaf" ABSOLUTE)
+  get_filename_component(NEO_IAF_HEADERS_DIR "${NEO_SOURCE_DIR}/third_party/uapi/drm-uapi-helper" ABSOLUTE)
 endif()
 
 message(STATUS "iaf includes dir: ${NEO_IAF_HEADERS_DIR}")
--- third_party/uapi/README.md
+++ third_party/uapi/README.md
@@ -9,16 +9,7 @@ SPDX-License-Identifier: MIT
 
 Source locations of Linux kernel interface headers:
 
-1. https://repositories.intel.com/graphics/kernel-api/index.html
-    - dg1/i915_drm.h
-2. https://git.kernel.org/stable/h/v6.9.9
-    - drm/drm.h
-    - drm/drm_mode.h
-    - drm/drm_fourcc.h
-    - i915/i915.h
-    - xe/xe_drm.h
-3. https://github.com/intel-gpu/drm-uapi-helper/releases/tag/v2.0-rc24
-    - i915/prelim/i915.h
-    - i915/prelim/i915_prelim.h
-    - iaf/iaf_netlink.h
+1. dg1: https://repositories.intel.com/graphics/kernel-api/index.html
+2. drm-uapi-helper: https://github.com/intel-gpu/drm-uapi-helper
+3. upstream: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
      
