ff4a653c2e01a0d534c08fd831643ddcb3b3b58e
Lukasz Jobczyk <lukasz.jobczyk@intel.com>
2024-08-22 09:08:21 +0000

    fix: Remove UC PAT override for shared images on BMG
    Signed-off-by: Lukasz Jobczyk <lukasz.jobczyk@intel.com>

--- shared/test/unit_test/xe2_hpg_core/bmg/windows/product_helper_tests_bmg_windows.cpp
+++ shared/test/unit_test/xe2_hpg_core/bmg/windows/product_helper_tests_bmg_windows.cpp
@@ -5,7 +5,6 @@
  *
  */
 
-#include "shared/source/memory_manager/allocation_type.h"
 #include "shared/source/os_interface/product_helper.h"
 #include "shared/source/xe2_hpg_core/hw_cmds_bmg.h"
 #include "shared/source/xe2_hpg_core/hw_info_xe2_hpg_core.h"
@@ -25,28 +24,4 @@ BMGTEST_F(BmgProductHelperWindows, givenProductHelperWhenCheckDirectSubmissionSu
 
 BMGTEST_F(BmgProductHelperWindows, givenProductHelperWhenIsStagingBuffersEnabledThenTrueIsReturned) {
     EXPECT_TRUE(productHelper->isStagingBuffersEnabled());
-}
-
-BMGTEST_F(BmgProductHelperWindows, givenProductHelperWhenOverridePatIndexCalledThenCorrectValueIsReturned) {
-    DebugManagerStateRestore restorer;
-
-    uint64_t expectedPatIndex = 2u;
-    for (int i = 0; i < static_cast<int>(AllocationType::count); ++i) {
-        EXPECT_EQ(expectedPatIndex, productHelper->overridePatIndex(0u, expectedPatIndex, static_cast<AllocationType>(i)));
-    }
-
-    expectedPatIndex = 8u;
-    for (int i = 0; i < static_cast<int>(AllocationType::count); ++i) {
-        if (static_cast<AllocationType>(i) == AllocationType::sharedImage) {
-            EXPECT_EQ(13u, productHelper->overridePatIndex(0u, expectedPatIndex, static_cast<AllocationType>(i)));
-        } else {
-            EXPECT_EQ(expectedPatIndex, productHelper->overridePatIndex(0u, expectedPatIndex, static_cast<AllocationType>(i)));
-        }
-    }
-
-    debugManager.flags.OverrideUncachedSharedImages.set(0);
-
-    for (int i = 0; i < static_cast<int>(AllocationType::count); ++i) {
-        EXPECT_EQ(expectedPatIndex, productHelper->overridePatIndex(0u, expectedPatIndex, static_cast<AllocationType>(i)));
-    }
 }
