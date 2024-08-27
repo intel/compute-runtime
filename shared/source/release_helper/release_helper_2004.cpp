734b15314b95aa96a472da176dc1de7a23e291fa
Fabian Zwoliński <fabian.zwolinski@intel.com>
2024-08-27 12:13:19 +0000

    feature: enable bindless mode in L0 on LNL
    Related-To: NEO-7063
    Signed-off-by: Fabian Zwoliński <fabian.zwolinski@intel.com>

--- shared/source/release_helper/release_helper_2004.cpp
+++ shared/source/release_helper/release_helper_2004.cpp
@@ -37,6 +37,12 @@ template <>
 bool ReleaseHelperHw<release>::isLocalOnlyAllowed() const {
     return false;
 }
+
+template <>
+bool ReleaseHelperHw<release>::isBindlessAddressingDisabled() const {
+    return false;
+}
+
 } // namespace NEO
 
 #include "shared/source/release_helper/release_helper_common_xe2_hpg.inl"
