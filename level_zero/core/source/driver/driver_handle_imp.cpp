f5b88f085a4e51bf2f09fcff57f05b68a4ef2b17
Aravind Gopalakrishnan <aravind.gopalakrishnan@intel.com>
2024-09-12 18:58:23 +0000

    feature: Update L0 Core release version to v1.6
    Related-To: NEO-12628
    
    Signed-off-by: Aravind Gopalakrishnan <aravind.gopalakrishnan@intel.com>

--- level_zero/core/source/driver/driver_handle_imp.cpp
+++ level_zero/core/source/driver/driver_handle_imp.cpp
@@ -110,7 +110,7 @@ NEO::SVMAllocsManager *DriverHandleImp::getSvmAllocsManager() {
 }
 
 ze_result_t DriverHandleImp::getApiVersion(ze_api_version_t *version) {
-    *version = ZE_API_VERSION_1_5;
+    *version = ZE_API_VERSION_1_6;
     return ZE_RESULT_SUCCESS;
 }
 
