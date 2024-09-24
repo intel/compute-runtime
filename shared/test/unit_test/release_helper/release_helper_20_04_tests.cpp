2f4d240779534414ea54faa84e947898be333ea6
Mateusz Hoppe <mateusz.hoppe@intel.com>
2024-09-24 20:29:35 +0000

    feature: enable global bindless allocator on xe2hpg
    Related-To: NEO-7063, NEO-11533
    
    Signed-off-by: Mateusz Hoppe <mateusz.hoppe@intel.com>

--- shared/test/unit_test/release_helper/release_helper_20_04_tests.cpp
+++ shared/test/unit_test/release_helper/release_helper_20_04_tests.cpp
@@ -40,6 +40,7 @@ TEST_F(ReleaseHelper2004Tests, whenGettingCapabilitiesThenCorrectPropertiesAreRe
         EXPECT_FALSE(releaseHelper->isBindlessAddressingDisabled());
         EXPECT_EQ(8u, releaseHelper->getNumThreadsPerEu());
         EXPECT_TRUE(releaseHelper->isRayTracingSupported());
+        EXPECT_TRUE(releaseHelper->isGlobalBindlessAllocatorEnabled());
     }
 }
 
