b520c6477521c949d9e2cccc24d6a723f5385563
Szymon Morek <szymon.morek@intel.com>
2024-10-07 14:21:54 +0000

    performance: Enable direct submission on DG2 Windows
    Signed-off-by: Szymon Morek <szymon.morek@intel.com>

--- shared/source/xe_hpg_core/windows/product_helper_dg2.cpp
+++ shared/source/xe_hpg_core/windows/product_helper_dg2.cpp
@@ -23,7 +23,7 @@ namespace NEO {
 
 template <>
 bool ProductHelperHw<gfxProduct>::isDirectSubmissionSupported(ReleaseHelper *releaseHelper) const {
-    return false;
+    return true;
 }
 
 template <>
