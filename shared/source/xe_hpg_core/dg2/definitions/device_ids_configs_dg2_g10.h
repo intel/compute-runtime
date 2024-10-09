0168067c9c1cbd667bb0e848647ac3534561640a
Mateusz Jablonski <mateusz.jablonski@intel.com>
2024-10-09 12:59:02 +0000

    refactor: define dg2 g10 device ids in separate file
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

---
+++ shared/source/xe_hpg_core/dg2/definitions/device_ids_configs_dg2_g10.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2024 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#pragma once
+
+#include <vector>
+
+namespace NEO {
+const std::vector<unsigned short> dg2G10DeviceIds{
+    0x4F80,
+    0x4F81,
+    0x4F82,
+    0x4F83,
+    0x4F84,
+    0x5690,
+    0x5691,
+    0x5692,
+    0x56A0,
+    0x56A1,
+    0x56A2,
+    0x56C0,
+    0x56C2,
+    0x56BE,
+    0x56BF};
+} // namespace NEO
--- shared/source/xe_hpg_core/dg2/device_ids_configs_dg2.h
+++ shared/source/xe_hpg_core/dg2/device_ids_configs_dg2.h
@@ -7,26 +7,11 @@
 
 #pragma once
 
+#include "device_ids_configs_dg2_g10.h"
+
 #include <vector>
 
 namespace NEO {
-const std::vector<unsigned short> dg2G10DeviceIds{
-    0x4F80,
-    0x4F81,
-    0x4F82,
-    0x4F83,
-    0x4F84,
-    0x5690,
-    0x5691,
-    0x5692,
-    0x56A0,
-    0x56A1,
-    0x56A2,
-    0x56C0,
-    0x56C2,
-    0x56BE,
-    0x56BF};
-
 const std::vector<unsigned short> dg2G11DeviceIds{
     0x4F87,
     0x4F88,
