cf0acca62f1148e9687457b0d6061b1cd35d71a2
Dominik Dabek <dominik.dabek@intel.com>
2024-12-03 12:29:14 +0000

    fix: disable indirect detection, PVC
    Related-To: NEO-13372, GSD-10403
    
    Signed-off-by: Dominik Dabek <dominik.dabek@intel.com>
    Source: 5167f34a8ae352ee0f788ccc11e5bad563ef93fd

--- shared/source/helpers/definitions/indirect_detection_versions.h
+++ shared/source/helpers/definitions/indirect_detection_versions.h
@@ -11,9 +11,9 @@
 
 namespace NEO {
 namespace IndirectDetectionVersions {
-constexpr uint32_t requiredDetectIndirectVersionPVC = 3u;
+constexpr uint32_t disabled = std::numeric_limits<uint32_t>::max();
+constexpr uint32_t requiredDetectIndirectVersionPVC = disabled;
 constexpr uint32_t requiredDetectIndirectVersionBindless = 4u;
 constexpr uint32_t requiredDetectIndirectVersionVectorCompiler = 6u;
-constexpr uint32_t disabled = std::numeric_limits<uint32_t>::max();
 } // namespace IndirectDetectionVersions
 } // namespace NEO
