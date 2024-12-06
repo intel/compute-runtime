8f671cb6a8db2fb321b83c7c3c0302c436103e4d
Lukasz Jobczyk <lukasz.jobczyk@intel.com>
2024-12-06 10:49:54 +0000

    fix: Disable dc flush mitigation
    Signed-off-by: Lukasz Jobczyk <lukasz.jobczyk@intel.com>

--- shared/source/xe2_hpg_core/windows/product_helper_lnl.cpp
+++ shared/source/xe2_hpg_core/windows/product_helper_lnl.cpp
@@ -53,11 +53,6 @@ bool ProductHelperHw<gfxProduct>::isDirectSubmissionSupported(ReleaseHelper *rel
 }
 
 template <>
-bool ProductHelperHw<gfxProduct>::mitigateDcFlush() const {
-    return true;
-}
-
-template <>
 bool ProductHelperHw<gfxProduct>::restartDirectSubmissionForHostptrFree() const {
     return true;
 }
