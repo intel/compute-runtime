2062c1370401ea52db0a659f484c589658d4703c
Pawel Cieslak <pawel.cieslak@intel.com>
2024-12-17 14:56:07 +0000

    build: allow App Verifier to run as different user
    Related-To: NEO-9832
    Signed-off-by: Pawel Cieslak <pawel.cieslak@intel.com>

--- scripts/verify.bat
+++ scripts/verify.bat
@@ -1,5 +1,5 @@
 ::
-:: Copyright (C) 2020-2021 Intel Corporation
+:: Copyright (C) 2020-2024 Intel Corporation
 ::
 :: SPDX-License-Identifier: MIT
 ::
@@ -12,11 +12,19 @@
   goto end
 )
 
-appverif.exe -enable Exceptions Handles Heaps Leak Locks Memory Threadpool TLS DirtyStacks -for %1
-%*
-set testError=%errorlevel%
-echo App Verifier returned: %testError%
-appverif.exe -disable * -for * > nul
+@if not "%NEO_APPVERIF_USER%"=="" (
+  powershell -Command "$password = $Env:NEO_APPVERIF_PASS | ConvertTo-SecureString -AsPlainText -Force;$credential = [PSCredential]::New($Env:NEO_APPVERIF_USER,$password);Invoke-Command -ComputerName $(hostname) -Credential $credential -ScriptBlock {appverif.exe -enable Exceptions Handles Heaps Leak Locks Memory Threadpool TLS DirtyStacks -for %1}"
+  %*
+  set testError=%errorlevel%
+  echo App Verifier returned: %testError%
+  powershell -Command "$password = $Env:NEO_APPVERIF_PASS | ConvertTo-SecureString -AsPlainText -Force;$credential = [PSCredential]::New($Env:NEO_APPVERIF_USER,$password);Invoke-Command -ComputerName $(hostname) -Credential $credential -ScriptBlock {appverif.exe -disable * -for *}" > nul
+) else (
+  appverif.exe -enable Exceptions Handles Heaps Leak Locks Memory Threadpool TLS DirtyStacks -for %1
+  %*
+  set testError=%errorlevel%
+  echo App Verifier returned: %testError%
+  appverif.exe -disable * -for * > nul
+)
 
 :end
 exit /b %testError%
