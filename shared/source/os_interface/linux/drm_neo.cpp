474005de7d7830ac42e3f1a527b23da24b75d922
Mateusz Jablonski <mateusz.jablonski@intel.com>
2024-12-17 11:48:52 +0000

    refactor: move Drm methods definition to drm_neo.cpp file
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

--- shared/source/os_interface/linux/drm_neo.cpp
+++ shared/source/os_interface/linux/drm_neo.cpp
@@ -39,6 +39,7 @@
 #include "shared/source/os_interface/linux/pci_path.h"
 #include "shared/source/os_interface/linux/sys_calls.h"
 #include "shared/source/os_interface/linux/system_info.h"
+#include "shared/source/os_interface/linux/xe/ioctl_helper_xe.h"
 #include "shared/source/os_interface/os_environment.h"
 #include "shared/source/os_interface/os_interface.h"
 #include "shared/source/os_interface/product_helper.h"
@@ -1729,6 +1730,22 @@ uint64_t Drm::alignUpGttSize(uint64_t inputGttSize) {
         return gttSize48bit;
     }
     return inputGttSize;
+}
+
+bool Drm::isDrmSupported(int fileDescriptor) {
+    auto drmVersion = Drm::getDrmVersion(fileDescriptor);
+    return "i915" == drmVersion || "xe" == drmVersion;
+}
+
+bool Drm::queryDeviceIdAndRevision() {
+    auto drmVersion = Drm::getDrmVersion(getFileDescriptor());
+    if ("xe" == drmVersion) {
+        this->ioctlHelper = IoctlHelperXe::create(*this);
+        auto xeIoctlHelperPtr = static_cast<IoctlHelperXe *>(this->ioctlHelper.get());
+        this->setPerContextVMRequired(false);
+        return xeIoctlHelperPtr->initialize();
+    }
+    return queryI915DeviceIdAndRevision();
 }
 
 template std::vector<uint16_t> Drm::query<uint16_t>(uint32_t queryId, uint32_t queryItemFlags);
