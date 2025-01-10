5218923b6d323ab699eb092c2ab5677da405d788
Pryba, Michal <michal.pryba@intel.com>
2025-01-10 16:07:33 +0000

    feature: add automotive sku to alchemist product family
    Related-To: NEO-12234
    Signed-off-by: Pryba, Michal <michal.pryba@intel.com>

--- shared/source/xe_hpg_core/dg2/definitions/device_ids_configs_dg2_g10.h
+++ shared/source/xe_hpg_core/dg2/definitions/device_ids_configs_dg2_g10.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Intel Corporation
+ * Copyright (C) 2024-2025 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
@@ -25,5 +25,6 @@ const std::vector<unsigned short> dg2G10DeviceIds{
     0x56C0,
     0x56C2,
     0x56BE,
-    0x56BF};
+    0x56BF,
+    0x56AF};
 } // namespace NEO
