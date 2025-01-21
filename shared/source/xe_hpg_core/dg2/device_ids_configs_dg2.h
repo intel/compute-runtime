efa61ff06919d1954a70ec47192c179ccad3c455
Michał Pryba <michal.pryba@intel.com>
2025-01-21 17:29:55 +0000

    refactor: unify automotive sku device-id location
    Related-To: NEO-12234
    Signed-off-by: Michał Pryba <michal.pryba@intel.com>

--- shared/source/xe_hpg_core/dg2/device_ids_configs_dg2.h
+++ shared/source/xe_hpg_core/dg2/device_ids_configs_dg2.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022-2024 Intel Corporation
+ * Copyright (C) 2022-2025 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
@@ -7,11 +7,27 @@
 
 #pragma once
 
-#include "device_ids_configs_dg2_g10.h"
-
 #include <vector>
 
 namespace NEO {
+const std::vector<unsigned short> dg2G10DeviceIds{
+    0x4F80,
+    0x4F81,
+    0x4F82,
+    0x4F83,
+    0x4F84,
+    0x5690,
+    0x5691,
+    0x5692,
+    0x56A0,
+    0x56A1,
+    0x56A2,
+    0x56C0,
+    0x56C2,
+    0x56BE,
+    0x56BF,
+    0x56AF};
+
 const std::vector<unsigned short> dg2G11DeviceIds{
     0x4F87,
     0x4F88,
