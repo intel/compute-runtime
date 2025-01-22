2cdd9f46cdd27062170a8403cc270f2f7cf27ecf
Michał Pryba <michal.pryba@intel.com>
2025-01-22 13:10:44 +0000

    refactor: adjust file names after pre-gen12 removal 5/n
    Related-To: NEO-12681
    Signed-off-by: Michał Pryba <michal.pryba@intel.com>

--- opencl/source/gen12lp/gtpin_setup_gen12lp.cpp
+++ opencl/source/gen12lp/gtpin_setup_gen12lp.cpp
@@ -1,21 +1,32 @@
 /*
- * Copyright (C) 2019-2023 Intel Corporation
+ * Copyright (C) 2019-2025 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
  */
 
 #include "shared/source/compiler_interface/external_functions.h"
+#include "shared/source/debug_settings/debug_settings_manager.h"
 #include "shared/source/gen12lp/hw_cmds_base.h"
+#include "shared/source/helpers/hw_info.h"
 #include "shared/source/kernel/implicit_args_helper.h"
 
 #include "opencl/source/gtpin/gtpin_gfx_core_helper.h"
 #include "opencl/source/gtpin/gtpin_gfx_core_helper.inl"
-#include "opencl/source/gtpin/gtpin_gfx_core_helper_bdw_and_later.inl"
 
 #include "ocl_igc_shared/gtpin/gtpin_ocl_interface.h"
 
 namespace NEO {
+
+template <typename GfxFamily>
+bool GTPinGfxCoreHelperHw<GfxFamily>::canUseSharedAllocation(const HardwareInfo &hwInfo) const {
+    bool canUseSharedAllocation = false;
+    if (debugManager.flags.GTPinAllocateBufferInSharedMemory.get() != -1) {
+        canUseSharedAllocation = !!debugManager.flags.GTPinAllocateBufferInSharedMemory.get();
+    }
+    canUseSharedAllocation &= hwInfo.capabilityTable.ftrSvm;
+    return canUseSharedAllocation;
+}
 
 extern GTPinGfxCoreHelperCreateFunctionType gtpinGfxCoreHelperFactory[IGFX_MAX_CORE];
 
--- shared/source/gen12lp/command_stream_receiver_hw_gen12lp.cpp
+++ shared/source/gen12lp/command_stream_receiver_hw_gen12lp.cpp
@@ -10,15 +10,150 @@
 
 using Family = NEO::Gen12LpFamily;
 
-#include "shared/source/command_stream/command_stream_receiver_hw_bdw_and_later.inl"
+#include "shared/source/command_stream/command_stream_receiver_hw_base.inl"
 #include "shared/source/command_stream/command_stream_receiver_hw_heap_addressing.inl"
 #include "shared/source/command_stream/device_command_stream.h"
 #include "shared/source/gmm_helper/gmm.h"
+#include "shared/source/helpers/address_patch.h"
 #include "shared/source/helpers/blit_commands_helper_base.inl"
 #include "shared/source/helpers/populate_factory.h"
+#include "shared/source/helpers/state_base_address_tgllp_and_later.inl"
 
 namespace NEO {
 static auto gfxCore = IGFX_GEN12LP_CORE;
+
+template <typename GfxFamily>
+bool CommandStreamReceiverHw<GfxFamily>::are4GbHeapsAvailable() const { return true; }
+
+template <typename GfxFamily>
+size_t CommandStreamReceiverHw<GfxFamily>::getRequiredStateBaseAddressSize(const Device &device) const {
+    size_t size = 0;
+    const auto &productHelper = getProductHelper();
+    if (productHelper.is3DPipelineSelectWARequired()) {
+        size += (2 * PreambleHelper<GfxFamily>::getCmdSizeForPipelineSelect(peekRootDeviceEnvironment()));
+    }
+    size += sizeof(typename GfxFamily::STATE_BASE_ADDRESS) + sizeof(PIPE_CONTROL);
+    return size;
+}
+
+template <typename GfxFamily>
+void CommandStreamReceiverHw<GfxFamily>::programPipelineSelect(LinearStream &commandStream, PipelineSelectArgs &pipelineSelectArgs) {
+    if (csrSizeRequestFlags.mediaSamplerConfigChanged || csrSizeRequestFlags.systolicPipelineSelectMode || !isPreambleSent) {
+        if (!isPipelineSelectAlreadyProgrammed()) {
+            PreambleHelper<GfxFamily>::programPipelineSelect(&commandStream, pipelineSelectArgs, peekRootDeviceEnvironment());
+        }
+        this->lastMediaSamplerConfig = pipelineSelectArgs.mediaSamplerRequired;
+        this->lastSystolicPipelineSelectMode = pipelineSelectArgs.systolicPipelineSelectMode;
+        this->streamProperties.pipelineSelect.setPropertiesAll(true, this->lastMediaSamplerConfig, this->lastSystolicPipelineSelectMode);
+        this->streamProperties.pipelineSelect.clearIsDirty();
+    }
+}
+
+template <typename GfxFamily>
+void CommandStreamReceiverHw<GfxFamily>::createScratchSpaceController() {
+    scratchSpaceController = std::make_unique<ScratchSpaceControllerBase>(rootDeviceIndex, executionEnvironment, *internalAllocationStorage.get());
+}
+
+template <typename GfxFamily>
+void CommandStreamReceiverHw<GfxFamily>::programEpliogueCommands(LinearStream &csr, const DispatchFlags &dispatchFlags) {
+    this->programEngineModeEpliogue(csr, dispatchFlags);
+}
+
+template <typename GfxFamily>
+size_t CommandStreamReceiverHw<GfxFamily>::getCmdSizeForEpilogueCommands(const DispatchFlags &dispatchFlags) const {
+    return this->getCmdSizeForEngineMode(dispatchFlags);
+}
+
+template <typename GfxFamily>
+bool CommandStreamReceiverHw<GfxFamily>::isMultiOsContextCapable() const {
+    return false;
+}
+
+template <typename GfxFamily>
+inline void CommandStreamReceiverHw<GfxFamily>::addPipeControlBeforeStateSip(LinearStream &commandStream, Device &device) {}
+
+template <typename GfxFamily>
+inline void CommandStreamReceiverHw<GfxFamily>::addPipeControlBefore3dState(LinearStream &commandStream, DispatchFlags &dispatchFlags) {}
+
+template <typename GfxFamily>
+bool CommandStreamReceiverHw<GfxFamily>::checkPlatformSupportsNewResourceImplicitFlush() const {
+    return false;
+}
+
+template <typename GfxFamily>
+bool CommandStreamReceiverHw<GfxFamily>::checkPlatformSupportsGpuIdleImplicitFlush() const {
+    return false;
+}
+
+template <typename GfxFamily>
+GraphicsAllocation *CommandStreamReceiverHw<GfxFamily>::getClearColorAllocation() {
+    return nullptr;
+}
+
+template <typename GfxFamily>
+void CommandStreamReceiverHw<GfxFamily>::programPerDssBackedBuffer(LinearStream &commandStream, Device &device, DispatchFlags &dispatchFlags) {
+}
+
+template <typename GfxFamily>
+size_t CommandStreamReceiverHw<GfxFamily>::getCmdSizeForPerDssBackedBuffer(const HardwareInfo &hwInfo) {
+    return 0;
+}
+
+template <typename GfxFamily>
+void CommandStreamReceiverHw<GfxFamily>::dispatchRayTracingStateCommand(LinearStream &cmdStream, Device &device) {
+}
+
+template <typename GfxFamily>
+void CommandStreamReceiverHw<GfxFamily>::collectStateBaseAddresIohPatchInfo(uint64_t commandBufferAddress, uint64_t commandOffset, const LinearStream &ioh) {
+    using STATE_BASE_ADDRESS = typename GfxFamily::STATE_BASE_ADDRESS;
+
+    PatchInfoData indirectObjectPatchInfo = {ioh.getGraphicsAllocation()->getGpuAddress(), 0u, PatchInfoAllocationType::indirectObjectHeap, commandBufferAddress,
+                                             commandOffset + STATE_BASE_ADDRESS::PATCH_CONSTANTS::INDIRECTOBJECTBASEADDRESS_BYTEOFFSET, PatchInfoAllocationType::defaultType};
+    flatBatchBufferHelper->setPatchInfoData(indirectObjectPatchInfo);
+}
+
+template <typename GfxFamily>
+size_t CommandStreamReceiverHw<GfxFamily>::getCmdSizeForActivePartitionConfig() const {
+    return 0;
+}
+
+template <typename GfxFamily>
+inline void CommandStreamReceiverHw<GfxFamily>::programActivePartitionConfig(LinearStream &csr) {
+}
+
+template <typename GfxFamily>
+inline size_t CommandStreamReceiverHw<GfxFamily>::getCmdSizeForStallingNoPostSyncCommands() const {
+    return sizeof(typename GfxFamily::PIPE_CONTROL);
+}
+
+template <typename GfxFamily>
+inline size_t CommandStreamReceiverHw<GfxFamily>::getCmdSizeForStallingPostSyncCommands() const {
+    return MemorySynchronizationCommands<GfxFamily>::getSizeForBarrierWithPostSyncOperation(peekRootDeviceEnvironment(), false);
+}
+
+template <typename GfxFamily>
+inline void CommandStreamReceiverHw<GfxFamily>::programStallingNoPostSyncCommandsForBarrier(LinearStream &cmdStream) {
+    PipeControlArgs args;
+    MemorySynchronizationCommands<GfxFamily>::addSingleBarrier(cmdStream, args);
+}
+
+template <typename GfxFamily>
+inline void CommandStreamReceiverHw<GfxFamily>::programStallingPostSyncCommandsForBarrier(LinearStream &cmdStream, TagNodeBase &tagNode, bool dcFlushRequired) {
+    auto barrierTimestampPacketGpuAddress = TimestampPacketHelper::getContextEndGpuAddress(tagNode);
+    PipeControlArgs args;
+    args.dcFlushEnable = this->dcFlushSupport && dcFlushRequired;
+    MemorySynchronizationCommands<GfxFamily>::addBarrierWithPostSyncOperation(
+        cmdStream,
+        PostSyncMode::immediateData,
+        barrierTimestampPacketGpuAddress,
+        0,
+        peekRootDeviceEnvironment(),
+        args);
+}
+
+template <typename GfxFamily>
+inline void CommandStreamReceiverHw<GfxFamily>::configurePostSyncWriteOffset() {
+}
 
 template <typename GfxFamily>
 uint64_t BlitCommandsHelper<GfxFamily>::getMaxBlitWidthOverride(const RootDeviceEnvironment &rootDeviceEnvironment) {
