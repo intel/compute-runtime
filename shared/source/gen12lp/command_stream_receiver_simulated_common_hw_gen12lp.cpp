9119a1e802eea9bef79fd88e6c003397427971ca
Michał Pryba <michal.pryba@intel.com>
2025-01-22 13:10:44 +0000

    refactor: adjust file names after pre-gen12 removal 6/n
    Related-To: NEO-12681
    Signed-off-by: Michał Pryba <michal.pryba@intel.com>

--- shared/source/gen12lp/command_stream_receiver_simulated_common_hw_gen12lp.cpp
+++ shared/source/gen12lp/command_stream_receiver_simulated_common_hw_gen12lp.cpp
@@ -1,14 +1,41 @@
 /*
- * Copyright (C) 2019-2023 Intel Corporation
+ * Copyright (C) 2019-2025 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
  */
 
-#include "shared/source/command_stream/command_stream_receiver_simulated_common_hw_bdw_and_later.inl"
+#include "shared/source/command_stream/command_stream_receiver_simulated_common_hw_base.inl"
 
 namespace NEO {
 typedef Gen12LpFamily Family;
+
+template <typename GfxFamily>
+void CommandStreamReceiverSimulatedCommonHw<GfxFamily>::initGlobalMMIO() {
+    for (auto &mmioPair : AUBFamilyMapper<GfxFamily>::globalMMIO) {
+        stream->writeMMIO(mmioPair.first, mmioPair.second);
+    }
+}
+
+template <typename GfxFamily>
+uint32_t CommandStreamReceiverSimulatedCommonHw<GfxFamily>::getMemoryBankForGtt() const {
+    return MemoryBanks::getBank(getDeviceIndex());
+}
+
+template <typename GfxFamily>
+const AubMemDump::LrcaHelper &CommandStreamReceiverSimulatedCommonHw<GfxFamily>::getCsTraits(aub_stream::EngineType engineType) {
+    return *AUBFamilyMapper<GfxFamily>::csTraits[engineType];
+}
+
+template <typename GfxFamily>
+void CommandStreamReceiverSimulatedCommonHw<GfxFamily>::initEngineMMIO() {
+    auto mmioList = AUBFamilyMapper<GfxFamily>::perEngineMMIO[osContext->getEngineType()];
+
+    DEBUG_BREAK_IF(!mmioList);
+    for (auto &mmioPair : *mmioList) {
+        stream->writeMMIO(mmioPair.first, mmioPair.second);
+    }
+}
 
 template <>
 void CommandStreamReceiverSimulatedCommonHw<Family>::initGlobalMMIO() {
