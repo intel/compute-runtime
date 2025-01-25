c265e9bc7a443748e0f160f61399a3bd5394391f
Mateusz Hoppe <mateusz.hoppe@intel.com>
2025-01-25 07:18:48 +0000

    test: disable context group in OCL DeviceGetCapsTest
    Related-To: NEO-13789
    
    Signed-off-by: Mateusz Hoppe <mateusz.hoppe@intel.com>

--- opencl/test/unit_test/device/device_caps_tests.cpp
+++ opencl/test/unit_test/device/device_caps_tests.cpp
@@ -42,6 +42,7 @@ using namespace NEO;
 
 struct DeviceGetCapsTest : public ::testing::Test {
     void SetUp() override {
+        debugManager.flags.ContextGroupSize.set(0);
         MockSipData::clearUseFlags();
         backupSipInitType = std::make_unique<VariableBackup<bool>>(&MockSipData::useMockSip, true);
     }
@@ -130,6 +131,7 @@ struct DeviceGetCapsTest : public ::testing::Test {
         EXPECT_EQ(clDevice.getDeviceInfo().openclCFeatures.end(), ++openclCFeatureIterator);
     }
 
+    DebugManagerStateRestore restorer;
     std::unique_ptr<VariableBackup<bool>> backupSipInitType;
 };
 
