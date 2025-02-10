493a100d5f67a4c16b439f7dcc66ac56e7345ee4
Damian Tomczak <damian.tomczak@intel.com>
2025-02-10 04:10:50 +0000

    test: additional chaining sempahore required when dcFlushRequired
    Related-to: NEO-12737
    
    Signed-off-by: Damian Tomczak <damian.tomczak@intel.com>

--- level_zero/core/test/unit_tests/sources/cmdlist/test_in_order_cmdlist_1.cpp
+++ level_zero/core/test/unit_tests/sources/cmdlist/test_in_order_cmdlist_1.cpp
@@ -1904,11 +1904,11 @@ HWTEST2_F(InOrderCmdListTests, givenCmdsChainingFromAppendCopyWhenDispatchingKer
 
     offset = cmdStream->getUsed();
     immCmdList->appendMemoryCopyRegion(alloc, &region, 1, 1, alloc, &region, 1, 1, eventHandle, 0, nullptr, copyParams);
-    findSemaphores(heaplessEnabled ? 1 : 2); // implicit dependency + chaining
+    findSemaphores((heaplessEnabled && !immCmdList->dcFlushSupport) ? 1 : 2); // implicit dependency + chaining
 
     offset = cmdStream->getUsed();
     immCmdList->appendMemoryCopyRegion(alloc, &region, 1, 1, alloc, &region, 1, 1, nullptr, 0, nullptr, copyParams);
-    findSemaphores(heaplessEnabled ? 1 : 0); // no implicit dependency
+    findSemaphores((heaplessEnabled && !immCmdList->dcFlushSupport) ? 1 : 0); // no implicit dependency
 
     context->freeMem(alloc);
 }
