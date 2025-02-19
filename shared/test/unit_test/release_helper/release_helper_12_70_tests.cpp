0b6b51fa19c243c3f9dca9ad0c531e1e1f29ca3f
Lukasz Jobczyk <lukasz.jobczyk@intel.com>
2025-02-19 13:55:30 +0000

    performance: Enable ULLS light for ARLS, ARLU, MTLU
    Related-To: NEO-13922
    
    Signed-off-by: Lukasz Jobczyk <lukasz.jobczyk@intel.com>

--- shared/test/unit_test/release_helper/release_helper_12_70_tests.cpp
+++ shared/test/unit_test/release_helper/release_helper_12_70_tests.cpp
@@ -43,6 +43,7 @@ TEST_F(ReleaseHelper1270Tests, whenGettingCapabilitiesThenCorrectPropertiesAreRe
         EXPECT_TRUE(releaseHelper->isRayTracingSupported());
         EXPECT_TRUE(releaseHelper->isNumRtStacksPerDssFixedValue());
         EXPECT_FALSE(releaseHelper->getFtrXe2Compression());
+        EXPECT_TRUE(releaseHelper->isDirectSubmissionLightSupported());
     }
 }
 
