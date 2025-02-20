0996247dfd4046e0d2bdbe79e56837e412abd87d
Compute-Runtime-Validation <compute-runtime-validation@intel.com>
2025-02-20 08:39:42 +0100

    Revert "performance: Enable ULLS light for MTLH"
    This reverts commit 7a62f27d8b2b41af00446d65732b0fca9a531219.
    
    Signed-off-by: Compute-Runtime-Validation <compute-runtime-validation@intel.com>

--- shared/test/unit_test/release_helper/release_helper_12_71_tests.cpp
+++ shared/test/unit_test/release_helper/release_helper_12_71_tests.cpp
@@ -43,7 +43,6 @@ TEST_F(ReleaseHelper1271Tests, whenGettingCapabilitiesThenCorrectPropertiesAreRe
         EXPECT_TRUE(releaseHelper->isRayTracingSupported());
         EXPECT_TRUE(releaseHelper->isNumRtStacksPerDssFixedValue());
         EXPECT_FALSE(releaseHelper->getFtrXe2Compression());
-        EXPECT_TRUE(releaseHelper->isDirectSubmissionLightSupported());
     }
 }
 
