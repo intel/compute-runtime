3d9fc8968e6dc12a30093212dd2cd99bf84b9c7c
Filip Hazubski <filip.hazubski@intel.com>
2025-03-28 12:07:37 +0000

    fix: Add BMG device id
    Add device ID: 0xE211
    
    Signed-off-by: Filip Hazubski <filip.hazubski@intel.com>

--- opencl/test/unit_test/xe2_hpg_core/bmg/linux/dll/device_id_tests_bmg.cpp
+++ opencl/test/unit_test/xe2_hpg_core/bmg/linux/dll/device_id_tests_bmg.cpp
@@ -10,12 +10,13 @@
 using namespace NEO;
 
 TEST_F(DeviceIdTests, GivenBmgSupportedDeviceIdThenDeviceDescriptorTableExists) {
-    std::array<DeviceDescriptor, 8> expectedDescriptors = {{
+    std::array<DeviceDescriptor, 9> expectedDescriptors = {{
         {0xE202, &BmgHwConfig::hwInfo, &BmgHwConfig::setupHardwareInfo},
         {0xE20B, &BmgHwConfig::hwInfo, &BmgHwConfig::setupHardwareInfo},
         {0xE20C, &BmgHwConfig::hwInfo, &BmgHwConfig::setupHardwareInfo},
         {0xE20D, &BmgHwConfig::hwInfo, &BmgHwConfig::setupHardwareInfo},
         {0xE210, &BmgHwConfig::hwInfo, &BmgHwConfig::setupHardwareInfo},
+        {0xE211, &BmgHwConfig::hwInfo, &BmgHwConfig::setupHardwareInfo},
         {0xE212, &BmgHwConfig::hwInfo, &BmgHwConfig::setupHardwareInfo},
         {0xE215, &BmgHwConfig::hwInfo, &BmgHwConfig::setupHardwareInfo},
         {0xE216, &BmgHwConfig::hwInfo, &BmgHwConfig::setupHardwareInfo},
