213e34135c0173268b6e273df64d05b9e2cd968a
Grzegorz Wawiorko <grzegorz.wawiorko@intel.com>
2025-04-16 11:19:02 +0000

    fix: Update CL_DEVICE_LATEST_CONFORMANCE_VERSION_PASSED value
    Signed-off-by: Grzegorz Wawiorko <grzegorz.wawiorko@intel.com>

--- opencl/source/cl_device/cl_device_caps.cpp
+++ opencl/source/cl_device/cl_device_caps.cpp
@@ -34,7 +34,7 @@ namespace NEO {
 static std::string vendor = "Intel(R) Corporation";
 static std::string profile = "FULL_PROFILE";
 static std::string spirVersions = "1.2 ";
-const char *latestConformanceVersionPassed = "v2024-02-27-00";
+const char *latestConformanceVersionPassed = "v2025-04-14-00";
 #define QTR(a) #a
 #define TOSTR(b) QTR(b)
 static std::string driverVersion = TOSTR(NEO_OCL_DRIVER_VERSION);
