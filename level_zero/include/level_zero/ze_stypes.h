711d250b8a4011cab9764bd4a65efdca301fc7cc
Mateusz Jablonski <mateusz.jablonski@intel.com>
2025-04-18 12:15:47 +0000

    refactor: remove redundant definition of stype
    Removed definitions are already defined in level-zero repo
    
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

--- level_zero/include/level_zero/ze_stypes.h
+++ level_zero/include/level_zero/ze_stypes.h
@@ -11,9 +11,6 @@
 #include <level_zero/ze_api.h>
 #include <level_zero/zet_api.h>
 
-#define ZE_STRUCTURE_TYPE_PITCHED_ALLOC_DEVICE_EXP_PROPERTIES (ze_structure_type_t)0x0002001D
-#define ZE_STRUCTURE_TYPE_BINDLESS_IMAGE_EXP_DESC (ze_structure_type_t)0x0002001E
-#define ZE_STRUCTURE_TYPE_PITCHED_IMAGE_EXP_DESC (ze_structure_type_t)0x0002001F
 #define ZE_STRUCTURE_TYPE_SYNCHRONIZED_DISPATCH_EXP_DESC (ze_structure_type_t)0x00020020
 #define ZE_STRUCTURE_TYPE_INTEL_MEDIA_COMMUNICATION_DESC (ze_structure_type_t)0x00020021
 #define ZE_STRUCTURE_TYPE_INTEL_MEDIA_DOORBELL_HANDLE_DESC (ze_structure_type_t)0x00020022
