936a3d2f3142a31adffc2e7e0eb6c4d04b4e1092
Filip Hazubski <filip.hazubski@intel.com>
2025-05-16 13:36:56 +0000

    build: kernels_bin update
    Correct CompilerInterfaceOclElfCacheTest fixture.
    
    Signed-off-by: Filip Hazubski <filip.hazubski@intel.com>

--- shared/test/unit_test/compiler_interface/compiler_cache_tests.cpp
+++ shared/test/unit_test/compiler_interface/compiler_cache_tests.cpp
@@ -501,11 +501,15 @@ class CompilerInterfaceOclElfCacheTest : public ::testing::Test, public Compiler
     using CompilerCacheHelper::processPackedCacheBinary;
 
     void SetUp() override {
+        hwInfoBackup.reset(new VariableBackup<HardwareInfo>(defaultHwInfo.get()));
         std::unique_ptr<CompilerCacheMock> cache(new CompilerCacheMock());
         cache->config.enabled = true;
         compilerInterface = std::make_unique<MockCompilerInterface>();
         bool initRet = compilerInterface->initialize(std::move(cache), true);
         ASSERT_TRUE(initRet);
+
+        auto compilerProductHelper = CompilerProductHelper::create(defaultHwInfo->platform.eProductFamily);
+        compilerProductHelper->adjustHwInfoForIgc(*defaultHwInfo);
 
         mockCompilerCache = static_cast<CompilerCacheMock *>(compilerInterface->cache.get());
 
@@ -552,6 +556,7 @@ class CompilerInterfaceOclElfCacheTest : public ::testing::Test, public Compiler
 
     std::unique_ptr<MockCompilerInterface> compilerInterface;
     CompilerCacheMock *mockCompilerCache;
+    std::unique_ptr<VariableBackup<HardwareInfo>> hwInfoBackup;
 };
 
 TEST_F(CompilerInterfaceOclElfCacheTest, givenIncorrectBinaryCausingPackDeviceBinaryToReturnEmptyVectorWhenPackAndCacheBinaryThenBinaryIsNotStoredInCache) {
