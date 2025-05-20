7ef85b5853cd525e2f3c91ce600a45a273513388
Jaroslaw Warchulski <jaroslaw.warchulski@intel.com>
2025-05-20 12:33:06 +0000

    feature: enable bindless mode and global bindless allocator in L0 on ARL
    Related-To: NEO-14995
    Signed-off-by: Jaroslaw Warchulski <jaroslaw.warchulski@intel.com>

--- shared/source/release_helper/release_helper_1274.cpp
+++ shared/source/release_helper/release_helper_1274.cpp
@@ -50,6 +50,16 @@ bool ReleaseHelperHw<release>::isBlitImageAllowedForDepthFormat() const {
 }
 
 template <>
+inline bool ReleaseHelperHw<release>::isBindlessAddressingDisabled() const {
+    return false;
+}
+
+template <>
+inline bool ReleaseHelperHw<release>::isGlobalBindlessAllocatorEnabled() const {
+    return true;
+}
+
+template <>
 const SizeToPreferredSlmValueArray &ReleaseHelperHw<release>::getSizeToPreferredSlmValue(bool isHeapless) const {
     using PREFERRED_SLM_ALLOCATION_SIZE = typename XeHpgCoreFamily::INTERFACE_DESCRIPTOR_DATA::PREFERRED_SLM_ALLOCATION_SIZE;
     static const SizeToPreferredSlmValueArray sizeToPreferredSlmValue = {{
--- shared/test/unit_test/release_helper/release_helper_12_74_tests.cpp
+++ shared/test/unit_test/release_helper/release_helper_12_74_tests.cpp
@@ -35,7 +35,8 @@ TEST_F(ReleaseHelper1274Tests, whenGettingCapabilitiesThenCorrectPropertiesAreRe
         EXPECT_TRUE(releaseHelper->isDirectSubmissionSupported());
         EXPECT_FALSE(releaseHelper->isAuxSurfaceModeOverrideRequired());
         EXPECT_FALSE(releaseHelper->isRcsExposureDisabled());
-        EXPECT_TRUE(releaseHelper->isBindlessAddressingDisabled());
+        EXPECT_FALSE(releaseHelper->isBindlessAddressingDisabled());
+        EXPECT_TRUE(releaseHelper->isGlobalBindlessAllocatorEnabled());
         EXPECT_EQ(8u, releaseHelper->getNumThreadsPerEu());
         EXPECT_EQ(0u, releaseHelper->getStackSizePerRay());
         EXPECT_TRUE(releaseHelper->isRayTracingSupported());
