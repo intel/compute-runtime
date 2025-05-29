ca3079f263148b2cd02532efe208c6af60d0fe1c
Mateusz Jablonski <mateusz.jablonski@intel.com>
2025-05-29 11:49:56 +0000

    fix: setup printf surface when implicit args are programmed
    even if main kernel is not using printf
    
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

--- opencl/source/kernel/kernel.cpp
+++ opencl/source/kernel/kernel.cpp
@@ -1793,7 +1793,7 @@ void Kernel::unsetArg(uint32_t argIndex) {
 }
 
 bool Kernel::hasPrintfOutput() const {
-    return kernelInfo.kernelDescriptor.kernelAttributes.flags.usesPrintf;
+    return kernelInfo.kernelDescriptor.kernelAttributes.flags.usesPrintf || (pImplicitArgs && !isBuiltIn);
 }
 
 void Kernel::resetSharedObjectsPatchAddresses() {
