9ca238a73b159d26f8e2f839347d62139c3f4555
Compute-Runtime-Validation <compute-runtime-validation@intel.com>
2025-07-01 00:39:54 +0200

    Revert "performance: Do not prefer blit enqueue on PTL"
    This reverts commit 62f2a86f161fab630464f58c906cbc9f4c8caba5.
    
    Signed-off-by: Compute-Runtime-Validation <compute-runtime-validation@intel.com>

--- shared/test/unit_test/xe3_core/ptl/excludes_xe3_core_ptl.cpp
+++ shared/test/unit_test/xe3_core/ptl/excludes_xe3_core_ptl.cpp
@@ -8,4 +8,3 @@
 #include "shared/test/common/test_macros/hw_test_base.h"
 
 HWTEST_EXCLUDE_PRODUCT(ProductHelperTest, whenGettingPreferredAllocationMethodThenNoPreferenceIsReturned, IGFX_PTL);
-HWTEST_EXCLUDE_PRODUCT(ProductHelperTest, givenProductHelperWhenCheckBlitEnqueuePreferredThenReturnTrue, IGFX_PTL);
