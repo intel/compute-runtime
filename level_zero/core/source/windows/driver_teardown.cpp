d952298321e776975eb5ee240f7969bbabbece2c
Compute-Runtime-Validation <compute-runtime-validation@intel.com>
2025-07-07 15:36:54 +0200

    Revert "fix: skip cleanup in L0 DllMain when terminating process"
    This reverts commit b70c7fd078643322e4d0d4a4d8f4039bef932cc5.
    
    Signed-off-by: Compute-Runtime-Validation <compute-runtime-validation@intel.com>

--- level_zero/core/source/windows/driver_teardown.cpp
+++ level_zero/core/source/windows/driver_teardown.cpp
@@ -17,14 +17,10 @@ BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
         L0::globalDriverSetup();
     }
     if (fdwReason == DLL_PROCESS_DETACH) {
-        /* If lpvReserved is non-NULL with DLL_PROCESS_DETACH, the process is terminating,
-         * cleanup should be skipped according to the DllMain spec. */
-        if (!lpvReserved) {
-            L0::globalDriverTeardown();
-            if (L0::globalOsSysmanDriver != nullptr) {
-                delete L0::globalOsSysmanDriver;
-                L0::globalOsSysmanDriver = nullptr;
-            }
+        L0::globalDriverTeardown();
+        if (L0::globalOsSysmanDriver != nullptr) {
+            delete L0::globalOsSysmanDriver;
+            L0::globalOsSysmanDriver = nullptr;
         }
     }
     return TRUE;
