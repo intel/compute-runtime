171b39846520065a422f97e0192bba0220ae6258
Grochowski, Stanislaw <stanislaw.grochowski@intel.com>
2025-07-11 12:53:01 +0000

    fix: Add PTL sku device ids
    Related-To: NEO-14999
    
    Signed-off-by: Grochowski, Stanislaw <stanislaw.grochowski@intel.com>

--- opencl/test/unit_test/xe3_core/ptl/linux/dll/device_id_tests_ptl.cpp
+++ opencl/test/unit_test/xe3_core/ptl/linux/dll/device_id_tests_ptl.cpp
@@ -10,10 +10,14 @@
 using namespace NEO;
 
 TEST_F(DeviceIdTests, GivenPtlSupportedDeviceIdThenDeviceDescriptorTableExists) {
-    std::array<DeviceDescriptor, 10> expectedDescriptors = {{{0xB080, &PtlHwConfig::hwInfo, &PtlHwConfig::setupHardwareInfo},
+    std::array<DeviceDescriptor, 14> expectedDescriptors = {{{0xB080, &PtlHwConfig::hwInfo, &PtlHwConfig::setupHardwareInfo},
                                                              {0xB081, &PtlHwConfig::hwInfo, &PtlHwConfig::setupHardwareInfo},
                                                              {0xB082, &PtlHwConfig::hwInfo, &PtlHwConfig::setupHardwareInfo},
                                                              {0xB083, &PtlHwConfig::hwInfo, &PtlHwConfig::setupHardwareInfo},
+                                                             {0xB084, &PtlHwConfig::hwInfo, &PtlHwConfig::setupHardwareInfo},
+                                                             {0xB085, &PtlHwConfig::hwInfo, &PtlHwConfig::setupHardwareInfo},
+                                                             {0xB086, &PtlHwConfig::hwInfo, &PtlHwConfig::setupHardwareInfo},
+                                                             {0xB087, &PtlHwConfig::hwInfo, &PtlHwConfig::setupHardwareInfo},
                                                              {0xB08F, &PtlHwConfig::hwInfo, &PtlHwConfig::setupHardwareInfo},
                                                              {0xB090, &PtlHwConfig::hwInfo, &PtlHwConfig::setupHardwareInfo},
                                                              {0xB0A0, &PtlHwConfig::hwInfo, &PtlHwConfig::setupHardwareInfo},
