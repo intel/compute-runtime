8bdc479fe77d66de60a07037ccef13068a504f7d
Mateusz Jablonski <mateusz.jablonski@intel.com>
2025-07-15 15:02:13 +0000

    fix: correct global device synchronization
    skip uninitialized command stream receivers
    
    Additionally, add L0 black box for kernel with arg slm
    
    Test verifies new simplified L0 API
    
    Functions:
    - zerDriverGetDefaultContext
    - zeDeviceSynchronize
    - zeCommandListAppendLaunchKernelWithArguments
    - zerIdentifierTranslateToDeviceHandle
    - zerDeviceTranslateToIdentifier
    - zerDriverGetLastErrorDescription
    
    Definitions:
    - defaultCommandQueueDesc
    - defaultHostMemDesc
    - defaultDeviceMemDesc
    
    Related-To: NEO-14560
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

---
+++ level_zero/core/test/black_box_tests/zello_arg_slm.cpp
@@ -0,0 +1,155 @@
+/*
+ * Copyright (C) 2025 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include "zello_common.h"
+#include "zello_compile.h"
+
+#include <cstring>
+
+constexpr std::string_view blackBoxName = "Zello Arg Slm";
+
+void executeKernelAndValidate(ze_context_handle_t context, uint32_t deviceIdentfier,
+                              bool &outputValidationSuccessful) {
+
+    if (LevelZeroBlackBoxTests::verbose) {
+        std::cout << "Testing for device " << deviceIdentfier << std::endl;
+    }
+    ze_command_list_handle_t cmdList;
+    auto device = LevelZeroBlackBoxTests::zerIdentifierTranslateToDeviceHandleFunc(deviceIdentfier);
+    SUCCESS_OR_TERMINATE(zeCommandListCreateImmediate(context, device, &defaultCommandQueueDesc, &cmdList));
+
+    constexpr ze_group_count_t groupCounts{16, 1, 1};
+
+    // Create output buffer
+    void *dstBuffer = nullptr;
+    constexpr size_t allocSize = groupCounts.groupCountX * sizeof(uint32_t) * 2;
+    SUCCESS_OR_TERMINATE(zeMemAllocShared(context, &defaultDeviceMemDesc, &defaultHostMemDesc, allocSize, sizeof(uint32_t), device, &dstBuffer));
+
+    std::string buildLog;
+    auto spirV = LevelZeroBlackBoxTests::compileToSpirV(LevelZeroBlackBoxTests::slmArgKernelSrc, "", buildLog);
+    LevelZeroBlackBoxTests::printBuildLog(buildLog);
+    SUCCESS_OR_TERMINATE((0 == spirV.size()));
+
+    ze_module_handle_t module = nullptr;
+    ze_kernel_handle_t kernel = nullptr;
+
+    ze_module_desc_t moduleDesc = {ZE_STRUCTURE_TYPE_MODULE_DESC};
+    ze_module_build_log_handle_t buildlog;
+    moduleDesc.format = ZE_MODULE_FORMAT_IL_SPIRV;
+    moduleDesc.pInputModule = spirV.data();
+    moduleDesc.inputSize = spirV.size();
+    moduleDesc.pBuildFlags = "";
+
+    if (zeModuleCreate(context, device, &moduleDesc, &module, &buildlog) != ZE_RESULT_SUCCESS) {
+        size_t szLog = 0;
+        zeModuleBuildLogGetString(buildlog, &szLog, nullptr);
+
+        std::vector<char> strLog(szLog + 1, 0);
+        zeModuleBuildLogGetString(buildlog, &szLog, strLog.data());
+        LevelZeroBlackBoxTests::printBuildLog(strLog.data());
+
+        SUCCESS_OR_TERMINATE(zeModuleBuildLogDestroy(buildlog));
+        std::cerr << std::endl
+                  << blackBoxName << " Results validation FAILED. Module creation error."
+                  << std::endl;
+        SUCCESS_OR_TERMINATE_BOOL(false);
+    }
+    SUCCESS_OR_TERMINATE(zeModuleBuildLogDestroy(buildlog));
+
+    ze_kernel_desc_t kernelDesc = {ZE_STRUCTURE_TYPE_KERNEL_DESC};
+    kernelDesc.pKernelName = "test_arg_slm";
+    SUCCESS_OR_TERMINATE(zeKernelCreate(module, &kernelDesc, &kernel));
+    ze_kernel_properties_t kernProps = {ZE_STRUCTURE_TYPE_KERNEL_PROPERTIES};
+    SUCCESS_OR_TERMINATE(zeKernelGetProperties(kernel, &kernProps));
+    LevelZeroBlackBoxTests::printKernelProperties(kernProps, kernelDesc.pKernelName);
+
+    for (auto groupSize : {64u, 128u, 256u}) {
+
+        // Initialize memory
+        constexpr uint8_t initValue = 77;
+        zeCommandListAppendMemoryFill(cmdList, dstBuffer, &initValue, sizeof(initValue), allocSize, nullptr, 0, nullptr);
+
+        ze_group_size_t groupSizes{groupSize, 1, 1};
+
+        size_t localWorkSizeForUint = groupSizes.groupSizeX * 4u;
+
+        const void *kernelArgs[] = {
+            &dstBuffer,            // output buffer
+            &localWorkSizeForUint, // local buffer for local ids
+            &localWorkSizeForUint  // local buffer for global ids
+        };
+
+        SUCCESS_OR_TERMINATE(LevelZeroBlackBoxTests::zeCommandListAppendLaunchKernelWithArgumentsFunc(cmdList, kernel, groupCounts, groupSizes, kernelArgs, nullptr, nullptr, 0, nullptr));
+
+        SUCCESS_OR_TERMINATE(LevelZeroBlackBoxTests::zeDeviceSynchronizeFunc(device));
+
+        // Validate
+        outputValidationSuccessful = true;
+
+        std::vector<uint32_t> expectedOutput(groupCounts.groupCountX * 2, 0);
+
+        for (auto i = 0; i < static_cast<int>(groupCounts.groupCountX); ++i) {
+            auto sumOfLocalIds = groupSize * (groupSize - 1) / 2;                                                          // Sum of local IDs from 0 to localWorkSize-1
+            auto maxGlobalId = groupSize * (i + 1) - 1;                                                                    // max global id for this group
+            auto minGlobalId = groupSize * i;                                                                              // min global id for this group
+            auto sumOfGlobalIdWithinGroup = (maxGlobalId * (maxGlobalId + 1) / 2) - (minGlobalId * (minGlobalId - 1) / 2); // sum of global ids within this group
+
+            expectedOutput[i * 2] = sumOfLocalIds;
+            expectedOutput[i * 2 + 1] = sumOfGlobalIdWithinGroup;
+        }
+        for (auto i = 0; i < static_cast<int>(expectedOutput.size()); ++i) {
+            auto expectedValue = expectedOutput[i];
+            auto actualValue = reinterpret_cast<uint32_t *>(dstBuffer)[i];
+            if (actualValue != expectedValue) {
+                std::cout << "dstBuffer[" << i << "] = "
+                          << std::dec << actualValue << " not equal to "
+                          << expectedValue << "\n";
+                outputValidationSuccessful = false;
+                break;
+            }
+        }
+        if (!outputValidationSuccessful) {
+            break;
+        }
+    }
+
+    // Cleanup
+    SUCCESS_OR_TERMINATE(zeMemFree(context, dstBuffer));
+    SUCCESS_OR_TERMINATE(zeCommandListDestroy(cmdList));
+    SUCCESS_OR_TERMINATE(zeKernelDestroy(kernel));
+    SUCCESS_OR_TERMINATE(zeModuleDestroy(module));
+}
+
+int main(int argc, char *argv[]) {
+    LevelZeroBlackBoxTests::verbose = LevelZeroBlackBoxTests::isVerbose(argc, argv);
+    bool aubMode = LevelZeroBlackBoxTests::isAubMode(argc, argv);
+
+    ze_context_handle_t context = nullptr;
+    const char *errorMsg = nullptr;
+    auto devices = LevelZeroBlackBoxTests::zelloInitContextAndGetDevices(context);
+    SUCCESS_OR_TERMINATE(LevelZeroBlackBoxTests::zerDriverGetLastErrorDescriptionFunc(&errorMsg));
+
+    if (errorMsg != nullptr && errorMsg[0] != 0) {
+        std::cerr << "Error initializing context: " << (errorMsg ? errorMsg : "Unknown error") << std::endl;
+        return 1;
+    }
+
+    uint32_t deviceOrdinal = LevelZeroBlackBoxTests::zerDeviceTranslateToIdentifierFunc(devices[0]);
+
+    SUCCESS_OR_TERMINATE(LevelZeroBlackBoxTests::zerDriverGetLastErrorDescriptionFunc(&errorMsg));
+
+    if (errorMsg != nullptr && errorMsg[0] != 0) {
+        std::cerr << "Error zerDeviceTranslateToIdentifier: " << errorMsg << std::endl;
+        return 1;
+    }
+    bool outputValidationSuccessful = false;
+    executeKernelAndValidate(context, deviceOrdinal, outputValidationSuccessful);
+
+    LevelZeroBlackBoxTests::printResult(aubMode, outputValidationSuccessful, blackBoxName);
+    outputValidationSuccessful = aubMode ? true : outputValidationSuccessful;
+    return (outputValidationSuccessful ? 0 : 1);
+}
--- level_zero/core/test/unit_tests/sources/device/test_l0_device.cpp
+++ level_zero/core/test/unit_tests/sources/device/test_l0_device.cpp
@@ -6521,6 +6521,7 @@ TEST_F(DeviceSimpleTests, whenWorkgroupSizeCheckedThenSizeLimitIs1kOrLess) {
 
 HWTEST_F(DeviceSimpleTests, givenGpuHangWhenSynchronizingDeviceThenErrorIsPropagated) {
     auto &csr = neoDevice->getUltCommandStreamReceiver<FamilyType>();
+    csr.resourcesInitialized = true;
     csr.waitForTaskCountWithKmdNotifyFallbackReturnValue = WaitStatus::gpuHang;
 
     auto result = zeDeviceSynchronize(device);
@@ -6540,6 +6541,7 @@ HWTEST_F(DeviceSimpleTests, givenNoGpuHangWhenSynchronizingDeviceThenCallWaitFor
         csr->flushStamp->setStamp(flushStampToWait++);
         csr->captureWaitForTaskCountWithKmdNotifyInputParams = true;
         csr->waitForTaskCountWithKmdNotifyFallbackReturnValue = WaitStatus::ready;
+        csr->resourcesInitialized = true;
     }
 
     auto &secondaryCsrs = neoDevice->getSecondaryCsrs();
@@ -6551,6 +6553,7 @@ HWTEST_F(DeviceSimpleTests, givenNoGpuHangWhenSynchronizingDeviceThenCallWaitFor
         csr->flushStamp->setStamp(flushStampToWait++);
         csr->captureWaitForTaskCountWithKmdNotifyInputParams = true;
         csr->waitForTaskCountWithKmdNotifyFallbackReturnValue = WaitStatus::ready;
+        csr->resourcesInitialized = true;
     }
 
     auto result = zeDeviceSynchronize(device);
@@ -6575,6 +6578,48 @@ HWTEST_F(DeviceSimpleTests, givenNoGpuHangWhenSynchronizingDeviceThenCallWaitFor
     }
 }
 
+HWTEST_F(DeviceSimpleTests, whenSynchronizingDeviceThenIgnoreUninitializedCsrs) {
+    auto &engines = neoDevice->getAllEngines();
+
+    TaskCountType taskCountToWait = 1u;
+    FlushStamp flushStampToWait = 4u;
+    for (auto &engine : engines) {
+        auto csr = static_cast<UltCommandStreamReceiver<FamilyType> *>(engine.commandStreamReceiver);
+        csr->latestSentTaskCount = 0u;
+        csr->latestFlushedTaskCount = 0u;
+        csr->taskCount = taskCountToWait++;
+        csr->flushStamp->setStamp(flushStampToWait++);
+        csr->captureWaitForTaskCountWithKmdNotifyInputParams = true;
+        csr->waitForTaskCountWithKmdNotifyFallbackReturnValue = WaitStatus::gpuHang;
+        csr->resourcesInitialized = false;
+    }
+
+    auto &secondaryCsrs = neoDevice->getSecondaryCsrs();
+    for (auto &secondaryCsr : secondaryCsrs) {
+        auto csr = static_cast<UltCommandStreamReceiver<FamilyType> *>(secondaryCsr.get());
+        csr->latestSentTaskCount = 0u;
+        csr->latestFlushedTaskCount = 0u;
+        csr->taskCount = taskCountToWait++;
+        csr->flushStamp->setStamp(flushStampToWait++);
+        csr->captureWaitForTaskCountWithKmdNotifyInputParams = true;
+        csr->waitForTaskCountWithKmdNotifyFallbackReturnValue = WaitStatus::gpuHang;
+        csr->resourcesInitialized = false;
+    }
+
+    auto result = zeDeviceSynchronize(device);
+    EXPECT_EQ(ZE_RESULT_SUCCESS, result);
+
+    for (auto &engine : engines) {
+        auto csr = static_cast<UltCommandStreamReceiver<FamilyType> *>(engine.commandStreamReceiver);
+        EXPECT_EQ(0u, csr->waitForTaskCountWithKmdNotifyInputParams.size());
+    }
+
+    for (auto &secondaryCsr : secondaryCsrs) {
+        auto csr = static_cast<UltCommandStreamReceiver<FamilyType> *>(secondaryCsr.get());
+        EXPECT_EQ(0u, csr->waitForTaskCountWithKmdNotifyInputParams.size());
+    }
+}
+
 HWTEST_F(DeviceSimpleTests, givenGpuHangOnSecondaryCsrWhenSynchronizingDeviceThenErrorIsPropagated) {
     if (neoDevice->getSecondaryCsrs().empty()) {
         GTEST_SKIP();
@@ -6583,12 +6628,14 @@ HWTEST_F(DeviceSimpleTests, givenGpuHangOnSecondaryCsrWhenSynchronizingDeviceThe
 
     for (auto &engine : engines) {
         auto csr = static_cast<UltCommandStreamReceiver<FamilyType> *>(engine.commandStreamReceiver);
+        csr->resourcesInitialized = true;
         csr->waitForTaskCountWithKmdNotifyFallbackReturnValue = WaitStatus::ready;
     }
 
     auto &secondaryCsrs = neoDevice->getSecondaryCsrs();
     for (auto &secondaryCsr : secondaryCsrs) {
         auto csr = static_cast<UltCommandStreamReceiver<FamilyType> *>(secondaryCsr.get());
+        csr->resourcesInitialized = true;
         csr->waitForTaskCountWithKmdNotifyFallbackReturnValue = WaitStatus::gpuHang;
     }
 
