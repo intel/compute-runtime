d7151714e8a5f5964eaef50d5af58ef239ee0545
Szymon Morek <szymon.morek@intel.com>
2025-08-05 08:18:49 +0000

    fix: make misaligned userptr 2-way coh on PTL
    Related-To: NEO-15295
    
    Signed-off-by: Szymon Morek <szymon.morek@intel.com>

--- shared/source/xe3_core/ptl/os_agnostic_product_helper_ptl.inl
+++ shared/source/xe3_core/ptl/os_agnostic_product_helper_ptl.inl
@@ -44,4 +44,9 @@ bool ProductHelperHw<gfxProduct>::isStagingBuffersEnabled() const {
     return true;
 }
 
+template <>
+bool ProductHelperHw<gfxProduct>::isMisalignedUserPtr2WayCoherent() const {
+    return true;
+}
+
 } // namespace NEO
--- shared/test/unit_test/xe3_core/ptl/product_helper_tests_ptl.cpp
+++ shared/test/unit_test/xe3_core/ptl/product_helper_tests_ptl.cpp
@@ -136,3 +136,7 @@ PTLTEST_F(PtlProductHelper, givenProductHelperWhenCheckingIsHostDeviceUsmPoolAll
         EXPECT_TRUE(productHelper->isDeviceUsmPoolAllocatorSupported());
     }
 }
+
+PTLTEST_F(PtlProductHelper, givenProductHelperWhenIsMisalignedUserPtr2WayCoherentThenReturnTrue) {
+    EXPECT_TRUE(productHelper->isMisalignedUserPtr2WayCoherent());
+}
