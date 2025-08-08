1b3fa0801ded78cf508c097ac700794b70a154fe
Compute-Runtime-Validation <compute-runtime-validation@intel.com>
2025-08-08 02:25:44 +0200

    Revert "performance: disable tlb flush on LNL Linux"
    This reverts commit a6fe012b6a0c247f9fdd5832c8f568a68768525e.
    
    Signed-off-by: Compute-Runtime-Validation <compute-runtime-validation@intel.com>

--- shared/source/xe2_hpg_core/linux/product_helper_lnl.cpp
+++ shared/source/xe2_hpg_core/linux/product_helper_lnl.cpp
@@ -39,10 +39,5 @@ bool ProductHelperHw<gfxProduct>::deferMOCSToPatIndex(bool isWddmOnLinux) const 
     return !isWddmOnLinux;
 }
 
-template <>
-bool ProductHelperHw<gfxProduct>::isTlbFlushRequired() const {
-    return false;
-}
-
 template class ProductHelperHw<gfxProduct>;
 } // namespace NEO
