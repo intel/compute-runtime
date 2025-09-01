4a2c938c544bd90fbfe6d3d64e10b150f8e6e738
Szymon Morek <szymon.morek@intel.com>
2025-09-01 13:02:43 +0000

    performance: disable tlb invalidation on LNL/Xe
    Related-To: NEO-15939
    
    Signed-off-by: Szymon Morek <szymon.morek@intel.com>

--- shared/source/xe2_hpg_core/linux/product_helper_lnl.cpp
+++ shared/source/xe2_hpg_core/linux/product_helper_lnl.cpp
@@ -39,5 +39,10 @@ bool ProductHelperHw<gfxProduct>::deferMOCSToPatIndex(bool isWddmOnLinux) const 
     return !isWddmOnLinux;
 }
 
+template <>
+bool ProductHelperHw<gfxProduct>::isTlbFlushRequired() const {
+    return false;
+}
+
 template class ProductHelperHw<gfxProduct>;
 } // namespace NEO
