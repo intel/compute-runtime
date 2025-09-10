34d4e526e98fcf96c822329ded707ce60247fbe1
Narendra Bagria <narendra.bagria@intel.com>
2025-09-10 09:37:22 +0000

    feature: enable defer backing by default
    Related-To: NEO-13403
    
    Signed-off-by: Bellekallu Rajkiran <bellekallu.rajkiran@intel.com>
    Signed-off-by: Narendra Bagria <narendra.bagria@intel.com>

--- shared/source/os_interface/linux/xe/ioctl_helper_xe.h
+++ shared/source/os_interface/linux/xe/ioctl_helper_xe.h
@@ -227,6 +227,8 @@ class IoctlHelperXe : public IoctlHelper {
     };
 
     std::unique_ptr<EuDebugInterface> euDebugInterface;
+
+    bool enableDeferBacking = true;
 };
 
 template <typename... XeLogArgs>
