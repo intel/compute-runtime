f33095928c114b218e33f4c20b0fcfb1f9d40ab6
Mateusz Jablonski <mateusz.jablonski@intel.com>
2025-11-21 08:32:48 +0000

    build: rename target gtest to neo_gtest
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

--- cmake/find_gtest.cmake
+++ cmake/find_gtest.cmake
@@ -7,10 +7,10 @@
 get_filename_component(GTEST_DIR "${NEO_SOURCE_DIR}/third_party/gtest" ABSOLUTE)
 if(IS_DIRECTORY ${GTEST_DIR})
 
-  add_library(gtest STATIC EXCLUDE_FROM_ALL
+  add_library(neo_gtest STATIC EXCLUDE_FROM_ALL
               ${NEO_SOURCE_DIR}/cmake/find_gtest.cmake
               ${GTEST_DIR}/src/gtest-all.cc
   )
-  target_include_directories(gtest PRIVATE ${GTEST_DIR})
-  target_include_directories(gtest PUBLIC ${GTEST_DIR}/include)
+  target_include_directories(neo_gtest PRIVATE ${GTEST_DIR})
+  target_include_directories(neo_gtest PUBLIC ${GTEST_DIR}/include)
 endif()
