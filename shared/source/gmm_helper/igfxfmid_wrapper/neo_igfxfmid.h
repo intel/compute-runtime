35901ecfeca2ad69b12325ad6d122c91e7bf1ccc
Mateusz Jablonski <mateusz.jablonski@intel.com>
2025-11-26 18:38:19 +0000

    feature: add placeholder value for cri product
    Related-To: NEO-16649
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

--- shared/source/gmm_helper/igfxfmid_wrapper/neo_igfxfmid.h
+++ shared/source/gmm_helper/igfxfmid_wrapper/neo_igfxfmid.h
@@ -10,8 +10,9 @@
 #include "igfxfmid.h"
 
 namespace NEO {
-constexpr GFXCORE_FAMILY xe3pCoreEnumValue = static_cast<GFXCORE_FAMILY>(0x2300);                     // NOLINT(clang-analyzer-optin.core.EnumCastOutOfRange), NEO-16649
-constexpr GFXCORE_FAMILY maxCoreEnumValue = static_cast<GFXCORE_FAMILY>(xe3pCoreEnumValue + 1);       // NOLINT(clang-analyzer-optin.core.EnumCastOutOfRange), NEO-16649
-constexpr PRODUCT_FAMILY nvlsProductEnumValue = static_cast<PRODUCT_FAMILY>(1340);                    // NOLINT(clang-analyzer-optin.core.EnumCastOutOfRange), NEO-16649
-constexpr PRODUCT_FAMILY maxProductEnumValue = static_cast<PRODUCT_FAMILY>(nvlsProductEnumValue + 1); // NOLINT(clang-analyzer-optin.core.EnumCastOutOfRange), NEO-16649
+constexpr GFXCORE_FAMILY xe3pCoreEnumValue = static_cast<GFXCORE_FAMILY>(0x2300);                    // NOLINT(clang-analyzer-optin.core.EnumCastOutOfRange), NEO-16649
+constexpr GFXCORE_FAMILY maxCoreEnumValue = static_cast<GFXCORE_FAMILY>(xe3pCoreEnumValue + 1);      // NOLINT(clang-analyzer-optin.core.EnumCastOutOfRange), NEO-16649
+constexpr PRODUCT_FAMILY nvlsProductEnumValue = static_cast<PRODUCT_FAMILY>(1340);                   // NOLINT(clang-analyzer-optin.core.EnumCastOutOfRange), NEO-16649
+constexpr PRODUCT_FAMILY criProductEnumValue = static_cast<PRODUCT_FAMILY>(1380);                    // NOLINT(clang-analyzer-optin.core.EnumCastOutOfRange), NEO-16649
+constexpr PRODUCT_FAMILY maxProductEnumValue = static_cast<PRODUCT_FAMILY>(criProductEnumValue + 1); // NOLINT(clang-analyzer-optin.core.EnumCastOutOfRange), NEO-16649
 } // namespace NEO
