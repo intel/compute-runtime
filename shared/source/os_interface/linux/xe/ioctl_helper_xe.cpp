94a857b97584b6aa75e7ff4634baf1317087fddf
Bellekallu Rajkiran <bellekallu.rajkiran@intel.com>
2025-12-01 16:20:16 +0000

    feature: enable defer backing by default
    Related-To: NEO-13403
    
    Signed-off-by: Narendra Bagria <narendra.bagria@intel.com>
    Signed-off-by: Bellekallu Rajkiran <bellekallu.rajkiran@intel.com>

--- shared/source/os_interface/linux/xe/ioctl_helper_xe.cpp
+++ shared/source/os_interface/linux/xe/ioctl_helper_xe.cpp
@@ -271,6 +271,10 @@ bool IoctlHelperXe::initialize() {
             assignValue(tileIdToMediaGtId, gt.tile_id, gt.gt_id);
         }
     }
+    if (debugManager.flags.EnableDeferBacking.get() != -1) {
+        enableDeferBacking = debugManager.flags.EnableDeferBacking.get();
+    }
+
     return true;
 }
 
@@ -653,7 +657,7 @@ int IoctlHelperXe::createGemExt(const MemRegionsVec &memClassInstances, size_t a
     create.placement = static_cast<uint32_t>(memoryInstances.to_ulong());
     create.cpu_caching = this->getCpuCachingMode(isCoherent, isSysMemOnly);
 
-    if (debugManager.flags.EnableDeferBacking.get()) {
+    if (enableDeferBacking) {
         create.flags |= DRM_XE_GEM_CREATE_FLAG_DEFER_BACKING;
     }
 
@@ -1854,11 +1858,7 @@ bool IoctlHelperXe::isImmediateVmBindRequired() const {
 }
 
 bool IoctlHelperXe::makeResidentBeforeLockNeeded() const {
-    auto makeResidentBeforeLockNeeded = false;
-    if (debugManager.flags.EnableDeferBacking.get()) {
-        makeResidentBeforeLockNeeded = true;
-    }
-    return makeResidentBeforeLockNeeded;
+    return enableDeferBacking;
 }
 
 void IoctlHelperXe::insertEngineToContextParams(ContextParamEngines<> &contextParamEngines, uint32_t engineId, const EngineClassInstance *engineClassInstance, uint32_t tileId, bool hasVirtualEngines) {
--- shared/source/os_interface/linux/xe/ioctl_helper_xe.h
+++ shared/source/os_interface/linux/xe/ioctl_helper_xe.h
@@ -233,6 +233,7 @@ class IoctlHelperXe : public IoctlHelper {
     };
 
     std::unique_ptr<EuDebugInterface> euDebugInterface;
+    bool enableDeferBacking = true;
 };
 
 template <typename... XeLogArgs>
