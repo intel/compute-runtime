561385cda19ff8a3f0d7b33256e261a86b56bf36
Szymon Morek <szymon.morek@intel.com>
2025-12-09 16:00:57 +0000

    performance: add compressed pool for cl_buffer
    Related-To: NEO-16423
    
    Signed-off-by: Szymon Morek <szymon.morek@intel.com>

--- opencl/source/context/pool_buffer_additional_checks.cpp
+++ opencl/source/context/pool_buffer_additional_checks.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022-2024 Intel Corporation
+ * Copyright (C) 2022-2025 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
@@ -9,8 +9,9 @@
 
 namespace NEO {
 bool Context::BufferPoolAllocator::flagsAllowBufferFromPool(const cl_mem_flags &flags, const cl_mem_flags_intel &flagsIntel) const {
-    return (flagsIntel & CL_MEM_COMPRESSED_HINT_INTEL) == false &&
-           (flags & CL_MEM_COMPRESSED_HINT_INTEL) == false;
+    auto forbiddenFlag = this->poolType == BufferPoolType::SmallBuffersPool ? CL_MEM_COMPRESSED_HINT_INTEL : CL_MEM_UNCOMPRESSED_HINT_INTEL;
+    return (flagsIntel & forbiddenFlag) == false &&
+           (flags & forbiddenFlag) == false;
 }
 
 } // namespace NEO
