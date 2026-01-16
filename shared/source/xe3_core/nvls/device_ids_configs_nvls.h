61fa94b76c816b4d122bfb1e5b941c43785a8f48
Krzysztof Gibala <krzysztof.gibala@intel.com>
2026-01-16 10:12:10 +0000

    fix: update NVL-U device ids
    Related-To: NEO-17301
    Signed-off-by: Krzysztof Gibala <krzysztof.gibala@intel.com>

--- shared/source/xe3_core/nvls/device_ids_configs_nvls.h
+++ shared/source/xe3_core/nvls/device_ids_configs_nvls.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Intel Corporation
+ * Copyright (C) 2025-2026 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
@@ -10,6 +10,6 @@
 #include <vector>
 
 namespace NEO {
-const std::vector<unsigned short> nvlUDeviceIds{0xD741, 0xD742};
-const std::vector<unsigned short> nvlSDeviceIds{0xD740, 0xD743, 0xD744, 0xD745};
+const std::vector<unsigned short> nvlUDeviceIds{0xD741, 0xD742, 0xD745};
+const std::vector<unsigned short> nvlSDeviceIds{0xD740, 0xD743, 0xD744};
 } // namespace NEO
