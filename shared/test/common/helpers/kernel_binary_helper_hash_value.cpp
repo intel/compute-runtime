8b9c6c1e41e4f61ac1ac532be0bf423a0398e538
Alicja Lukaszewicz <alicja.lukaszewicz@intel.com>
2026-03-25 12:52:14 +0000

    refactor: replace copy-paste LSC code in builtins with header include
    Related-To: NEO-17584
    
    Signed-off-by: Alicja Lukaszewicz <alicja.lukaszewicz@intel.com>

--- shared/test/common/helpers/kernel_binary_helper_hash_value.cpp
+++ shared/test/common/helpers/kernel_binary_helper_hash_value.cpp
@@ -7,5 +7,5 @@
 
 #include "shared/test/common/helpers/kernel_binary_helper.h"
 
-const std::string KernelBinaryHelper::BUILT_INS("7436613300830063646");
-const std::string KernelBinaryHelper::BUILT_INS_WITH_IMAGES("5461894634929597724_images");
+const std::string KernelBinaryHelper::BUILT_INS("3161397265813678743");
+const std::string KernelBinaryHelper::BUILT_INS_WITH_IMAGES("17929825823388309843_images");
