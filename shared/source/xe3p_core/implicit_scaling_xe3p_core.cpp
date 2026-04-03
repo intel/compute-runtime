a0c2954e25606ab62693dab97ce9bd7226042b00
Kamil Kopryk <kamil.kopryk@intel.com>
2026-04-03 16:39:11 +0000

    fix: remove heapful implicit scaling instantiation on xe3p
    Related-To: NEO-16110
    Signed-off-by: Kamil Kopryk <kamil.kopryk@intel.com>

--- shared/source/xe3p_core/implicit_scaling_xe3p_core.cpp
+++ shared/source/xe3p_core/implicit_scaling_xe3p_core.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Intel Corporation
+ * Copyright (C) 2025-2026 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
@@ -12,7 +12,6 @@
 
 namespace NEO {
 using Family = Xe3pCoreFamily;
-using COMPUTE_WALKER = Family::COMPUTE_WALKER;
 using COMPUTE_WALKER_2 = Family::COMPUTE_WALKER_2;
 
 template <>
@@ -29,13 +28,11 @@ bool ImplicitScalingDispatch<Family>::platformSupportsImplicitScaling(const Root
 }
 
 template struct ImplicitScalingDispatch<Family>;
-template void ImplicitScalingDispatch<Family>::dispatchCommands<COMPUTE_WALKER>(LinearStream &commandStream, COMPUTE_WALKER &walkerCmd, const DeviceBitfield &devices,
-                                                                                ImplicitScalingDispatchCommandArgs &dispatchCommandArgs);
+
 template void ImplicitScalingDispatch<Family>::dispatchCommands<COMPUTE_WALKER_2>(LinearStream &commandStream, COMPUTE_WALKER_2 &walkerCmd, const DeviceBitfield &devices,
                                                                                   ImplicitScalingDispatchCommandArgs &dispatchCommandArgs);
-template size_t ImplicitScalingDispatch<Family>::getSize<COMPUTE_WALKER>(bool apiSelfCleanup, bool preferStaticPartitioning, const DeviceBitfield &devices, const Vec3<size_t> &groupStart, const Vec3<size_t> &groupCount);
+
 template size_t ImplicitScalingDispatch<Family>::getSize<COMPUTE_WALKER_2>(bool apiSelfCleanup, bool preferStaticPartitioning, const DeviceBitfield &devices, const Vec3<size_t> &groupStart, const Vec3<size_t> &groupCount);
 } // namespace NEO
 
-template void WalkerPartition::appendWalkerFields<NEO::Family, NEO::COMPUTE_WALKER>(NEO::COMPUTE_WALKER &walkerCmd, uint32_t tileCount, uint32_t workgroupCount);
 template void WalkerPartition::appendWalkerFields<NEO::Family, NEO::COMPUTE_WALKER_2>(NEO::COMPUTE_WALKER_2 &walkerCmd, uint32_t tileCount, uint32_t workgroupCount);
