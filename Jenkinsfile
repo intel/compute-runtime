6b5ee1007b3bd9d2bc366fbce5d1e28c11a96736
ocldev <ocldev@intel.com>
2026-04-30 11:37:34 +0200

    build: buildConfig update
    Signed-off-by: ocldev <ocldev@intel.com>

--- Jenkinsfile
+++ Jenkinsfile
@@ -8,3 +8,8 @@
  */
 
 dependenciesRevision='ac7faf0393ad3fa18bd9d36c3c13fafdba04d838-4110'
+buildConfig = [
+	'master-build': ['gcc11', 'gcc12', 'gcc13', 'gcc14'],
+	'master-gate': ['gcc11', 'gcc12', 'gcc13', 'gcc14'],
+	'branch': 'master',
+]
--- version.cmake
+++ version.cmake
@@ -4,99 +4,36 @@
 # SPDX-License-Identifier: MIT
 #
 
-include(${CMAKE_CURRENT_SOURCE_DIR}/scripts/neo_ww_calculator.cmake)
-
-find_program(GIT NAMES git)
-if(NOT "${GIT}" STREQUAL "GIT-NOTFOUND")
-  if(IS_DIRECTORY ${NEO_SOURCE_DIR}/.git)
-    set(GIT_arg --git-dir=${NEO_SOURCE_DIR}/.git show -s --format=%ct)
-    execute_process(
-                    COMMAND ${GIT} ${GIT_arg}
-                    OUTPUT_VARIABLE GIT_output
-                    OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-    neo_ww_calculator(${GIT_output} VERSION_output)
-    string(REPLACE "." ";" VERSION_list ${VERSION_output})
-  else()
-    message(WARNING "Unable to determine OpenCL major.minor version. Defaulting to 1.0")
-  endif()
-
-  if(NOT DEFINED NEO_OCL_VERSION_MAJOR)
-    if(NOT DEFINED VERSION_list)
-      set(NEO_OCL_VERSION_MAJOR 1)
-    else()
-      list(GET VERSION_list 0 NEO_OCL_VERSION_MAJOR)
-      message(STATUS "Computed OpenCL version major is: ${NEO_OCL_VERSION_MAJOR}")
-    endif()
-  endif()
-
-  if(NOT DEFINED NEO_OCL_VERSION_MINOR)
-    if(NOT DEFINED VERSION_list)
-      set(NEO_OCL_VERSION_MINOR 0)
-    else()
-      list(GET VERSION_list 1 NEO_OCL_VERSION_MINOR)
-      message(STATUS "Computed OpenCL version minor is: ${NEO_OCL_VERSION_MINOR}")
-    endif()
-  endif()
-
-  if(IS_DIRECTORY ${NEO_SOURCE_DIR}/.git)
-    set(GIT_arg --git-dir=${NEO_SOURCE_DIR}/.git rev-parse HEAD)
-    execute_process(
-                    COMMAND ${GIT} ${GIT_arg}
-                    OUTPUT_VARIABLE NEO_REVISION
-                    OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-  endif()
-endif()
-
-if(NOT DEFINED NEO_REVISION)
-  set(NEO_REVISION "No git SHA found, compiled outside git folder")
-endif()
+set(NEO_OCL_VERSION_MAJOR 26)
+set(NEO_OCL_VERSION_MINOR 18)
 
 if(NOT DEFINED NEO_VERSION_BUILD)
-  set(NEO_VERSION_BUILD 0)
+  set(NEO_VERSION_BUILD 038308)
+  set(NEO_REVISION 038308)
+else()
+  set(NEO_REVISION ${NEO_VERSION_BUILD})
 endif()
 
 if(NOT DEFINED NEO_VERSION_HOTFIX)
   set(NEO_VERSION_HOTFIX 0)
 endif()
 
-if(UNIX)
-  set(NEO_OCL_VERSION_SUFFIX "")
-  if(NOT ("${NEO_VERSION_HOTFIX}" STREQUAL "0"))
-    set(NEO_OCL_VERSION_SUFFIX ".${NEO_VERSION_HOTFIX}")
-  endif()
+set(NEO_OCL_VERSION_SUFFIX "")
+if(NOT ("${NEO_VERSION_HOTFIX}" STREQUAL "0"))
+  set(NEO_OCL_VERSION_SUFFIX ".${NEO_VERSION_HOTFIX}")
+endif()
 
-  if(NEO_VERSION_BUILD MATCHES "^([0-9]+)\\.([0-9]+)$")
-    set(NEO_VERSION_BUILD "${CMAKE_MATCH_1}")
-    if(NOT ("${NEO_OCL_VERSION_SUFFIX}" STREQUAL "") AND NOT ("${NEO_OCL_VERSION_SUFFIX}" STREQUAL ".${CMAKE_MATCH_2}"))
-      message(FATAL_ERROR "Inconsistent hotfix version provided: ${NEO_VERSION_HOTFIX} vs ${CMAKE_MATCH_2}")
-    endif()
-    set(NEO_VERSION_HOTFIX "${CMAKE_MATCH_2}")
-    set(NEO_OCL_VERSION_SUFFIX ".${NEO_VERSION_HOTFIX}")
+if(NEO_VERSION_BUILD MATCHES "^([0-9]+)\\.([0-9]+)$")
+  set(NEO_VERSION_BUILD "${CMAKE_MATCH_1}")
+  if(NOT ("${NEO_OCL_VERSION_SUFFIX}" STREQUAL "") AND NOT ("${NEO_OCL_VERSION_SUFFIX}" STREQUAL ".${CMAKE_MATCH_2}"))
+    message(FATAL_ERROR "Inconsistent hotfix version provided: ${NEO_VERSION_HOTFIX} vs ${CMAKE_MATCH_2}")
   endif()
+  set(NEO_VERSION_HOTFIX "${CMAKE_MATCH_2}")
+  set(NEO_OCL_VERSION_SUFFIX ".${NEO_VERSION_HOTFIX}")
 endif()
 
 # OpenCL package version
-if(WIN32)
-
-  if(NOT DEFINED WDDM_VERSION_NUMBER)
-    set(WDDM_VERSION_NUMBER "1")
-  endif()
-
-  if(NOT DEFINED BUILD_WINDOWS_VERSION_STRING_MINOR)
-    set(BUILD_WINDOWS_VERSION_STRING_MINOR "0")
-  endif()
-
-  if(NOT DEFINED BUILD_WINDOWS_VERSION_STRING_MAJOR)
-    set(BUILD_WINDOWS_VERSION_STRING_MAJOR "0")
-  endif()
-
-  set(NEO_OCL_DRIVER_VERSION "${WDDM_VERSION_NUMBER}.0.${BUILD_WINDOWS_VERSION_STRING_MAJOR}.${BUILD_WINDOWS_VERSION_STRING_MINOR} (${NEO_OCL_VERSION_MAJOR}.${NEO_OCL_VERSION_MINOR})")
-
-else()
-  set(NEO_OCL_DRIVER_VERSION "${NEO_OCL_VERSION_MAJOR}.${NEO_OCL_VERSION_MINOR}.${NEO_VERSION_BUILD}${NEO_OCL_VERSION_SUFFIX}")
-endif()
+set(NEO_OCL_DRIVER_VERSION "${NEO_OCL_VERSION_MAJOR}.${NEO_OCL_VERSION_MINOR}.${NEO_VERSION_BUILD}${NEO_OCL_VERSION_SUFFIX}")
 
 # Level-Zero package version
 set(NEO_L0_VERSION_MAJOR 1)
