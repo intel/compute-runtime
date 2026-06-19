05bfa1364dbc65e8cc30f096fa6d4ac756962bb2
Pawel Richert <pawel.richert@intel.com>
2026-06-19 15:14:35 +0000

    fix: validate size is multiple of patternSize
    zeCommandListAppendMemoryFill and zexCommandListAppendMemoryFillWithParameters
    Related-To: NEO-18120
    
    Signed-off-by: Pawel Richert <pawel.richert@intel.com>

--- level_zero/api/driver_experimental/public/zex_cmdlist.cpp
+++ level_zero/api/driver_experimental/public/zex_cmdlist.cpp
@@ -159,6 +159,9 @@ zexCommandListAppendMemoryFillWithParameters(
     if (ret != ZE_RESULT_ERROR_NOT_AVAILABLE) {
         return ret;
     }
+    if (size % patternSize != 0) {
+        return ZE_RESULT_ERROR_INVALID_SIZE;
+    }
 
     return cmdList->appendMemoryFillWithParameters(ptr, pattern, patternSize, size, pNext, hEvent, numWaitEvents, phWaitEvents);
 }
