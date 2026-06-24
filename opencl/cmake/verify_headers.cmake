2f54419bd0c46336e8b85c1590fea44ee2f2ded5
Mateusz Jablonski <mateusz.jablonski@intel.com>
2026-06-24 16:06:03 +0000

    fix: correct include path for verify opencl headers
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

--- opencl/cmake/verify_headers.cmake
+++ opencl/cmake/verify_headers.cmake
@@ -6,6 +6,16 @@
 
 list(APPEND opencl_include_dirs ${KHRONOS_GL_HEADERS_DIR})
 
+if(WIN32)
+  list(APPEND opencl_include_dirs
+       ${NEO_SOURCE_DIR}/opencl/source/os_interface/windows
+  )
+else()
+  list(APPEND opencl_include_dirs
+       ${NEO_SOURCE_DIR}/opencl/source/os_interface/linux
+  )
+endif()
+
 list(APPEND opencl_exclude_patterns
      "/cl_gl_private_intel_structures.h"
 )
