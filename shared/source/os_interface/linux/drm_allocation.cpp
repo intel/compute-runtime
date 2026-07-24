4079249956fdb9f655d83318e97f5923d1c6dcb2
Grochowski, Stanislaw <stanislaw.grochowski@intel.com>
2026-07-24 15:05:26 +0000

    fix: set numHandles to 1 for single bo drm allocations
    Related-To: HSD-18044074067
    
    Signed-off-by: Grochowski, Stanislaw <stanislaw.grochowski@intel.com>

--- shared/source/os_interface/linux/drm_allocation.cpp
+++ shared/source/os_interface/linux/drm_allocation.cpp
@@ -32,6 +32,7 @@ namespace NEO {
 DrmAllocation::DrmAllocation(uint32_t rootDeviceIndex, size_t numGmms, AllocationType allocationType, BufferObject *bo, void *ptrIn, size_t sizeIn, osHandle sharedHandle, MemoryPool pool, uint64_t canonizedGpuAddress)
     : GraphicsAllocation(rootDeviceIndex, numGmms, allocationType, ptrIn, sizeIn, sharedHandle, pool, MemoryManager::maxOsContextCount, canonizedGpuAddress) {
     bufferObjects.push_back(bo);
+    this->numHandles = (bo != nullptr) ? 1u : 0u;
     resizeBufferObjects(EngineLimits::maxHandleCount);
     handles.resize(EngineLimits::maxHandleCount, std::numeric_limits<uint64_t>::max());
 }
@@ -39,6 +40,7 @@ DrmAllocation::DrmAllocation(uint32_t rootDeviceIndex, size_t numGmms, Allocatio
 DrmAllocation::DrmAllocation(uint32_t rootDeviceIndex, size_t numGmms, AllocationType allocationType, BufferObject *bo, void *ptrIn, uint64_t canonizedGpuAddress, size_t sizeIn, MemoryPool pool)
     : GraphicsAllocation(rootDeviceIndex, numGmms, allocationType, ptrIn, canonizedGpuAddress, 0, sizeIn, pool, MemoryManager::maxOsContextCount) {
     bufferObjects.push_back(bo);
+    this->numHandles = (bo != nullptr) ? 1u : 0u;
     resizeBufferObjects(EngineLimits::maxHandleCount);
     handles.resize(EngineLimits::maxHandleCount, std::numeric_limits<uint64_t>::max());
 }
@@ -46,6 +48,7 @@ DrmAllocation::DrmAllocation(uint32_t rootDeviceIndex, size_t numGmms, Allocatio
 DrmAllocation::DrmAllocation(uint32_t rootDeviceIndex, size_t numGmms, AllocationType allocationType, BufferObjects &bos, void *ptrIn, uint64_t canonizedGpuAddress, size_t sizeIn, MemoryPool pool)
     : GraphicsAllocation(rootDeviceIndex, numGmms, allocationType, ptrIn, canonizedGpuAddress, 0, sizeIn, pool, MemoryManager::maxOsContextCount),
       bufferObjects(bos) {
+    this->numHandles = static_cast<uint32_t>(bufferObjects.size());
     handles.resize(EngineLimits::maxHandleCount, std::numeric_limits<uint64_t>::max());
 }
 
--- shared/source/os_interface/linux/drm_memory_manager.cpp
+++ shared/source/os_interface/linux/drm_memory_manager.cpp
@@ -3711,7 +3711,6 @@ GraphicsAllocation *DrmMemoryManager::createSharedUnifiedMemoryAllocation(const 
     allocation->setMmapPtr(cpuBasePointer);
     allocation->setMmapSize(totalSizeToAlloc);
     allocation->setReservedAddressRange(reinterpret_cast<void *>(preferredAddress), totalSizeToAlloc);
-    allocation->setNumHandles(static_cast<uint32_t>(bos.size()));
     allocation->storageInfo = allocationData.storageInfo;
     allocation->storageInfo.isChunked = useChunking;
     allocation->storageInfo.numOfChunks = numOfChunks;
