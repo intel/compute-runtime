e9ebb5dc9b05d03f8a9faa7c259a29daf34cb895
Mateusz Jablonski <mateusz.jablonski@intel.com>
2026-08-28 09:11:37 +0000

    refactor: remove not needed code
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

--- shared/wsl_compute_helper/source/wsl_compute_helper_types_demarshall.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) 2021 Intel Corporation
- *
- * SPDX-License-Identifier: MIT
- *
- */
-
-// This is a generated file - please don't modify directly
-#include "wsl_compute_helper_types_demarshall.h"
