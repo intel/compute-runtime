cd7ed30db9fa205b156f114cb11ad982a411b4e8
Mateusz Jablonski <mateusz.jablonski@intel.com>
2026-09-10 12:51:01 +0000

    build: update drm-next headers
    Signed-off-by: Mateusz Jablonski <mateusz.jablonski@intel.com>

--- third_party/uapi/drm-next/drm/drm_mode.h
+++ third_party/uapi/drm-next/drm/drm_mode.h
@@ -955,6 +955,18 @@ enum drm_colorop_type {
 	 *         color = lut3d[index]
 	 */
 	DRM_COLOROP_3D_LUT,
+
+	/**
+	 * @DRM_COLOROP_FIXED_MATRIX:
+	 *
+	 * enum string "Fixed Matrix"
+	 *
+	 * A Colorop block that performs a pre-defined matrix operation selected
+	 * via the FIXED_MATRIX_TYPE enum property. The driver advertises the supported
+	 * operations through this property.
+	 */
+	DRM_COLOROP_FIXED_MATRIX,
+
 };
 
 /**
